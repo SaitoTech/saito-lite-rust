2fbeee66e523679bfbbeca6137c13d7ae281a8e3
David Lancashire <david.lancashire@gmail.com>
2021-11-02 18:08:47 +0700

    feat: initial miner and golden ticket code

--- bundler/default/lib/saito/goldenticket.js
+++ bundler/default/lib/saito/goldenticket.js
@@ -8,8 +8,27 @@ class GoldenTicket {
     this.app = app || {};
   }
 
+
+
+  validate(target_hash, random_hash, publickey, difficulty) {
+
+    let solution = this.app.crypto.hash(random_hash+publickey);
+    let difficulty_order = Math.round(parseFloat(difficulty)/parseFloat(1_0000_0000));
+    let difficulty_grain = difficulty % 1_0000_0000;
+
+console.log(solution + " --- " + difficulty_order + " --- " + difficulty_grain);
+
+    let gtsol = parseInt(sol.slice(0,difficulty_order+1),16);
+    let bksol = parseInt(blk.returnHash().slice(0,difficulty_order+1),16);
+
+    if (gtsol >= bksol && (gtsol-bksol)/16 <= difficulty_grain) {
+      return true;
+    }
+    return false;
+
+  }
+
 }
 
 module.exports = GoldenTicket;
-
 
