3ae8c9ee85ea578a15d98410bd3f81ec33feffff
Daniel Worlton <f8daniel@gmail.com>
2022-02-07 15:28:05 +0700

    enhance: game instructions

--- mods/wuziqi/wuziqi.js
+++ mods/wuziqi/wuziqi.js
@@ -155,7 +155,7 @@ class Wuziqi extends GameTemplate {
     //html for game intro/rules
     returnGameRulesHTML() {
 
-        let overlay_html = `<div class="intro">
+        let overlay_html = `<div class="rules-overlay intro">
           <h2>Wuziqi （五子棋）</h2>
            <p> Wuziqi, also known as Gokomu and Gobang, is a simple two player game played on a Go board. It is similar to <abbr title="also known as Naughts and Crosses">Tic-Tac-Toe</abbr> or Connect Four in that players alternately place tokens in an attempt to create a line of five consecutive tokens of their color. Tokens may be placed anywhere on the board not already occupied.</p>
            <p> The first player to place five of their own tokens in a continuous line--vertical, horizontal or diagonally--wins the round. The player who wins the most rounds, wins the match.</p><p> Matches are best out of three by default, but you can change this in the advanced options in the arcade. You may also specify the size of the board for added challenge.</p>
