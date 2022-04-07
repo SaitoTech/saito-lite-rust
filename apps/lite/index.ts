a78a7200705868622cbc1d0e9ff97776adef56ea
Sanka Darshana <sankadarshana@gmail.com>
2022-04-07 14:53:06 +0530

    test code on wasm loading

--- apps/lite/index.ts
+++ apps/lite/index.ts
@@ -2,11 +2,17 @@ import { Saito } from "../core";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import mods_config from "../../config/modules.config";
-
+import SaitoJs from "saito-js";
 async function init() {
+  console.log("aaaaaaaaaaaaa");
+  await SaitoJs.initialize();
+  console.log("33333");
+  let ins = SaitoJs.getInstance();
+  console.log("44444");
   const saito = new Saito({ mod_paths: mods_config.lite });
   saito.BROWSER = 1;
   await saito.init();
+  console.log("bbbbbbbbbbbbb");
 }
 
 // init();
