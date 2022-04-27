df4d13b24d498e1846f1a4840c9baaf60b717196
Daniel Worlton <f8daniel@gmail.com>
2022-04-27 12:19:00 +0700

    enhance: styling of share links

--- mods/arcade/lib/arcade-main/templates/arcade-link.template.js
+++ mods/arcade/lib/arcade-main/templates/arcade-link.template.js
@@ -8,11 +8,11 @@ module.exports = (app, mod, data) => {
             <div>
               <i id="copy-invite-link" class="fa fa-clipboard" aria-hidden="true"></i>
             </div>
-            <div class="social-links">
+            <!--div class="social-links">
               <img src="/saito/img/invite/facebook.png" />
               <img src="/saito/img/invite/instagram.png" />
               <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" target="_blank" class="twitter-share-button" data-show-count="false" data-text="Come play ${game} with me on the Saito Arcade" data-url=${invite_link}><img src="/saito/img/invite/twitter.png" /></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </div>
+            </div-->
           </div>
 
           <p id="invite-link-text" class="invite-link-text">${invite_link}</p>
