09ff161a5666a653687ca875ae2afaceeca5ca22
davikstone2 <onyeji@saito.tech>
2022-05-02 09:48:28 +0100

    update

--- mods/stun/stun.js
+++ mods/stun/stun.js
@@ -40,21 +40,21 @@ class Stun extends ModTemplate {
       {
         urls: "stun:openrelay.metered.ca:80",
       },
-      // {
-      //   urls: "turn:openrelay.metered.ca:80",
-      //   username: "openrelayproject",
-      //   credential: "openrelayproject",
-      // },
-      // {
-      //   urls: "turn:openrelay.metered.ca:443",
-      //   username: "openrelayproject",
-      //   credential: "openrelayproject",
-      // },
-      // {
-      //   urls: "turn:openrelay.metered.ca:443?transport=tcp",
-      //   username: "openrelayproject",
-      //   credential: "openrelayproject",
-      // },
+      {
+        urls: "turn:openrelay.metered.ca:80",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443?transport=tcp",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
     ]
 
 
