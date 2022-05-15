82b42c36b267d64aee21ba1bdb099781f0b0add0
davikstone2 <onyeji@saito.tech>
2022-05-15 21:14:36 +0100

    chat

--- mods/ui-elements/ui-elements.js
+++ mods/ui-elements/ui-elements.js
@@ -283,6 +283,69 @@ class UI_ELEMENTS extends AppTemplate {
 
     });
 
+    //  Chat events
+    document.querySelector("#chat-container-close").addEventListener("click", () => {
+
+      document.querySelector('.saito-chat-container').classList.remove('display-chat');
+    });
+
+    document.querySelectorAll(".saito-chat-toggle").forEach(item => {
+      item.addEventListener("click", () => {
+        const chatContainer = document.querySelector('.saito-chat-container');
+        const chatBody = document.querySelector('.saito-chat-body');
+        chatContainer.classList.contains('display-chat') == false ? chatContainer.classList.add('display-chat') : chatContainer.classList.remove('display-chat')
+        chatBody.scroll({
+          top: chatBody.scrollHeight,
+          behavior: "smooth"
+        })
+
+      });
+
+    })
+
+    document.body.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+
+        sendMessage()
+      }
+    })
+
+    document.querySelector("#saito-sendmsg-btn").addEventListener('click', sendMessage)
+
+    function sendMessage() {
+      const chatInput = document.querySelector('#saito-chat-input');
+      const chatBody = document.querySelector('.saito-chat-body')
+
+      const time = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} `
+      if (chatInput.value != "") {
+        console.log(chatInput.value);
+        const template = `<div class="saito-chat-bubble me"> 
+       <div class="chat-dialog">
+         <img src="/saito/img/account.svg"/>
+         <div>
+           <p class="saito-chat-address">kkadiaiudaol...</p>
+           <p>${chatInput.value.trim()}</p>
+
+         </div>
+         <span>${time}</span>
+       </div>
+
+     </div>`;
+
+        chatBody.insertAdjacentHTML('beforeend', template);
+        chatInput.value = "";
+
+        chatBody.scroll({
+          top: chatBody.scrollHeight,
+          behavior: "smooth"
+        })
+
+        console.log(template);
+      }
+    }
+
+
+
 
   }
 
