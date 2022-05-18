04e2a89ed71d4dc0a0f438cdd045e23469640966
davikstone2 <onyeji@saito.tech>
2022-05-18 04:55:32 +0400

    chirp port

--- mods/ui-elements/ui-elements.js
+++ mods/ui-elements/ui-elements.js
@@ -180,6 +180,7 @@ class UI_ELEMENTS extends AppTemplate {
 
   render(app) {
     if (app.BROWSER != 1 || this.browser_active != 1) {
+      console.log('not the one')
       return;
     }
 
@@ -203,192 +204,18 @@ class UI_ELEMENTS extends AppTemplate {
   }
 
 
-  attachEvents(app) {
-    const self = this;
-    const listener2 = $('#render_arcade').on('click', function (e) {
-      const app_mod = app.modules.returnModule("App");
-      const arcade_mod = app.modules.returnModule("Arcade");
-
-      app_mod.render(app, arcade_mod);
-      self.eventListeners.push({ type: 'click', listener2 });
-    });
-
-
-    document.querySelectorAll('.big-menu-link').forEach(link => {
-      link.addEventListener('click', function () {
-
-        const app_mod = app.modules.returnModule("App");
-        const ui_mod = app.modules.returnModule("ui-elements");
-
-        const url = this.getAttribute('data-url');
-        const route = this.getAttribute('data-route');
-
-        if (route) {
-          const mod = app.modules.returnModule(route);
-          app_mod.render(app, mod, route);
-        } else if (url) {
-          console.log(ui_mod, url, this);
-          ui_mod.parameters['display'] = url.split("=")[url.split('=').length - 1];
-          ui_mod.render(app);
-
-        }
-
-      })
-    })
-    document.querySelectorAll('.breadcrumb-link').forEach(link => {
-      link.addEventListener('click', function () {
-
-        const app_mod = app.modules.returnModule("App");
-        const ui_mod = app.modules.returnModule("ui-elements");
-
-        const url = this.getAttribute('data-url');
-        const route = this.getAttribute('data-route');
-
-        if (route) {
-          const mod = app.modules.returnModule(route);
-          app_mod.render(app, mod, route);
-        } else if (url) {
-          console.log(ui_mod, url, this);
-          ui_mod.parameters['display'] = url.split("=")[url.split('=').length - 1];
-          ui_mod.render(app);
-
-        }
-        self.toggleMenu();
-
-      })
-    })
-
-    document.querySelector(".switch").addEventListener('click', (e) => {
-      e.preventDefault();
-      console.log('toggling dark mode')
-      const slider = document.querySelector(".slider");
-      console.log(slider.classList.contains("checked"));
-      const app_mod = app.modules.returnModule("App");
-
-      app_mod.toggleDarkMode();
-      // self.eventListeners.push({ type: 'click', listener });
-      if (slider.classList.contains("checked")) {
-        slider.classList.remove("checked");
-      } else {
-        slider.classList.add("checked");
-      }
-    })
-
-    document.querySelector("#menuToggle").addEventListener("click", this.toggleMenu);
-
-
-    document.querySelector("#sidebar-toggle-mobile").addEventListener("click", () => {
-      console.log('clicking')
-      document.querySelector('.sidebar-mobile').classList.contains('display') == false ? document.querySelector('.sidebar-mobile').classList.add('display') : document.querySelector('.sidebar-mobile').classList.remove('display')
-
-    });
-
-    //  Chat events
-    document.querySelector("#chat-container-close").addEventListener("click", () => {
-
-      document.querySelector('.saito-chat-container').classList.remove('display-chat');
-    });
-
-    document.querySelectorAll(".saito-chat-toggle").forEach(item => {
-      item.addEventListener("click", () => {
-        const chatContainer = document.querySelector('.saito-chat-container');
-        const chatBody = document.querySelector('.saito-chat-body');
-        chatContainer.classList.contains('display-chat') == false ? chatContainer.classList.add('display-chat') : chatContainer.classList.remove('display-chat')
-        chatBody.scroll({
-          top: chatBody.scrollHeight,
-          behavior: "smooth"
-        })
-
-      });
-
-    })
-
-    document.body.addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
-
-        sendMessage()
-      }
-    })
-
-    document.querySelector("#saito-sendmsg-btn").addEventListener('click', sendMessage)
-
-    function sendMessage() {
-      const chatInput = document.querySelector('#saito-chat-input');
-      const chatBody = document.querySelector('.saito-chat-body')
-
-      const time = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} `
-      if (chatInput.value != "") {
-        console.log(chatInput.value);
-        const template = `<div class="saito-chat-bubble me"> 
-       <div class="chat-dialog">
-         <img src="/saito/img/account.svg"/>
-         <div>
-           <p class="saito-chat-address">kkadiaiudaol...</p>
-           <p>${chatInput.value.trim()}</p>
-
-         </div>
-         <span>${time}</span>
-       </div>
-
-     </div>`;
-
-        chatBody.insertAdjacentHTML('beforeend', template);
-        chatInput.value = "";
-
-        chatBody.scroll({
-          top: chatBody.scrollHeight,
-          behavior: "smooth"
-        })
-
-        console.log(template);
-      }
-    }
-
-
-
-
-  }
-
 
   returnBaseHTML(app) {
 
     return null;
   }
 
-  toggleMenu(e) {
-    console.log("toggling menu");
-    document
-      .querySelector("#hamburger-contents")
-      .classList.contains("show-menu")
-      ? document
-        .querySelector("#hamburger-contents")
-        .classList.remove("show-menu")
-      : document
-        .querySelector("#hamburger-contents")
-        .classList.add("show-menu");
-  }
 
 
 
 
-  renderMain(app) {
-    if (app.BROWSER != 1 || this.browser_active != 1) {
-      return;
-    }
-    EmailMain.render(app, this);
-    EmailMain.attachEvents(app, this);
-  }
 
-  renderSidebar(app) {
-    if (app.BROWSER != 1 || this.browser_active != 1) {
-      return;
-    }
-    console.log("### 1");
-    EmailSidebar.render(app, this);
-    console.log("### 2");
-    EmailSidebar.attachEvents(app, this);
-    console.log("### 3");
-  }
+
 
 
   respondTo(type = "") {
