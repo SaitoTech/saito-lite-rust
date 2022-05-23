b8f071fcab46486ee6d82a93d6a2d6de1cfc360b
Umair Khan <umair@saito.tech>
2022-05-23 14:40:38 +0500

    Update the layout for withdrawing tokens from arcade

--- mods/wallet/lib/overlay/send-tokens-overlay.js
+++ mods/wallet/lib/overlay/send-tokens-overlay.js
@@ -12,33 +12,64 @@ module.exports = SendTokensOverlay = {
 
   attachEvents(app, mod) {
 
-    document.getElementById("wallet-send-tokens-form").onsubmit = (e) => {
+    let preferred_crypto = app.wallet.wallet.preferred_crypto;
+    let ticker = preferred_crypto;
+    let cryptomod = null;
+    let asset_id = null;
+
+    for (let i = 0; i < app.modules.mods.length; i++) {
+      if (app.modules.mods[i].ticker === ticker) {
+        cryptomod = app.modules.mods[i];
+        asset_id = cryptomod.asset_id;        
+      }
+    }
+
+    sender =  cryptomod.destination + "|" + cryptomod.mixin.mixin.user_id + "|" + "mixin";
+
+    document.querySelector("#withdraw-fee-accept").onclick = (e) => {
+      let hash = app.wallet.sendPayment([sender], [address], [amount], (new Date().getTime()), btoa(sender+address+amount+Date.now()), function() {
+                  cryptomod.overlay.hide();
+                }, ticker);
+      
+      salert('Withdrawal successful!');
+      setTimeout(function(){
+        location.reload();
+      }, 2000);
+    }
+
+    document.querySelector("#withdraw-fee-reject").onclick = (e) => {
+      document.querySelector("#withdrawl-sent-cont").style.display = 'none';
+      document.querySelector("#withdrawl-form-cont").style.display = 'block';
+    }
+
+    document.querySelector("#withdrawal-form").onsubmit = (e) => {
       e.preventDefault();
+      document.querySelector(".error-msg").style.display = "none";
+      amount = document.querySelector(".withdraw_amount").value;
+      address = document.querySelector(".withdraw_address").value;
+      amount_avl = document.querySelector("#amount-avl").getAttribute('data-amount-avl');
 
-      let recipient = document.getElementById("wallet-send-tokens-recipient").value;
-      let amount = document.getElementById("wallet-send-tokens-amount").value;
-      let preferred_crypto = app.wallet.wallet.preferred_crypto;
-      let ticker = preferred_crypto;
-      let cryptomod = null;
-
-      for (let i = 0; i < app.modules.mods.length; i++) {
-	if (app.modules.mods[i].ticker === ticker) {
-	  cryptomod = app.modules.mods[i];
-	}
+      if (amount > amount_avl) {
+        document.querySelector(".max-amount-error").innerHTML = "Error: Not enough amount avaibale ("+amount_avl+" available)";
+        document.querySelector(".max-amount-error").style.display = "block";
+        return false;          
       }
 
-alert("PREFERRED CRYPTO: " + app.wallet.wallet.preferred_crypto);
-
-      let c = confirm(`Do you wish to send ${amount} ${app.wallet.wallet.preferred_crypto}/${ticker} to ${recipient}`);
-      if (c) {
-	let sender = cryptomod.returnAddress();
-	let hash = app.wallet.sendPayment([sender], [recipient], [amount], (new Date().getTime()), btoa(sender+recipient+amount+Date.now()), function() {
-	  mod.overlay.hide();
-    salert("Transfer successful");
-	}, ticker);
-      } else {
-        salert("Transfer cancelled");
+      if (amount <= 0) {
+        document.querySelector(".max-amount-error").innerHTML = "Error: Amount should be greater than 0";
+        document.querySelector(".max-amount-error").style.display = "block";
+        return false;          
       }
+
+      document.querySelector(".decision-cont").style.display = 'none';
+      document.getElementById("confirm-fee-text").innerHTML = amount+' ' + ticker + ' to '+ address + '. Continue with withdrawal?';
+      document.querySelector("#withdrawl-sent-cont").style.display = 'block';
+
+    }
+
+    document.querySelector("#max-amount-btn").onclick = (e) => {
+      let amount_avl = document.querySelector("#amount-avl").getAttribute('data-amount-avl');
+      document.querySelector("#withdraw_amount").value = amount_avl;
     }
 
   },
