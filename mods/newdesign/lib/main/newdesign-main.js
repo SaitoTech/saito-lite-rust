b9fba6a921b84ffcb9d53e7f9fedccb7c17781d2
davikstone2 <onyeji@saito.tech>
2022-05-25 12:11:30 +0100

    css design

--- mods/newdesign/lib/main/newdesign-main.js
+++ mods/newdesign/lib/main/newdesign-main.js
@@ -2,10 +2,10 @@ const NewDesignMainTemplate = require('./newdesign-main.template');
 
 module.exports = NewDesignMain = {
 
-    render(app) {
-      if (!document.querySelector('.container')) {
-        app.browser.addElementToDom(NewDesignMainTemplate(app));
-      }
-    },
+  render(app) {
+    if (!document.querySelector('.container')) {
+      app.browser.addElementToDom(NewDesignMainTemplate(app));
+    }
+  },
 
 }
