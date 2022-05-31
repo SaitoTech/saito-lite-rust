20badb6ea22a45e8ada9583a73493cda2e821e0d
davikstone2 <102211421+davikstone2@users.noreply.github.com>
2022-05-31 10:25:55 +0100

    Newdesign module (#589)
    * feat: new design module - initial
    
    * feat: new design module added
    
    * fix: new design
    
    * added html elements template
    
    * added style sheet
    
    * added saito.css
    
    * update
    
    * css design
    
    * update
    
    * css theme
    
    * update
    
    * buttons added
    
    * update styles
    
    * update
    
    * custom select
    
    * update
    
    * update css
    
    * cleaned up format
    
    * css updates
    
    * css updates
    
    * update
    
    * add grid system
    
    * update boxes
    
    * update boxes
    
    * update boxes
    
    * update
    
    * css updates
    
    * css updates
    
    * renaming main css file
    
    * add appropriate background colors
    
    * update
    
    * remove compile-e
    
    * update
    
    Co-authored-by: David Lancashire <david.lancashire@gmail.com>
    Co-authored-by: arpee <richard@saito.tech>

---
+++ mods/newdesign/newdesign.js
@@ -0,0 +1,34 @@
+const ModTemplate = require("../../lib/templates/modtemplate");
+const NewDesignMain = require("./lib/main/newdesign-main");
+
+class NewDesign extends ModTemplate {
+
+  constructor(app) {
+    super(app);
+
+    this.name = "NewDesign";
+    this.slug = "newdesign";
+    this.description = "Saito Design Reference Module";
+    this.categories = "Design Development";
+
+    this.header = null;
+    this.overlay = null;
+
+    this.styles = ['/newdesign/saito.css', '/saito/lib/date-picker/dist/css/datepicker.css'];
+    this.scripts = ['/saito/lib/date-picker/dist/js/datepicker-full.js']
+
+  }
+
+  render(app) {
+
+    super.render(app)
+
+    NewDesignMain.render(app, this);
+
+  }
+
+}
+
+module.exports = NewDesign;
+
+
