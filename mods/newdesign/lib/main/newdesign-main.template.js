0220aaf7033fb4806f31922df94442473b7d8084
davikstone2 <onyeji@saito.tech>
2022-06-01 08:07:35 +0100

    fix select

--- mods/newdesign/lib/main/newdesign-main.template.js
+++ mods/newdesign/lib/main/newdesign-main.template.js
@@ -32,7 +32,7 @@ module.exports = (app) => {
         type="radio"></input></div>
     <label>Select</label>
 
-    <select>
+    <select class="saito-new-select">
       <option value="0">Select car:</option>
       <option value="1">Audi</option>
       <option value="2">BMW</option>
