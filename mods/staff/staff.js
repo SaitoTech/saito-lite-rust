ecc5507616219bbd6c535bbb0130d5ade96b4fc7
karlsaito <karl@saito.tech>
2022-06-09 17:48:34 +0800

    final push of clean coding

--- mods/staff/staff.js
+++ mods/staff/staff.js
@@ -19,22 +19,14 @@ class Staff extends ModTemplate {
     //this function will trigger when you are connected to a peer.
     //Use this to check if the key from the wallet is in the db.
     onPeerHandshakeComplete(app, peer) {
-        let staff_self = app.modules.returnModule("Staff");
-        //run on the node, rendering BROWSER on browser checking - don't do this on full node
         if (app.BROWSER == 1) {
             this.checkRecord(this.app.wallet.returnPublicKey());
         }
-
-        // check if key in db
-        // do something like
-        //   document.getElementById("isRegistered").value = this.getRecord(this.app.wallet.returnPublicKey());
-
     }
 
     render(app, mod) {
         document.querySelector('#publicKey').innerHTML = this.app.wallet.returnPublicKey();
         document.getElementById("isRegistered").checked = this.isThisRegistered;
-        console.log("registered state"+ this.isThisRegistered);
         if (this.isThisRegistered) {
             document.getElementById("add_staff").style.display = "none";
             document.getElementById("remove_staff").style.display = "block";
@@ -44,40 +36,33 @@ class Staff extends ModTemplate {
         }
     }
 
+
     attachEvents(app, mod) {
         document.querySelector('#add_staff').onclick = () => {
             var publicKey = this.app.wallet.returnPublicKey();
             if (publicKey) {
-                this.app.modules.returnModule("Staff").sendRegisterTX(publicKey, "register");
-                //this.registerToDatabase(publicKey);
-                // here you want to use sendRegisterTransaction with the wallet public key
+                this.app.modules.returnModule(this.name).sendRegisterTX(publicKey, "register");
             }
         }
         document.querySelector('#remove_staff').onclick = () => {
             var publicKey = this.app.wallet.returnPublicKey();
             if (publicKey) {
-                this.app.modules.returnModule("Staff").sendRegisterTX(publicKey, "deregister");
-                //this.registerToDatabase(publicKey);
-                // here you want to use sendRegisterTransaction with the wallet public key
+                this.app.modules.returnModule(this.name).sendRegisterTX(publicKey, "deregister");
             }
         }
     }
 
     async checkRecord(publickey) {
-        console.log("key:" + publickey);
         sql = 'SELECT publickey FROM staff WHERE publickey = "' + publickey + '";';
-        console.log(sql);
-        this.sendPeerDatabaseRequestWithFilter("Staff", sql,
+        this.sendPeerDatabaseRequestWithFilter(this.name, sql,
             (res) => {
                 if (res) {
                     if (res.rows) { 
                         if (res.rows.length > 0) {
-                            console.log(res.rows[0].publickey);
                             this.isThisRegistered = true;
                         } else {
                             this.isThisRegistered = false;
                         }
-                        console.log("check record isthis registered" + this.isThisRegistered);
                         this.render();
                     }
                 }
@@ -87,8 +72,7 @@ class Staff extends ModTemplate {
 
     createRegisterTransaction(publicKey, msgType) {
         let newtx = this.app.wallet.createUnsignedTransaction();
-
-        newtx.msg.module = "Staff";
+        newtx.msg.module = this.name;
         newtx.msg.type = msgType;
         newtx.msg.publicKey = publicKey;
 
@@ -96,19 +80,16 @@ class Staff extends ModTemplate {
     }
 
     sendRegisterTX(publicKey, msgType) {
-        var registerTx = this.app.modules.returnModule("Staff").createRegisterTransaction(this.app.wallet.returnPublicKey(), msgType);
+        var registerTx = this.app.modules.returnModule(this.name).createRegisterTransaction(this.app.wallet.returnPublicKey(), msgType);
         this.app.network.propagateTransaction(registerTx);
     }
 
     onConfirmation(blk, tx, conf, app) {
-        console.log("running on: " + app.BROWSER);
+        let txmsg = tx.returnMessage();
         if (app.BROWSER == 0) {
             if (conf == 0) {
-                let txmsg = tx.returnMessage();
-                console.log(txmsg.module == "Staff");
-                if (txmsg.module == "Staff") {
-                    let staff_self = app.modules.returnModule("Staff");
-                    console.log("processing: " + txmsg.type);
+                if (txmsg.module == this.name) {
+                    let staff_self = app.modules.returnModule(this.name);
                     if (txmsg.type == "register") {
                         let sql = "INSERT INTO staff (publickey) VALUES ($publickey)";
                         staff_self.receiveRegisterTransaction(txmsg, sql);
@@ -121,11 +102,8 @@ class Staff extends ModTemplate {
             }
         } else {
             if (conf == 0) {
-                let txmsg = tx.returnMessage();
-                if (txmsg.module == "Staff") {
-                    console.log("went here on DOM onconfirmation");
+                if (txmsg.module == this.name) {
                     if (txmsg.publicKey == this.app.wallet.returnPublicKey()) {
-                        console.log("went here on DOM onconfirmation check record");
                       this.checkRecord(this.app.wallet.returnPublicKey());
                   }
                 }
@@ -135,81 +113,12 @@ class Staff extends ModTemplate {
 
     receiveRegisterTransaction(tx, sql) {
         let publickey = tx.publicKey;
-        console.log("pubkey" + publickey);
         let params = {
             $publickey: publickey
         }
 
         this.app.storage.executeDatabase(sql, params, "staff");
-
-        //unpack transaction
-        // save into sql db.
     }
-    /* 
-
-    async addRecord(publicKey = "") {
-
-        let sql = `INSERT INTO staff (publicKey) VALUES ($publicKey)`;
-        let params = {
-            $publicKey: publicKey
-        }
-
-        await this.app.storage.executeDatabase(sql, params, "staff");
-
-        sql = "SELECT * FROM staff WHERE publicKey = $publicKey";
-
-        let rows = this.app.storage.queryDatabase(sql, params, "staff");
-        if (rows.length <= 0) {
-            alert(rows);
-            alert("not found in db");
-            return 0;
-        } else if (rows.length > 0) {
-            let text = "";
-            alert(rows);
-            //  document.getElementById("#list_of_keys") = text;
-            for (let index = 0; index < rows.length; index++) {
-                alert("value: " + rows.length);
-                //  text += (index + 1) + ":" + rows[index].publicKey + "<br>";
-            }
-            alert("found in db");
-            return 1;
-        } else {
-            alert("db not called");
-        }
-
-    }
-
-    async registerToDatabase(publicKey) {
-        //alert("went here");
-        let am = this.app.modules.returnActiveModule();
-        if (am == null) {
-            console.log("no active module");
-            return;
-        } else {
-            this.addRecord(publicKey);
-        }
-
-
-
-        //let sql = "SELECT * FROM records WHERE publicKey = $publicKey";
-        //alert("sql");
-
-        //am.sendPeerDatabaseRequestWithFilter("Staff", sql,
-        //    (res) => {
-        //        if (res.rows) {
-        //            if (res.rows.length > 0) {
-        //                alert("went here");
-        //                // addrecord(publicKey);
-        //            }
-        //        } else {
-        //            alert("none");
-        //        }
-        //    })
-    }
-    */
-
-
-
 }
 
 module.exports = Staff;
