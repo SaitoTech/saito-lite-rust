02b601051f1c01142a3f27174ecafa03d1f64c3b
davikstone2 <102211421+davikstone2@users.noreply.github.com>
2022-06-14 19:20:58 +0100

    Newdesign module (#633)
    * tabs
    
    * tab update, minor ui adjustments
    
    * grid fix
    
    * css update
    
    * feat: new saito-header and class redesign
    
    * fix: minor update
    
    * small changes to html and boxes
    
    * update
    
    * update
    
    * example tab populated
    
    * examples tab populated
    
    * html and other cleanup
    
    * update
    
    * updates
    
    * css updates
    
    * examples tab
    
    * fix pre
    
    * form-element css updates
    
    * right sidebar, components tab
    
    * remove compile-e
    
    * update
    
    * leftsidebar mobile
    
    * updates
    
    * updates
    
    Co-authored-by: David Lancashire <david.lancashire@gmail.com>
    Co-authored-by: arpee <richard@saito.tech>

--- mods/newdesign/lib/main/newdesign-main.js
+++ mods/newdesign/lib/main/newdesign-main.js
@@ -1,21 +1,83 @@
 const NewDesignMainTemplate = require('./newdesign-main.template');
 
-module.exports = {
+class NewDesignMain {
+
+  constructor(app) {
+  };
 
   render(app) {
     if (!document.querySelector('.container')) {
       app.browser.addElementToDom(NewDesignMainTemplate(app));
     }
-
-    this.attachEvents()
-
-
-  },
+    this.attachEvents();
+  };
 
   attachEvents() {
-    const elem = document.querySelector('input[name="datepicker"]');
-    const datepicker = new Datepicker(elem, {
-    })
-  }
 
+    // Date picker
+    const elems = document.querySelectorAll('input[name="datepicker"]');
+
+    elems.forEach(elem => {
+      let datepicker = new Datepicker(elem, {});
+    })
+
+
+
+
+
+    // UserDrop Down
+    const icons = document.querySelectorAll('.saito-user > i');
+    const dropdowns = document.querySelectorAll('.saito-user-dropdown')
+
+    icons.forEach(icon => {
+      icon.addEventListener('click', (e) => {
+        console.log(icon.nextElementSibling);
+        const dropdown = icon.nextElementSibling;
+        if (dropdown.classList.contains('show')) {
+          dropdown.classList.remove('show');
+        } else {
+          dropdown.classList.add('show');
+        }
+
+      });
+    })
+    dropdowns.forEach(dropdown => {
+      dropdown.addEventListener('click', (e) => {
+        dropdown.classList.remove('show');
+      })
+    })
+
+    // functions
+    const salert_btn = document.querySelector('#salert_btn');
+    const sprompt_btn = document.querySelector('#sprompt_btn');
+    const sconfirm_btn = document.querySelector('#sconfirm_btn');
+    const sitemsg_btn = document.querySelector('#sitemsg_btn');
+
+    salert_btn.addEventListener('click', (e) => {
+      salert("Clicked Successfully");
+    })
+    sprompt_btn.addEventListener('click', (e) => {
+      sprompt("Please insert something");
+    })
+    sconfirm_btn.addEventListener('click', (e) => {
+      sconfirm("Activity Confirmed");
+    })
+    sitemsg_btn.addEventListener('click', (e) => {
+      siteMessage("New site message");
+    })
+
+    // hamburger menu
+    const mobileHamburger = document.querySelectorAll('.saito-left-sidebar-hamburger')
+    mobileHamburger.forEach(item => {
+      item.addEventListener('click', (e) => {
+        console.log(item.parentElement);
+        const mobileSidebar = item.parentElement.querySelector('.saito-left-sidebar-mobile');
+        mobileSidebar.classList.contains('show-left-sidebar-mobile') ? mobileSidebar.classList.remove('show-left-sidebar-mobile') : mobileSidebar.classList.add('show-left-sidebar-mobile');
+      })
+    })
+
+  };
 }
+
+module.exports = NewDesignMain;
+
