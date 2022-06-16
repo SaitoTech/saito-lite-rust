d7ff9d4e1f6763a74ae4ca8f1f221e1a31b71feb
Umair Khan <umair@saito.tech>
2022-06-16 20:55:03 +0500

    Readme file added for league module

---
+++ mods/league/README.md
@@ -0,0 +1,14 @@
+# Saito League Module
+
+Leaderboards and leagues for Saito Games
+
+Players can create private and public league to compete against each other for ranking on leaderboard.
+
+
+
+----
+
+
+### Contributors :
+- @umairkhannn
+- @trevelyan
--- mods/league/lib/main/league-main.template.js
+++ mods/league/lib/main/league-main.template.js
@@ -4,7 +4,7 @@ module.exports = (app, mod) => {
   let html = `
     <div class="league-main-wrapper">
       <div id="league-main-header" class="saito-contentbox saito-box-shadow mb-2">
-        <h2>Create a league</h2>
+        <h2>Saito League</h2>
         <p>Create leagues to compete against other players, through rankings, and win prizes</p>
       </div>
       
