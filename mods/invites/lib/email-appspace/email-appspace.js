25b19db05c3537be2b3b871e128a893e517524aa
David Lancashire <david.lancashire@gmail.com>
2022-06-24 14:56:17 +0700

    fix: UI refactor for redsquare merge

---
+++ mods/invites/lib/email-appspace/email-appspace.js
@@ -0,0 +1,80 @@
+const InvitesEmailAppspaceTemplate = require('./email-appspace.template.js');
+const jsonTree = require('json-tree-viewer');
+
+class InvitesEmailAppspace {
+
+  constructor(app) {
+  }
+
+  render(app, mod, container = "") {
+
+    if (!document.querySelector(".invite-email-appspace")) {
+      app.browser.addElementToClass(new InvitesEmailAppspaceTemplate(app, mod), ".email-appspace");
+    }
+
+    try {
+      var tree = jsonTree.create(app.options.invites, document.getElementById("invites"));
+    } catch (err) {
+      console.log("error creating jsonTree: " + jsonTree);
+    }
+
+    let actions = '';
+    for (let i = 0; i < mod.invites.length; i++) {
+      let inv = mod.invites[i];
+      if (inv.transaction.msg.request === "open" || inv.transaction.msg.request === "invite") {
+        actions += `<div class="action_link action_link_invite" data-id="${i}" id="action_link_${i}">accept ${i}</div>`;
+      }
+      if (inv.transaction.msg.request === "accept" || inv.transaction.msg.request === "accept") {
+        actions += `<div class="action_link action_link_accept" data-id="${i}" id="action_link_${i}">cancel ${i}</div>`;
+      }
+    }
+    document.getElementById("actions").innerHTML = actions;
+
+    this.attachEvents(app, mod);
+
+  }
+
+
+
+  attachEvents(app, mod) {
+
+      //
+      // button to initiate invites
+      //
+      document.getElementById("invite_btn").onclick = (e) => {
+
+	let address = document.getElementById("invite_address").value;
+
+	alert("Clicked w/ address: " + address);
+
+	let newtx = app.wallet.createUnsignedTransaction(address);
+	newtx.transaction.msg = {
+	  module : mod.name ,
+	  request : "open" ,
+	}
+	newtx = app.wallet.signTransaction(newtx);
+	app.network.propagateTransaction(newtx);
+
+	let relay_mod = app.modules.returnModule("Relay");
+	if (relay_mod) {
+          relay_mod.sendRelayMessage([address, app.wallet.returnPublicKey()], "invites open", newtx);
+	}
+
+      }
+
+      //
+      // buttons to respond
+      //
+      document.querySelectorAll(".action_link_invite").forEach((el) => {
+alert("accept");
+      });
+      document.querySelectorAll(".action_link_accept").forEach((el) => {
+alert("cancel");
+      });
+
+  }
+
+}
+
+module.exports = InvitesEmailAppspace;
+
---
+++ mods/invites/lib/email-appspace/email-appspace.template.js
@@ -0,0 +1,32 @@
+module.exports = InvitesEmailAppspaceTemplate = () => {
+  return `
+  
+  <h3>Invite Display:</h3> 
+
+  <p></p>
+
+  <div class="invites" id="invites"></div>
+
+  <p></p>
+
+  <h3>Invite Actions:</h3> 
+
+  <p></p>
+
+  <div class="actions" id="actions"></div>
+
+  <p></p>
+
+  <div class="invites" id="invites"></div>
+
+  <p></p>
+
+  <input type="text" class="invite_address" id="invite_address" />
+
+  <p></p>
+
+  <div class="button invite_btn" id="invite_btn">Send Invite</div>
+
+  `;
+
+}
