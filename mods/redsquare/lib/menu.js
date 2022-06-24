5f48f2956beb44a47f42302c2dbb4df5e5b6332c
David Lancashire <david.lancashire@gmail.com>
2022-06-24 16:14:57 +0700

    fix: removing section, updating invites

--- mods/redsquare/lib/menu.js
+++ mods/redsquare/lib/menu.js
@@ -12,7 +12,21 @@ class RedSquareMenu {
       app.browser.addElementToClass(RedSquareMenuTemplate(app, mod), container);
     }
 
+    
+
+    this.attachEvents(app, mod);
+
   }
+
+  attachEvents(app, mod) {
+
+    let obj = document.querySelector('.redsquare-menu-invites');
+    obj.onclick = (e) => {
+      document.querySelector(".email-appspace").innerHTML = "";
+      app.modules.returnModule("Invites").respondTo("email-appspace").render(app, this);
+    }
+
+  } 
 
 }
 
