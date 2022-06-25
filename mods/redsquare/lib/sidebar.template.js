e07d9c8e9d92c1cfc365b98cc14aaf447f9b977b
davikstone2 <onyeji@saito.tech>
2022-06-25 09:42:02 +0100

    update right sidebar

--- mods/redsquare/lib/sidebar.template.js
+++ mods/redsquare/lib/sidebar.template.js
@@ -6,28 +6,12 @@ module.exports = RedSquareMenuTemplate = (app, mod) => {
       <div class="saito-search-bar">
         <i class="fas fa-search"></i> <input type="text" placeholder="Search on Saito" />
       </div>
-      <div id="redsquare-trends-container">
-       <h5> Trends for you </h5>
-         <div>
-             <p> Gaming </p>
-             <h6> Settlers </h6>
-             <p> 5k chirps </p>
-         </div>
-         <div>
-             <p> Technology </p>
-             <h6> Saitozens </h6>
-             <p> 16k chirps </p>
-         </div>
-         <div>
-             <p> Business & Finance </p>
-             <h6> David Lanchashire </h6>
-             <p> 6.7k chirps </p>
-         </div>
+      <div class="redsquare-calendar">
       </div>
       <div id="redsquare-follow-container">
        <h5> Who to follow </h5>
        <div>
-          <div class="saito-item-list circle">
+          <div class="saito-item-list">
           <div>
           <img src="/redsquare/images/david.jpeg" />
           <div class="item-info">
@@ -59,3 +43,23 @@ module.exports = RedSquareMenuTemplate = (app, mod) => {
 
 }
 
+
+
+{/* <div id="redsquare-trends-container">
+<h5> Trends for you </h5>
+  <div>
+      <p> Gaming </p>
+      <h6> Settlers </h6>
+      <p> 5k chirps </p>
+  </div>
+  <div>
+      <p> Technology </p>
+      <h6> Saitozens </h6>
+      <p> 16k chirps </p>
+  </div>
+  <div>
+      <p> Business & Finance </p>
+      <h6> David Lanchashire </h6>
+      <p> 6.7k chirps </p>
+  </div>
+</div> */}
