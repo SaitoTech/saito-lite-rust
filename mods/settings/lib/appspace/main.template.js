344cedffa83b79e81dbc8aa71ab87c7a3c9f8cb7
davikstone2 <onyeji@saito.tech>
2022-07-04 14:46:39 +0100

    update settings css

--- mods/settings/lib/appspace/main.template.js
+++ mods/settings/lib/appspace/main.template.js
@@ -7,25 +7,25 @@ module.exports = SettingsAppspaceTemplate = (app) => {
 
   let modules_html = "Wallet Outdated - module selection not supported";
   try {
-  modules_html = app.options.modules
-    .map((mod, i) => {
-      let CHECKED = mod.active ? 'CHECKED': '';
-      return `
+    modules_html = app.options.modules
+      .map((mod, i) => {
+        let CHECKED = mod.active ? 'CHECKED' : '';
+        return `
         <div class="settings-app-select">
+        <input
+        type="checkbox"
+        value="modules_mods_${i}"
+        class="modules_mods_checkbox"
+        name="modules_mods_${i}" id="${i}" ${CHECKED}/>
           <label class="s-container">${mod.name}
-            <input
-              type="checkbox"
-              value="modules_mods_${i}"
-              class="modules_mods_checkbox"
-              name="modules_mods_${i}" id="${i}" ${CHECKED}/>
             <span class="s-checkmark"></span>
           </label>
          </div>
       `;
-    })
-    .join('');
+      })
+      .join('');
   } catch (err) {
-    if(err.message.startsWith("Cannot read property 'map'")) {
+    if (err.message.startsWith("Cannot read property 'map'")) {
       modules_html = "Initialization error. Refresh page should fix this.";
     } else {
       modules_html = `Unknown error<br/>${err}`;
@@ -50,32 +50,35 @@ module.exports = SettingsAppspaceTemplate = (app) => {
 
       <div class="settings-appspace-wallet-management">
 
-        <div class="settings-buttons">
+        <div class="saito-grid-1-1-1">
           <button id="backup-account-btn" class="backup-account-btn"">Backup Wallet</button>
           <button id="restore-account-btn" class="restore-account-btn">Restore Wallet</button>
           <button id="restore-privatekey-btn" class="restore-privatekey-btn">Import Private Key</button>
-          <input id="file-input" class="file-input" type="file" name="name" style="display:none;" />
+         
         </div>
 
+        <div class="saito-grid-1">
+        <button class="small"><input id="file-input" class="file-input" type="file" name="name" style="display:none;" /></button>
+        </div>
         ${balance_link}
 
 
-        <div class="grid-2">
+        <div class="saito-grid-1-1">
 
-          <div>Email:</div>
-          <div>${email_registered}</div>
+          <p class="saito-large-paragraph">Email:</p>
+          <p>${email_registered}</p>
 
-          <div>Username:</div>
-          <div>${identifier_registered}</div>
+          <p class="saito-large-paragraph">Username:</p>
+          <p>${identifier_registered}</p>
 
-          <div class="public-key">Public Key:</div>
-          <div>${app.wallet.returnPublicKey()}</div>
+          <p class="public-key saito-large-paragraph">Public Key:</p>
+          <p>${app.wallet.returnPublicKey()}</p>
 
-          <div>Private Key:</div>
-          <div class="private-key">
+          <p class="saito-large-paragraph">Private Key:</p>
+          <p class="private-key">
             <input id="privatekey" type="text" value="${app.wallet.returnPrivateKey()}" class="password" />
             <i class="see-password fas fa-eye" id="see-password"></i>
-          </div>
+          </p>
 
 	</div>
 
@@ -85,11 +88,11 @@ module.exports = SettingsAppspaceTemplate = (app) => {
 
         <h3>Installed Modules: `;
 
-      if (app.modules.returnModule("AppStore") != null) {
-	html += ` &nbsp; [<span id="trigger-appstore-btn" class="trigger-appstore-btn">&nbsp;install more&nbsp;</span>]`;
-      }
+  if (app.modules.returnModule("AppStore") != null) {
+    html += ` &nbsp; [<span id="trigger-appstore-btn" class="trigger-appstore-btn">&nbsp;install more&nbsp;</span>]`;
+  }
 
-      html += `</h2>
+  html += `</h2>
         <div class="settings-app-list">
         ${modules_html}
         </div>
@@ -98,16 +101,16 @@ module.exports = SettingsAppspaceTemplate = (app) => {
       <div id="settings-appspace" class="settings-appspace"></div>
 
       <hr />
-      <div class="grid-4">
-        <div>Code Version:</div>
-        <div>${app.wallet.wallet.version}</div>
-        <div>Wallet Version:</div>
-        <div>${app.options.wallet.version}</div>
+      <div class="saito-grid-1-1-1-1">
+        <p class="saito-large-paragraph">Code Version:</p>
+        <p class="saito-mid-paragraph">${app.wallet.wallet.version}</p>
+        <p class="saito-large-paragraph">Wallet Version:</p>
+        <p class="saito-mid-paragraph">${app.options.wallet.version}</p>
       </div>
 
-      <div style="padding-bottom:40px;">
+      <p style="padding-bottom:40px;">
         Having trouble? Try clearing your browser cache or <span style="cursor:pointer; border-bottom: 1px dashed" id="reset-account-btn" class="">resetting your account</span>. If that doesn't fix things, write to us at info@saito.tech.
-      </div>
+      </p>
 
     </div>
   </div>
