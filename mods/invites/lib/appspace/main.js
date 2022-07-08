fd1b2a2bdc1d18a3f8985578904f7ee74dc91797
davikstone2 <onyeji@saito.tech>
2022-07-08 01:38:20 +0100

    update

--- mods/invites/lib/appspace/main.js
+++ mods/invites/lib/appspace/main.js
@@ -20,17 +20,17 @@ class InvitesAppspace {
 
       for (let i = 0; i < mod.invites.length; i++) {
         if (mod.isPendingMe(mod.invites[i], app.wallet.returnPublicKey())) {
-	  let qs = `#invites-invitation-${i} > invites-invitation-accept`;
-	  document.querySelectorAll(qs).forEach((el) => {
-	    el.style.display = "none";
-	  });
-	}
+          let qs = `#invites-invitation-${i} > invites-invitation-accept`;
+          document.querySelectorAll(qs).forEach((el) => {
+            el.style.display = "none";
+          });
+        }
         if (mod.isPendingOthers(mod.invites[i], app.wallet.returnPublicKey())) {
-	  let qs = `#invites-invitation-${i} > invites-invitation-accept`;
-	  document.querySelectorAll(qs).forEach((el) => {
-	    el.style.display = "none";
-	  });
-	}
+          let qs = `#invites-invitation-${i} > invites-invitation-accept`;
+          document.querySelectorAll(qs).forEach((el) => {
+            el.style.display = "none";
+          });
+        }
       }
     }
 
@@ -44,30 +44,66 @@ class InvitesAppspace {
     //
     // button to initiate invites
     //
-    document.getElementById("invite_btn").onclick = (e) => {
+    // document.getElementById("invite_btn").onclick = (e) => {
 
-      let recipient = document.getElementById("invite_address").value;
-      if (recipient === "") { recipient = app.wallet.returnPublicKey(); }
+    //   let recipient = document.getElementById("invite_address").value;
+    //   if (recipient === "") { recipient = app.wallet.returnPublicKey(); }
 
-      mod.createOpenTransaction(recipient, { from : app.wallet.returnPublicKey() , to : recipient });
+    //   mod.createOpenTransaction(recipient, { from: app.wallet.returnPublicKey(), to: recipient });
 
-    }
+    // }
 
     //
     // buttons to respond
     //
-    document.querySelectorAll(".invites-invitation-accept").forEach((el) => {
-      el.onclick = (e) => {
-	let index = el.getAttr("data-id");
-        alert("accept: " + index);
+    // document.querySelectorAll(".invites-invitation-accept").forEach((el) => {
+    //   el.onclick = (e) => {
+    //     let index = el.getAttr("data-id");
+    //     alert("accept: " + index);
+    //   }
+    // });
+    // document.querySelectorAll(".invites-invitation-cancel").forEach((el) => {
+    //   el.onclick = (e) => {
+    //     let index = el.getAttr("data-id");
+    //     alert("cancel: " + index);
+    //   }
+    // });
+
+    const elems = document.querySelectorAll('input[name="datepicker"]');
+
+    elems.forEach(elem => {
+      let datepicker = new Datepicker(elem, {});
+    })
+
+    // expand height 
+    // const additionalInfoButtonContainer = document.querySelector('.invites-appspace-additional-info');
+    // const additionalInfoButton = document.querySelector('.invites-appspace-additional-info i');
+    // const containerId = additionalInfoButtonContainer.getAttribute('data-target');
+    // const container = document.querySelector(containerId)
+
+    // additionalInfoButton.addEventListener('click', () => {
+    //   if (!additionalInfoButton.classList.contains('rotate')) {
+    //     container.style.maxHeight = "200rem";
+    //     additionalInfoButton.classList.add('rotate');
+    //   }
+    //   else {
+    //     container.style.maxHeight = "22rem";
+    //     additionalInfoButton.classList.remove('rotate');
+
+    //   }
+    // })
+
+    document.body.addEventListener('click', (e) => {
+      if (e.target.classList.contains("close-overlay")) {
+        document.querySelector('.saito-overlay').classList.remove('show');
       }
-    });
-    document.querySelectorAll(".invites-invitation-cancel").forEach((el) => {
-      el.onclick = (e) => {
-	let index = el.getAttr("data-id");
-        alert("cancel: " + index);
+
+      if (e.target.classList.contains("invites-appspace-create-invite")) {
+        document.querySelector('.saito-overlay').classList.add('show');
       }
-    });
+
+    })
+
 
   }
 
--- mods/invites/lib/appspace/main.template.js
+++ mods/invites/lib/appspace/main.template.js
@@ -1,66 +1,209 @@
 module.exports = InvitesAppspaceTemplate = (app, mod) => {
 
   return `
-  
+  <div class="saito-main">
   <h3>Invitations:</h3> 
-
-  <p></p>
-
   <div class="invites-appspace">
-
-<div class="saito-list">
-    <div class="invites-appspace-event">
-
-  <div class="" style="width:100%;border: 1px solid grey;padding:1rem"> 
-
-    <div style="float:right">August 31 2022, 4:31 PM</div><h6>Twilight Struggle - League Match</h6>
-    
-
-      <div class="saito-userbox">
-        <div class="saito-list-user-content-box">
-  <div class="saito-list-user-image-box">
-    <img class="saito-identicon" src="/saito/img/background.png">
-  </div>
-          <div class="saito-username">david@saito</div>
-  	  <p>status: ACCEPTED</p>
-        </div>
+  <div>
+  <div class="invites-appspace-event">
+      <div class="invites-appspace-invite" id="invites-appspace-invite">
+          <div class="invites-appspace-date"> 7 July 2022 </div>
+          <!-- <p class="invites-appspace-additional-info" data-target="#invites-appspace-invite">
+              <i class="fas fa-chevron-circle-down"></i>
+          </p> -->
+          <span class=" invites-appspace-advancedoption-button"></span>
+          <div class="invites-appspace-section">
+              <div class="invites-appspace-icon">
+                  <i class="fas fa-circle"></i>
+              </div>
+              <div class="invites-appspace-header">
+                  <h6>
+                      WORDBLOCKS
+                  </h6>
+              </div>
+          </div>
+          <div class="invites-appspace-section">
+              <div>
+                  <i class="fas fa-user-plus invites-appspace-section-icon"></i>
+              </div>
+              <div class="saito-list horizontal">
+                  <div class="saito-list-user">
+                      <div class="saito-list-user-image-box">
+                          <img class="saito-idenitcon" src="/saito/img/david.jpeg" />
+                      </div>
+                      <div class="saito-list-user-content-box">
+                          <div class="saito-username">david@saito.io</div>
+                          <p>status: <span class="saito-green-color">Accepted </span>
+                          </p>
+                      </div>
+                  </div>
+                  <div class="saito-list-user">
+                      <div class="saito-list-user-image-box">
+                          <img class="saito-idenitcon" src="/saito/img/richard.jpeg" />
+                      </div>
+                      <div class="saito-list-user-content-box">
+                          <div class="saito-username">Richard Parris</div>
+                          <p>status: PENDING</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="invites-appspace-section ">
+              <div>
+                  <i class="fas fa-paperclip invites-appspace-section-icon"></i>
+              </div>
+              <div>
+                  <p class="invites-appspace-link">Link</p>
+              </div>
+          </div>
       </div>
-
-<div class="saito-userbox">
-        <div class="saito-list-user-content-box">
-  <div class="saito-list-user-image-box">
-    <img class="saito-identicon" src="/saito/img/background.png">
-  </div>
-          <div class="saito-username">richard@saito</div>
-          <p>status: PENDING</p>
-        </div>
+      <div class="invites-appspace-actions">
+          <div class="saito-button-secondary">Accept</div>
       </div>
-    
-      <div class="invites-button-accept saito-button-secondary">Accept</div>
-      <div class="invites-button-modify saito-button-secondary">Modify</div>
-      <div class="invites-button-cancel saito-button-secondary">Cancel</div>
-
   </div>
-    </div>
-
-  <div class="invites-json-tree" id="invites-json-tree"><ul class="jsontree_tree clearfix"><li class="jsontree_node jsontree_node_complex jsontree_node_expanded jsontree_node_empty">                        <div class="jsontree_value-wrapper">                            <div class="jsontree_value jsontree_value_array">                                <b>[</b>                                <span class="jsontree_show-more">…</span>                                <ul class="jsontree_child-nodes"></ul>                                <b>]</b></div></div></li></ul></div>
-
-  <p></p>
-
-  <div class="invites" id="invites"></div>
-
-  <p></p>
-
-  <h3>Create New Invitation</h3>
-
-  <input type="text" class="invite_address" id="invite_address">
-
-  <p></p>
-
-  <div class="saito-button-primary invite_btn" id="invite_btn">Send Invite</div>
-
+  <div class="invites-appspace-event">
+      <div class="invites-appspace-invite" id="invites-appspace-invite">
+          <div class="invites-appspace-date"> 7 July 2022 </div>
+          <!-- <p class="invites-appspace-additional-info" data-target="#invites-appspace-invite">
+              <i class="fas fa-chevron-circle-down"></i>
+          </p> -->
+          <span class=" invites-appspace-advancedoption-button"></span>
+          <div class="invites-appspace-section">
+              <div class="invites-appspace-icon">
+                  <i class="fas fa-circle"></i>
+              </div>
+              <div class="invites-appspace-header">
+                  <h6>
+                      VIDEO CALL
+                  </h6>
+              </div>
+          </div>
+          <div class="invites-appspace-section">
+              <div>
+                  <i class="fas fa-user-plus invites-appspace-section-icon"></i>
+              </div>
+              <div class="saito-list horizontal">
+                  <div class="saito-list-user">
+                      <div class="saito-list-user-image-box">
+                          <img class="saito-idenitcon" src="/saito/img/david.jpeg" />
+                      </div>
+                      <div class="saito-list-user-content-box">
+                          <div class="saito-username">david@saito.io</div>
+                          <p>status: <span class="saito-green-color">Online </span>
+                          </p>
+                      </div>
+                  </div>
+                  <div class="saito-list-user">
+                      <div class="saito-list-user-image-box">
+                          <img class="saito-idenitcon" src="/saito /img/richard.jpeg" />
+                      </div>
+                      <div class="saito-list-user-content-box">
+                          <div class="saito-username">Richard Parris</div>
+                          <p>status: PENDING</p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="invites-appspace-section ">
+              <div>
+                  <i class="fas fa-paperclip invites-appspace-section-icon"></i>
+              </div>
+              <div>
+                  <p class="invites-appspace-link"> Link</p>
+              </div>
+          </div>
+      </div>
+      <div class="invites-appspace-actions">
+          <div class="saito-button-secondary">Accept</div>
+      </div>
   </div>
-
+</div>
+<div class="">
+  <button class="invites-appspace-create-invite">Create Invite</button>
+</div>
+        <div class="saito-overlay  close-overlay">
+          <div class="saito-backdrop">
+            <!-- <i class="close-overlay fas fa-times"> </i> -->
+            <div>
+              <div class="invites-appspace-create">
+                <div class="invites-appspace-invite" id="invites-appspace-invite">
+                  <div class="invites-appspace-date"> 7 July 2022 </div>
+                 
+                  <span class=" invites-appspace-advancedoption-button">advanced options</span>
+                  <div class="invites-appspace-section">
+                    <div class="invites-appspace-icon">
+                      <i class="fas fa-circle"></i>
+                    </div>
+                    <div class="invites-appspace-header">
+                      <h6>
+                        WORDBLOCKS
+                      </h6>
+                    </div>
+                  </div>
+                  <div class="invites-appspace-section">
+                    <div>
+                      <i class="fas fa-user-plus invites-appspace-section-icon"></i>
+                    </div>
+                    <div class="saito-list horizontal">
+                      <div class="saito-list-user">
+                        <div class="saito-list-user-image-box">
+                          <img class="saito-idenitcon"  src="/saito/img/background.png" />
+                        </div>
+                        <div class="saito-list-user-content-box">
+                          <div class="saito-username">david@saito.io</div>
+                          <p>status: <span class="saito-green-color">Online </span>
+                          </p>
+                        </div>
+                      </div>
+                      <div class="saito-list-user invites-appspace-adduser-box">
+                        <i class="fas fa-plus-circle"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="invites-appspace-section ">
+                    <div>
+                      <i class="fas fa-user-friends invites-appspace-section-icon"></i>
+                    </div>
+                    <div>
+                      <select class="saito-new-select">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="invites-appspace-section ">
+                    <div>
+                      <i class="fas fa-paperclip invites-appspace-section-icon"></i>
+                    </div>
+                    <div>
+                      <input placeholder="Additional Info" type="text" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="saito-overlay-actions">
+                <div class="saito-button-secondary">Create</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="invites-json-tree" id="invites-json-tree">
+          <ul class="jsontree_tree clearfix">
+            <li class="jsontree_node jsontree_node_complex jsontree_node_expanded jsontree_node_empty">
+            </li>
+          </ul>
+        </div>
+        <p></p>
+        <div class="invites" id="invites"></div>
+        <p></p>
+      </div>
+</div>
   `;
 
 }
+
+
+{/* <p class="invites-appspace-additional-info" data-target="#invites-appspace-invite"><i
+class="fas fa-chevron-circle-down"></i></p> */}
