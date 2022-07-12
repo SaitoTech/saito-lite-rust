ba01e477c4ee2512fb7343c2b18a5987c02945b1
arpee <richard@saito.tech>
2022-07-13 01:25:29 +0800

    css and template updates/fixes to redsquare

--- mods/redsquare/lib/sidebar.template.js
+++ mods/redsquare/lib/sidebar.template.js
@@ -2,75 +2,73 @@
 module.exports = RedSquareSidebarTemplate = (app, mod) => {
 
   return `
-<div class="saito-sidebar right">
 
-<div class="redsquare-sidebar-calendar">
-</div>
+  <div class="saito-sidebar right">
 
-    <div class="saito-leaderboard">
-<h6 style="
-    font-size: 2rem;
-    padding-bottom: 0.5rem;
-">Your Rankings:</h6>
-<div class="saito-leaderboard-row odd">
-  <div class="saito-leaderboard-gamename">Twilight Struggle</div>
-  <div class="saito-leaderboard-rank">4</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
-<div class="saito-leaderboard-row">
-  <div class="saito-leaderboard-gamename">Red Imperium</div>
-  <div class="saito-leaderboard-rank">97</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
-<div class="saito-leaderboard-row odd">
-  <div class="saito-leaderboard-gamename">Spider Solitaire</div>
-  <div class="saito-leaderboard-rank">1435</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
-<div class="saito-leaderboard-row">
-  <div class="saito-leaderboard-gamename">Settlers of Saitoa</div>
-  <div class="saito-leaderboard-rank">352</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
-<div class="saito-leaderboard-row odd">
-  <div class="saito-leaderboard-gamename">Pandemic</div>
-  <div class="saito-leaderboard-rank">4242</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
-<div class="saito-leaderboard-row">
-  <div class="saito-leaderboard-gamename">Blackjack</div>
-  <div class="saito-leaderboard-rank">2222</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
-<div class="saito-leaderboard-row odd">
-  <div class="saito-leaderboard-gamename">Chess</div>
-  <div class="saito-leaderboard-rank">1283</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
-<div class="saito-leaderboard-row">
-  <div class="saito-leaderboard-gamename">Poker</div>
-  <div class="saito-leaderboard-rank">924</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
-<div class="saito-leaderboard-row odd">
-  <div class="saito-leaderboard-gamename">Solitrio Solitaire</div>
-  <div class="saito-leaderboard-rank">63</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
-<div class="saito-leaderboard-row">
-  <div class="saito-leaderboard-gamename">Wordblocks</div>
-  <div class="saito-leaderboard-rank">1452</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
-<div class="saito-leaderboard-row odd">
-  <div class="saito-leaderboard-gamename">Wuziqi</div>
-  <div class="saito-leaderboard-rank">843</div>
-  <div class="saito-leaderboard-change">+7</div>
-</div>
+  <div class="redsquare-sidebar-calendar">
+  </div>
 
+  <div class="saito-leaderboard">
+      <h6>Your Rankings:</h6>
+      <div class="saito-leaderboard-row odd">
+          <div class="saito-leaderboard-gamename">Twilight Struggle</div>
+          <div class="saito-leaderboard-rank">4</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+      <div class="saito-leaderboard-row">
+          <div class="saito-leaderboard-gamename">Red Imperium</div>
+          <div class="saito-leaderboard-rank">97</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+      <div class="saito-leaderboard-row odd">
+          <div class="saito-leaderboard-gamename">Spider Solitaire</div>
+          <div class="saito-leaderboard-rank">1435</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+      <div class="saito-leaderboard-row">
+          <div class="saito-leaderboard-gamename">Settlers of Saitoa</div>
+          <div class="saito-leaderboard-rank">352</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+      <div class="saito-leaderboard-row odd">
+          <div class="saito-leaderboard-gamename">Pandemic</div>
+          <div class="saito-leaderboard-rank">4242</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+      <div class="saito-leaderboard-row">
+          <div class="saito-leaderboard-gamename">Blackjack</div>
+          <div class="saito-leaderboard-rank">2222</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+      <div class="saito-leaderboard-row odd">
+          <div class="saito-leaderboard-gamename">Chess</div>
+          <div class="saito-leaderboard-rank">1283</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+      <div class="saito-leaderboard-row">
+          <div class="saito-leaderboard-gamename">Poker</div>
+          <div class="saito-leaderboard-rank">924</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+      <div class="saito-leaderboard-row odd">
+          <div class="saito-leaderboard-gamename">Solitrio Solitaire</div>
+          <div class="saito-leaderboard-rank">63</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+      <div class="saito-leaderboard-row">
+          <div class="saito-leaderboard-gamename">Wordblocks</div>
+          <div class="saito-leaderboard-rank">1452</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+      <div class="saito-leaderboard-row odd">
+          <div class="saito-leaderboard-gamename">Wuziqi</div>
+          <div class="saito-leaderboard-rank">843</div>
+          <div class="saito-leaderboard-change">+7</div>
+      </div>
+  </div>
 
   <div id="redsquare-follow-container">
-      <h5> Who to follow </h5>
+      <h6> Who to follow </h6>
       <div>
           <div class="saito-list">
               <div class="saito-list-user">
