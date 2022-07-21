0ef9b02d2a44e9e3c6c3691647dbdcc550632f55
Daniel Worlton <f8daniel@gmail.com>
2022-07-21 12:15:43 +0700

    enhance League: basic admin functions

--- mods/league/lib/components/existing-league.template.js
+++ mods/league/lib/components/existing-league.template.js
@@ -10,13 +10,14 @@ module.exports = (app, mod, league) => {
         
     `;
 
-    if (app.wallet.returnPublicKey() == league.admin) { 
-     html += `<button class="league-component-existing-league" data-cmd="invite" data-league-id="${league.id}">Invite</button>`;
-    }
     if ((league.myRank == undefined || league.myRank < 0) && (league.max_players == 0 || league.playerCnt < league.max_players)){
      html +=`<button class="league-component-existing-league" data-cmd="join" data-league-id="${league.id}">Join</button>`; 
     }
     html +=`<button class="league-component-existing-league" data-cmd="view" data-league-id="${league.id}">View</button>`;
+    if (app.wallet.returnPublicKey() == league.admin) { 
+     html += `<button class="league-component-existing-league" data-cmd="invite" data-league-id="${league.id}">Invite</button>`;
+     html += `<button class="league-component-existing-league" data-cmd="delete" data-league-id="${league.id}">Delete</button>`;
+    }
 
   html += `</div>`;
 
