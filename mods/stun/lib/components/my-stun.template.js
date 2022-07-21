0459d359435e82def57d1475f63730ed5694a0e9
davikstone2 <102211421+davikstone2@users.noreply.github.com>
2022-07-21 16:31:46 +0000

    Merge Test to Staging (#795)
    * Resolve jquery  conflict in events
    
    * Add stun mod to email-appspace element
    
    * Update stun server
    
    * Change in stun and turn servers
    
    * Change stun servers to google
    
    * forced wallet upgrade
    
    * update
    
    * stun update
    
    * update
    
    * update
    
    * updatew
    
    * updatew
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update stun ui
    
    * update stun ui
    
    * fix stun and add turn servers
    
    * update
    
    * update
    
    * update
    
    Co-authored-by: Umair Khan <umair@saito.tech>
    Co-authored-by: Daniel Worlton <70703104+f8daniel@users.noreply.github.com>
    Co-authored-by: Daniel Worlton <f8daniel@gmail.com>

--- mods/stun/lib/components/my-stun.template.js
+++ mods/stun/lib/components/my-stun.template.js
@@ -18,10 +18,12 @@ module.exports = (app, mod, listeners) => {
                 <div class="my-stun-container-actions">
                 <div class="my-stun-container-create">
                 <p> Create </p>
-                <button id="createInvite" class="saito-button-secondary"> Create Invite:</button>
+
+                <button id="createInvite" class="saito-button-secondary"> Create Invite</button>
                 </div>
                 <div class="my-stun-container-join">
-                   <p> Join an invite:</p>
+                   <p> Join an invite</p>
+
                   <div>
                   <input placeholder="Insert Room Code" id="inviteCode" />
                   <button id="joinInvite" class="saito-button-secondary">Join</button>
