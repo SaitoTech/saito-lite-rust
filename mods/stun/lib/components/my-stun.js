5be32c130526f8811769709887ff6218639a6f64
davikstone2 <102211421+davikstone2@users.noreply.github.com>
2022-07-25 13:50:55 +0000

    Stun and video module updates (#805)
    * remove jquery and other libraries
    
    * remove jquery and other libraries
    
    * fix multiple stun render

--- mods/stun/lib/components/my-stun.js
+++ mods/stun/lib/components/my-stun.js
@@ -8,38 +8,41 @@ class MyStun {
   }
 
   render(app, mod) {
-
     let publicKey = app.wallet.returnPublicKey();
     const key_index = app.keys.keys.findIndex(tk => tk.publickey === publicKey);
     let listeners = app.keys.keys[key_index].data.stun.listeners;
 
-    app.browser.addElementToDom(StunComponentMyStunTemplate(app, mod, listeners), "stun-information");
-    this.attachEvents(app, mod);
+    if (!document.querySelector(".my-stun-container")) {
+      app.browser.addElementToDom(StunComponentMyStunTemplate(app, mod, listeners), "stun-information");
+      this.attachEvents(app, mod);
+    }
+
 
   }
 
 
   attachEvents(app, mod) {
 
-    $('.stun-container').on('click', '#createInvite', function(e) {
+    document.body.addEventListener('click', (e) => {
+      if (e.target.id === "add-to-listeners-btn") {
+        let input = document.querySelector('#listeners-input').value.split(',');
+        const listeners = input.map(listener => listener.trim());
+        let stun_mod = app.modules.returnModule("Stun");
+        stun_mod.addListeners(listeners);
+      }
+
+      if (e.target.id === "createInvite") {
         let video_mod = app.modules.returnModule("Video");
         console.log(video_mod);
         video_mod.createVideoInvite();
-    });
-
-    $('.stun-container').on('click', '#joinInvite', function(e) {
+      }
+      if (e.target.id === "joinInvite") {
         let video_mod = app.modules.returnModule("Video");
         // invite code hardcoded for dev purposes
-        const inviteCode = $("#inviteCode").val();
+        const inviteCode = document.querySelector("#inviteCode").value;
         video_mod.joinVideoInvite(inviteCode.trim());
-    });
-
-    $('.stun-container').on('click', '#sendv-message-btn', (e) => {
-          if (!this.peer_connection) return console.log("Peer connection instance has not been created");
-          const text = $('#message-text').val();
-          // console.log('text message', text);
-          this.peer_connection.dc.send(text);
-    });
+      }
+    })
   }
 }
 
