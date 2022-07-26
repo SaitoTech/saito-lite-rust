2905818a6b04d71ae34259e63674961574457334
davikstone2 <onyeji@saito.tech>
2022-07-26 18:23:49 +0000

    update chat html

--- mods/chat/lib/chat-manager/popup.template.js
+++ mods/chat/lib/chat-manager/popup.template.js
@@ -4,57 +4,176 @@ module.exports = (app, mod, chat_popup_id) => {
     return `
 
   <div class="chat-container chat-popup-${chat_popup_id}" id="chat-popup-${chat_popup_id}">
-      <div>
-          <h6>Community Chat</h6>
-          <i id="chat-container-close-${chat_popup_id}" class="chat-container-close fas fa-times"></i>
+  <div class="chat-header">
+  <i class="far fa-comment-dots"></i>
+  <h5>Community Chat</h5>
+  <i id="chat-container-close-${chat_popup_id}" class="chat-container-close fas fa-times"></i>
+</div>
+<div class="chat-body">
+  <div class="chat-item other">
+      <div class="chat-time">
+          5:00am
       </div>
-      <div>
-          <div class="chat-dialog">
-              <div class="saito-list  saito-white-background  ">
-                  <div class="saito-list-chatbox">
-                      <div class="saito-list-user-image-box">
-                          <img class="saito-identicon" src="/saito/img/background.png" />
-                      </div>
-                      <div class="saito-list-user-content-box">
-                          <div class="saito-username">t4gEMWsDjZ4xeYGn2gCGxFg8ty53rs7UQKUpwfvsBq33 </div>
-                          <p> Hey how are you?</p>
-                      </div>
-                      <div class="saito-list-user-timestamp">
-                          22:00pm
-                      </div>
-                  </div>
-                  <div class="saito-list-chatbox">
-                      <div class="saito-list-user-image-box">
-                          <img class="saito-identicon" src="/saito/img/background.png" />
-                      </div>
-                      <div class="saito-list-user-content-box">
-                          <div class="saito-username">t4gEMWsDjZ4xeYGn2gCGxFg8ty53rs7UQKUpwfvsBq33 </div>
-                          <p> Hey how are you?</p>
-                      </div>
-                      <div class="saito-list-user-timestamp">
-                          22:00pm
-                      </div>
-                  </div>
-                  <div class="saito-list-chatbox">
-                      <div>
-                          <img src="/saito/img/background.png" />
-                      </div>
-                      <div>
-                          <div>t4gEMWsDjZ4xeYGn2gCGxFg8ty53rs7UQKUpwfvsBq33 </div>
-                          <p> Hey how are you?</p>
-                      </div>
-                      <div>
-                          22:00pm
-                      </div>
-                  </div>
+      <div class="chat-profile">
+          <div class="chat-profile-dropdown">
+              <div>
+                  <i class="fas fa-globe"></i>
+                  <p> Connect</p>
+              </div>
+              <div>
+                  <i class="far fa-calendar"></i>
+                  <p> Schedule Event</p>
+              </div>
+              <div>
+                  <i class="fas fa-chess-board"></i>
+                  <p> See Ranking</p>
+              </div>
+          </div>
+          <img src="/saito/img/background.png" />
+      </div>
+      <div class="chat-dialog">
+          <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </div>
+          <div>
+              Here is a link to a game i just made
+          </div>
+          <div>
+              Let's play a game
+          </div>
+      </div>
+  </div>
+  <div class="chat-item other">
+      <div class="chat-time">
+          5:00am
+      </div>
+      <div class="chat-profile">
+          <div class="chat-profile-dropdown">
+              <div>
+                  <i class="fas fa-globe"></i>
+                  <p> Connect</p>
+              </div>
+              <div>
+                  <i class="far fa-calendar"></i>
+                  <p> Schedule Event</p>
+              </div>
+              <div>
+                  <i class="fas fa-chess-board"></i>
+                  <p> See Ranking</p>
+              </div>
+          </div>
+          <img src="/saito/img/background.png" />
+      </div>
+      <div class="chat-dialog">
+          <div>
+              sure
+          </div>
+          <div>
+              Let's hope on
+          </div>
 
+      </div>
+  </div>
+  <div class="chat-item me">
+      <div class="chat-time">
+          5:00am
+      </div>
+      <div class="chat-dialog">
+          <div>
+              Let's play a game
+          </div>
+          <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum do
+          </div>
+
+      </div>
+  </div>
+
+  <div class="chat-item other">
+      <div class="chat-time">
+          5:00am
+      </div>
+      <div class="chat-profile">
+          <div class="chat-profile-dropdown">
+              <div>
+                  <i class="fas fa-globe"></i>
+                  <p> Connect</p>
+              </div>
+              <div>
+                  <i class="far fa-calendar"></i>
+                  <p> Schedule Event</p>
+              </div>
+              <div>
+                  <i class="fas fa-chess-board"></i>
+                  <p> See Ranking</p>
+              </div>
+          </div>
+          <img src="/saito/img/background.png" />
+      </div>
+      <div class="chat-dialog">
+          <div>
+              lorem ipsum dolor
+          </div>
+
+
+      </div>
+  </div>
+
+  <div class="chat-item other">
+      <div class="chat-time">
+          5:00am
+      </div>
+      <div class="chat-profile">
+          <div class="chat-profile-dropdown">
+              <div>
+                  <i class="fas fa-globe"></i>
+                  <p> Connect</p>
+              </div>
+              <div>
+                  <i class="far fa-calendar"></i>
+                  <p> Schedule Event</p>
+              </div>
+              <div>
+                  <i class="fas fa-chess-board"></i>
+                  <p> See Ranking</p>
+              </div>
+          </div>
+          <img src="/saito/img/background.png" />
+          <div class="chat-profile-dropdown">
+              <div>
+                  <i class="fas fa-globe"></i>
+                  <p> Connect</p>
+              </div>
+              <div>
+                  <i class="far fa-calendar"></i>
+                  <p> Schedule Event</p>
+              </div>
+              <div>
+                  <i class="fas fa-chess-board"></i>
+                  <p> See Ranking</p>
               </div>
           </div>
       </div>
-      <div>
-          <input type="text" placeholder="Type message" />
-          <i id="saito-sendmsg-btn" class="fas fa-paper-plane"></i>
+      <div class="chat-dialog">
+          <div>
+              sure
+          </div>
+          <div>
+              Let's hope on
+          </div>
+
       </div>
+  </div>
+
+</div>
+
+<div class="chat-footer">
+  <input type="text" placeholder="Type something..." />
+  <i class="fas fa-paper-plane"></i>
+</div>
   </div>
 
   `;
@@ -62,3 +181,7 @@ module.exports = (app, mod, chat_popup_id) => {
 }
 
 
+
+
+
+{/* <i id="chat-container-close-${chat_popup_id}" class="chat-container-close fas fa-times"></i> */ }
