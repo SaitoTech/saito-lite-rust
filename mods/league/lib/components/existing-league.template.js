d304bb6938e3cc0a8d29a0e55e64e01d17fed39f
arpee <richard@saito.tech>
2022-08-03 19:59:15 +1000

    correcting display quirks in leage view

--- mods/league/lib/components/existing-league.template.js
+++ mods/league/lib/components/existing-league.template.js
@@ -1,15 +1,17 @@
 module.exports = (app, mod, league) => {
   let html = `
       <div class="league-component-existing-league-box" id="${league.id}">
-        
-        <h2>${league.name}</h2>
-        <div>Type: ${league.type}</div>
-        <div>Algo: ${league.ranking}</div>
-        <div>Players: ${league.playerCnt}${league.max_players > 0 ? ` / ${league.max_players}`:""}</div>
-        <div>Admin: ${league.admin.substring(0,10)+"..."}</div>
-        
+        <div class="league-component-existing-league-details">
+          <h2>${league.name}</h2>
+          <div>Type: ${league.type}</div>
+          <div>Algo: ${league.ranking}</div>
+          <div>Players: ${league.playerCnt}${league.max_players > 0 ? ` / ${league.max_players}`:""}</div>
+          <div>Admin: ${league.admin.substring(0,10)+"..."}</div>
+        </div>
+        <div class="league-component-existing-league-controls">
     `;
-
+    html += `<div class="league-component-existing-league-controls">
+    `;
     if ((league.myRank == undefined || league.myRank < 0) && (league.max_players == 0 || league.playerCnt < league.max_players)){
      html +=`<button class="league-component-existing-league" data-cmd="join" data-league-id="${league.id}">Join</button>`; 
     }
@@ -19,7 +21,7 @@ module.exports = (app, mod, league) => {
      html += `<button class="league-component-existing-league" data-cmd="delete" data-league-id="${league.id}">Delete</button>`;
     }
 
-  html += `</div>`;
+  html += `</div></div>`;
 
   return html;
 }
