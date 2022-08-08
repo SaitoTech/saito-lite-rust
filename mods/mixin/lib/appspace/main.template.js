0cacdfd8487b5d8c2862e33efe28de1d11d1e5f3
arpee <richard@saito.tech>
2022-08-08 18:10:10 +1000

    fix page height (that i broke) and update name on mixin module and all references

--- mods/mixin/lib/appspace/main.template.js
+++ mods/mixin/lib/appspace/main.template.js
@@ -1,6 +1,6 @@
 module.exports = MixinAppspaceTemplate = (app) => {
 
-  let mixin_mod = app.modules.returnModule("Mixin");
+  let mixin_mod = app.modules.returnModule("Crypto Wallet");
 
   let html = `
 
