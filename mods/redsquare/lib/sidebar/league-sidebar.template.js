f3e44e2e39eb027dbe7ecb5ef29c95c0991e9118
Daniel Worlton <f8daniel@gmail.com>
2022-09-06 15:53:21 +0700

    RS: add functionality to League rankings in right-sidebar

--- mods/redsquare/lib/sidebar/league-sidebar.template.js
+++ mods/redsquare/lib/sidebar/league-sidebar.template.js
@@ -4,30 +4,24 @@ module.exports = RedSquareLeagueSidebarTemplate = (app, mod) => {
 	let html = "";
 
 	if (league_mod){
-	  html += `<div class="saito-leaderboard">
+	  if (league_mod.leagues.length > 0){
+	  	html += `<div class="saito-leaderboard">
 	        <h6>Your Rankings:</h6>
 	        <div class="saito-table">`;
 
-	  if (league_mod.leagues.length > 0){
 		var leagues = league_mod.leagues.sort((a,b) => a.myRank < b.myRank);
-	    leagues.forEach(l => {
-			if (l.myRank > 0) {
-				html += `<div class="saito-table-row">
-				<div class="saito-table-gamename">${l.name}</div>
-				<div class="saito-table-rank">${l.myRank}</div>
-			</div>`;
-			}
-		});
-		leagues.forEach(l => {
-			if (l.myRank <= 0) {
-				html += `<div class="saito-table-row">
-				<div class="saito-table-gamename">${l.name}</div>
-				<div class="saito-table-rank">Unranked</div>
-			</div>`;
-			}
-		});
+		let cnt = 0;
+
+        for (let l of league_mod.leagues){
+          html += `<div id="league_${l.id}" class="saito-table-row rs-league-sidebar-ranking${(cnt%2 == 1)?" odd":""}">
+                          <div class="saito-table-gamename">${l.name}</div>
+                          <div class="saito-table-rank">${(l.myRank <= 0)?"Unranked":l.myRank}</div>
+                      </div>`;
+          cnt++;
+        }
 
 	  }
+	  //What's this for???
 	  html += `
 	   <!--div class="saito-table-row">
 	     <div></div>
@@ -38,3 +32,4 @@ module.exports = RedSquareLeagueSidebarTemplate = (app, mod) => {
 	}
     return html;
 };
+
