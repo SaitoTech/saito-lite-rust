43d4eb1647b3d2abe17315f6d29f8c0d0692d65d
Daniel Worlton <f8daniel@gmail.com>
2022-09-07 10:47:15 +0700

    RedSquare: attach gameObserver to sidebar

--- mods/redsquare/lib/sidebar/observer-sidebar.template.js
+++ mods/redsquare/lib/sidebar/observer-sidebar.template.js
@@ -1,10 +1,12 @@
+const ObserverSidebarDetails = require("./observer-sidebar-details.template");
+
 module.exports = RedSquareObserverSidebarTemplate = (app, mod) => {
 
 	let obs_mod = app.modules.returnModule("Observer");
 	let html = "";
 
 	if (obs_mod){
-	  html = `<div>Observer games</div>`;
+	  html = `<div id="rs_sidebar_observer" class="observer_sidebar">${ObserverSidebarDetails(app, mod, obs_mod.games)}</div>`;
 	}
   return html;
 };
