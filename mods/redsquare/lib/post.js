9717e44903175393b3ec317bf6d04fa363a041b1
Umair Khan <umair@saito.tech>
2022-09-21 11:09:59 +0500

    Revert changes

--- mods/redsquare/lib/post.js
+++ mods/redsquare/lib/post.js
@@ -20,8 +20,7 @@ class Post {
       document.querySelector('#redsquare-tweet-overlay').parentNode.remove();
     }
 
-    let div = '<div id="redsquare-tweet-overlay" class="redsquare-tweet-overlay"></div>';
-    this.overlay.show(app, mod, div);
+    this.overlay.show(app, mod, '<div id="redsquare-tweet-overlay" class="redsquare-tweet-overlay"></div>');
     app.browser.addElementToSelector(PostTemplate(app, mod, app.wallet.returnPublicKey(), this.parent_id, this.thread_id), "#redsquare-tweet-overlay");
     document.getElementById("post-tweet-textarea").focus();
     this.attachEvents(app, mod);
