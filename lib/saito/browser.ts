81a6c055e0004d36c5d9129a1cbfa69c87e2a6d0
davikstone2 <onyeji@saito.tech>
2022-09-23 09:53:40 +0200

    update

--- lib/saito/browser.ts
+++ lib/saito/browser.ts
@@ -279,15 +279,6 @@ class Browser {
     return "en";
   }
 
-  getWidth() {
-    return Math.max(
-      document.body.scrollWidth,
-      document.documentElement.scrollWidth,
-      document.body.offsetWidth,
-      document.documentElement.offsetWidth,
-      document.documentElement.clientWidth
-    );
-  }
   
   isMobileBrowser(user_agent) {
     let check = false;
