d56e756ab4f6ef1ebc22050a91fa888e5cf900da
Umair Khan <umair@saito.tech>
2022-09-27 12:21:56 +0500

    Arcade priv link overlay

--- mods/arcade/lib/arcade-main/templates/arcade-link.template.js
+++ mods/arcade/lib/arcade-main/templates/arcade-link.template.js
@@ -3,22 +3,13 @@ module.exports = (app, mod, data) => {
     let game = data.game || "";
 
     return `
-        <div class="invite-link">
-          <div class="invite-link-actions">
-            <div>
-              <i id="copy-invite-link" class="fa fa-clipboard" aria-hidden="true"></i>
-            </div>
-            <!--div class="social-links">
-              <img src="/saito/img/invite/facebook.png" />
-              <img src="/saito/img/invite/instagram.png" />
-              <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" target="_blank" class="twitter-share-button" data-show-count="false" data-text="Come play ${game} with me on the Saito Arcade" data-url=${invite_link}><img src="/saito/img/invite/twitter.png" /></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </div-->
-          </div>
-
-          <p id="invite-link-text" class="invite-link-text">${invite_link}</p>
-       
-        </div>
-        `
+      <div class="arcade--private-link-overlay saito-modal saito-modal-add-contact">
+         <div class="saito-modal-title">Invite Friend to Game</div>
+         <div class="saito-modal-content">
+           <div id="copy-invite-link" class="saito-modal-option" data-link='${invite_link}'><i class="fas fa-link"></i><div>Copy Game Link</div></div>
+         </div>
+      </div>
+    `;
 }
 
 /* data-text="Play this game with me" data-url="https://saito.io/arcade" */
--- mods/stunx/lib/components/invite-overlay.template.js
+++ mods/stunx/lib/components/invite-overlay.template.js
@@ -1,12 +1,12 @@
 module.exports = (roomCode) => {
    return `
       <div class="stunx-invite-overlay saito-modal saito-modal-add-contact">
-      <div class="saito-modal-title">Room Details</div>
-      <div class="saito-modal-content">
-        <div id="stunx-copy-vide-invite-code" class="saito-modal-option"><i class="fas fa-key"></i><div class="">Room Code</div></div>
-        <div id="stunx-copy-video-invite-link" class="saito-modal-option"><i class="fas fa-link"></i><div>Room Link</div></div>
+         <div class="saito-modal-title">Invite Friends to Video Chat</div>
+         <div class="saito-modal-content">
+           <div id="stunx-copy-vide-invite-code" class="saito-modal-option"><i class="fas fa-key"></i><div class="">Share Code</div></div>
+           <div id="stunx-copy-video-invite-link" class="saito-modal-option"><i class="fas fa-link"></i><div>Share Link</div></div>
+         </div>
       </div>
-    </div>
     `;
 }
 
