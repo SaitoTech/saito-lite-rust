cb6aa8beaad3cc91258fe61c2ad95a406499fc55
Umair Khan <umair@saito.tech>
2022-09-27 13:26:23 +0500

    Remove dash

--- mods/arcade/lib/arcade-main/templates/arcade-link.template.js
+++ mods/arcade/lib/arcade-main/templates/arcade-link.template.js
@@ -3,7 +3,7 @@ module.exports = (app, mod, data) => {
     let game = data.game || "";
 
     return `
-      <div class="arcade--private-link-overlay saito-modal saito-modal-add-contact">
+      <div class="arcade-private-link-overlay saito-modal saito-modal-add-contact">
          <div class="saito-modal-title">Invite Friend to Game</div>
          <div class="saito-modal-content">
            <div id="copy-invite-link" class="saito-modal-option" data-link='${invite_link}'><i class="fas fa-link"></i><div>Copy Game Link</div></div>
