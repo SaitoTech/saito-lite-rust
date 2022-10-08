4d6892818dc3f6dbb8dc7af2d95753db173f3371
David Lancashire <david.lancashire@gmail.com>
2022-10-08 10:07:48 +0700

    fix: quake port game server 29760

--- mods/quake3/quake3.js
+++ mods/quake3/quake3.js
@@ -18,8 +18,9 @@ class Quake3 extends GameTemplate {
     this.maxPlayers      = 4;
 
     this.content_server  = "q3.saito.io";
+    this.game_server  = "q3.saito.io:27960";
     //this.content_server  = "18.163.184.251:80";
-    this.game_server     = "18.163.184.251:27960";
+    //this.game_server     = "18.163.184.251:27960";
   }
 
 
