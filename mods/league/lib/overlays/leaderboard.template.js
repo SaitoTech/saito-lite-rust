8266933c0a7433222edab384b01e7f92cc40ca54
Daniel Worlton <f8daniel@gmail.com>
2022-10-10 12:32:13 +0200

    Leaderboard: only show challenge if online

--- mods/league/lib/overlays/leaderboard.template.js
+++ mods/league/lib/overlays/leaderboard.template.js
@@ -16,7 +16,7 @@ module.exports = LeaderboardTemplate = (app, mod, league, players) => {
 
 	    if (league.id !== "SAITOLICIOUS"){
 		    if (player.pkey !== myKey) {
-		    	html +=  `<div><button class="button challenge-btn" data-id="${player.pkey}">CHALLENGE</button></div>`
+		    	html +=  `<div><button class="button challenge-btn" data-id="${player.pkey}" style="display:none;">CHALLENGE</button></div>`
 		    }else{
 		    	html += `<div></div>`;	
 		    }
