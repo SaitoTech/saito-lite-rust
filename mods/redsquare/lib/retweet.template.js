73711a54d373776bfdb3e95e9abab0f58811d43f
davikstone2 <onyeji@saito.tech>
2022-10-11 20:56:03 +0100

    update

--- mods/redsquare/lib/retweet.template.js
+++ mods/redsquare/lib/retweet.template.js
@@ -10,7 +10,9 @@ module.exports = (app, mod, publickey, tweet = null) => {
 
   return `
     ${SaitoUserTemplate(app, mod, publickey, "add a comment to your retweet or just click submit...")}
+    <div class="saito-emoji-container">
     <textarea rows="7" class="post-tweet-textarea" name="post-tweet-textarea" id="post-tweet-textarea" placeholder="Optional Comment?" cols="60"></textarea>
+    </div>
     <div class="saito-button-primary post-tweet-button" id="post-tweet-button-${tweet_id}"> Retweet / Share </div>
     <div style="clear:both"></div>
   `;
