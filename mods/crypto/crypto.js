f6ebfafa2fc9506517045f3745d42f3dc8118d15
David Lancashire <david.lancashire@gmail.com>
2022-10-13 10:59:06 +0700

    fix: cryptos listing

--- mods/crypto/crypto.js
+++ mods/crypto/crypto.js
@@ -19,28 +19,39 @@ class Crypto extends ModTemplate {
   
   respondTo(type = "") {
     if (type == "game-menu") {
-      return {
-	menus : [ {
-          menu_option: {
-	    text: "Crypto",
-	    id: "game-crypto",
-	    class : "game-crypto",
-      	    callback : async function(app, game_mod) {
-	      let cm = app.modules.returnModule("Crypto");
-	      let game_id = game_mod.game.id;
-	      cm.enableCrypto(game_mod, game_id);
-	    }
-          },
-	} ]
+      let ac = this.app.wallet.returnActivatedCryptos();
+      let menus = [];
+      for (let i = 0; i < ac.length; i++) {
+	menus[i] = {};
+        menus[i].menu_option = {
+	text: "Crypto",
+	  id: "game-crypto",
+	  class : "game-crypto",
+      	  callback : async function(app, game_mod) {
+	    game_mod.menu.showSubMenu("game-crypto");
+          }
+        };
+	menus[i].sub_menu_option = {
+          text : ac[i].ticker,
+          id : "game-crypto-"+ac[i].ticker,
+          class : "game-crypto-"+ac[i].ticker,
+          callback : async function(app, game_mod) {
+            game_mod.menu.hideSubMenus();
+	    let cm = app.modules.returnModule("Crypto");
+	    let game_id = game_mod.game.id;
+	    cm.enableCrypto(game_mod, game_id, a[i].ticker);
+          }
+        };
       }
+      return { menus : menus };
     }
     return null;
   }
 
 
-  enableCrypto(game_mod, game_id) {
+  enableCrypto(game_mod, game_id, ticker) {
     alert("Proposing Game Stake!");
-    game_mod.proposeGameStake("TRX", "0.2");
+    game_mod.proposeGameStake(ticker, "0.2");
     alert("Proposed!");
     //this.overlay.render(this.app, this);
   }
