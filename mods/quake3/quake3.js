729d03ecff0ad4978ea1a07d9e469f329829e33d
David Lancashire <david.lancashire@gmail.com>
2022-10-18 11:38:13 +0700

    fix: push quake to q3-us by default

--- mods/quake3/quake3.js
+++ mods/quake3/quake3.js
@@ -20,8 +20,8 @@ class Quake3 extends GameTemplate {
     // ask chat not to start on launch
     this.request_no_interrupts = true;
 
-    this.content_server  = "q3.saito.io";
-    this.game_server  = "q3.saito.io:27959";
+    this.content_server  = "q3-us.saito.io";
+    this.game_server  = "q3-us.saito.io:27959";
 
     //this.content_server  = "18.163.184.251:80";
     //this.game_server     = "18.163.184.251:27960";
