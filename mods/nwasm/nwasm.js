6ba77902b158c92df2a95c951f076b6a303f39f0
David Lancashire <david.lancashire@gmail.com>
2022-10-24 16:24:23 +0700

    fix: initialization

--- mods/nwasm/nwasm.js
+++ mods/nwasm/nwasm.js
@@ -1,4 +1,6 @@
 const GameTemplate = require('./../../lib/templates/gametemplate');
+const NwasmGameOptionsTemplate = require("./lib/nwasm-game-options.template");
+
 
 class Nwasm extends GameTemplate {
 
@@ -17,6 +19,103 @@ class Nwasm extends GameTemplate {
 
     return this;
   }
+
+  returnGameOptionsHTML() {
+    return NwasmGameOptionsTemplate(this.app, this);
+  }
+
+  initializeGame(game_id) {
+    console.log("SET WITH GAMEID: " + game_id);
+
+    if (!this.game.state) {
+      console.log("******Generating the Game******");
+      this.game.state = this.returnState();
+      this.game.queue = [];
+      this.game.queue.push("round");
+      this.game.queue.push("READY");
+    }
+
+    console.log(JSON.parse(JSON.stringify(this.game)));
+
+  }
+
+  returnState() {
+
+    let state = {};
+    return state;
+
+  }
+
+  handleGameLoop(msg=null) {
+
+    ///////////
+    // QUEUE //
+    ///////////
+    if (this.game.queue.length > 0) {
+
+      let qe = this.game.queue.length-1;
+      let mv = this.game.queue[qe].split("\t");
+      let shd_continue = 1;
+
+      console.log(JSON.stringify(mv));
+
+      if (mv[0] === "round") {
+        this.game.queue.splice(this.game.queue.length-1, 1);
+      }
+
+      //
+      // avoid infinite loops
+      //
+      if (shd_continue == 0) {
+        console.log("NOT CONTINUING");
+        return 0;
+      }
+
+    }
+    return 1;
+  }
+
+
+  initializeHTML(app) {
+
+    if (!this.browser_active) { return; }
+
+    super.initializeHTML(app);
+
+    //
+    // ADD MENU
+    //
+    this.menu.addMenuOption({
+      text : "Game",
+      id : "game-game",
+      class : "game-game",
+      callback : function(app, game_mod) {
+        game_mod.menu.showSubMenu("game-game");
+      }
+    });
+    this.menu.addSubMenuOption("game-game",{
+      text : "Start New Game",
+      id : "game-new",
+      class : "game-new",
+      callback : function(app, game_mod) {
+        game_mod.menu.hideSubMenus();
+      }
+    });
+    this.menu.addSubMenuOption("game-game", {
+      text : "Stats",
+      id : "game-stats",
+      class : "game-stats",
+      callback : function(app, game_mod) {
+        game_mod.menu.hideSubMenus();
+	alert("You're doing fine...");
+      }
+    });
+
+    this.menu.addChatMenu(app, this);
+    this.menu.render(app, this);
+
+  }
+
 
 }
 
