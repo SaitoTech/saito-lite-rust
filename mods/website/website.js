a22816b7910ad4b7b861d9afe96ee17664fe3ff1
arpee <richard@saito.tech>
2022-10-24 22:09:24 +1100

    fix for missing path inclusion in website module

--- mods/website/website.js
+++ mods/website/website.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const saito = require('../../lib/saito/saito');
 const ModTemplate = require('../../lib/templates/modtemplate');
 
@@ -30,10 +31,27 @@ class Website extends ModTemplate {
     expressapp.use("/", express.static(`${__dirname}/../../mods/website/web`));
     // TODO: change every reference in the site from /website/* to /* and remove this line
     expressapp.use("/website/", express.static(`${__dirname}/../../mods/website/web`));
+    expressapp.get('/l/:campaign/:channel/:subchannel', async (req, res) => {
+      res.sendFile(path.join(__dirname + '/web/marketing/marketing.html'));
+    });
+    expressapp.get('/l/matomohelpers.js', async (req, res) => {
+      res.sendFile(path.join(__dirname + "../../../mods/matomo/matomoHelpers.js"));
+    });
+    expressapp.get('/l/maketrackinglink', async (req, res) => {
+      res.sendFile(path.join(__dirname + '/web/marketing/linkmakerform.html'));
+    });
+    expressapp.get('/weixin', async (req, res) => {
+      res.sendFile(path.join(__dirname + '/web/weixininvite.html'));
+    });
+    expressapp.get('/subpagestyle/:subpage/:stylesheetfile', async (req, res) => {
+      console.log(req.params.stylesheetfile);
+      res.sendFile(path.join(__dirname + '/web/subpage/'  + req.params.subpage + "/" + req.params.stylesheetfile));
+    });
     expressapp.get('/website/*', async (req, res) => {
       // use website/* here so that website.js will be initialized, i.e. we are still operating within the website module
       res.sendFile(path.join(__dirname + '/web/subpage/index.html'));
     });
   }
+
 }
 module.exports = Website;
