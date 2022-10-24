7cda22c86ca1cfbfa14ea955a0f5802f8194af45
David Lancashire <david.lancashire@gmail.com>
2022-10-24 15:29:17 +0700

    fix: text updated

--- mods/nwasm/nwasm.js
+++ mods/nwasm/nwasm.js
@@ -9,7 +9,7 @@ class Nwasm extends GameTemplate {
     this.name = "Nwasm";
 
     this.gamename = "Nintendo";
-    this.description = "The Saito N64 module provides a user-friendly digital-rights management system that allows you to manage and play your N64 games directly in your browser on any device that you own, complete with all of the social media features that come with every Saito module. And if you don't own any games, why not make friends on Saito RedSquare and borrow theirs to play when they are not being used.";
+    this.description = "The Saito N64 module provides a user-friendly digital-rights management system that allows you to backup and play the N64 games you own directly in your browser. Game files are encrypted so only you can access them and uploaded for storage to the cloud.";
     this.categories = "Games Entertainment";
 
     this.maxPlayers      = 1;
