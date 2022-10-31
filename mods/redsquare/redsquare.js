1566397d937de1904a70cdf9abac9992354e76be
Umair Khan <umair@saito.tech>
2022-10-31 09:03:06 +0500

    Data uri

--- mods/redsquare/redsquare.js
+++ mods/redsquare/redsquare.js
@@ -1388,42 +1388,7 @@ class RedSquare extends ModTemplate {
             
           }
         } // if query param has tweet id
-
-        if (typeof query_params.img_sig != "undefined") {
-          let img_sig = query_params.img_sig;
-          let sql = `SELECT * FROM tweets WHERE sig = '${img_sig}' OR parent_id = '${img_sig}'`;
-
-          let rows = await app.storage.queryDatabase(sql, {}, "redsquare");
-          for (let i = 0; i < rows.length; i++) {
-            let tx = new saito.default.transaction(JSON.parse(rows[i].tx));
-            let txmsg = tx.returnMessage();
-            let text = tx.msg.data.text;
-
-            if (typeof tx.msg.data.images != "undefined") {
-              let image = tx.msg.data?.images[0];
-            } else {
-              let publickey = tx.transaction.from[0].add;
-              let image = app.keys.returnIdenticon(publickey);
-            }
-
-            if (image != null) {
-              res.setHeader("Content-type", "text/html");
-              res.send(Buffer.from(`
-                <a id="download-img" href="`+ image +`" download>
-                  <img src="`+ image +`">
-                </a>
-
-                <script>
-                  document.getElementById('download-img').click();
-                </script>
-                `
-              ));;
-              res.end();
-              return;
-            }
-          }
-        }
-
+        
       }
 
       res.setHeader("Content-type", "text/html");
