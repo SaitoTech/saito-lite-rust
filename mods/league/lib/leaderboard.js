837f7f00c2d3eabe3c16efbd94e7a3cb969ae713
David Lancashire <david.lancashire@gmail.com>
2022-11-17 11:23:02 +0700

    fix: saito mania v2

---
+++ mods/league/lib/leaderboard.js
@@ -0,0 +1,51 @@
+const LeagueLeaderboardTemplate = require("./leaderboard.template");
+
+class LeagueLeaderboard {
+	
+  constructor(app, mod, selector="") {
+    this.mod = mod;
+    this.selector = selector;
+    this.listening = 0;
+  }
+
+  render(app, mod, selector="") {
+
+    if (selector) {
+   	this.selector = selector;
+    }
+
+    if (this.listening == 0) {
+      app.connection.on("league-update", ()=>{
+console.log("LEAGUE UPDATE HERE");
+    	this.render(app, mod, selector);
+      });
+      this.listening = 1;
+    }
+console.log("redering with selector: " + this.selector); 
+
+    app.browser.replaceElementBySelector(LeagueLeaderboardTemplate(app, mod), this.selector);
+console.log(" 2 redering with selector: " + this.selector); 
+    this.attachEvents(app, mod);
+console.log(" 3 redering with selector: " + this.selector); 
+ 
+//   let div = document.querySelector(this.selector);
+//    if (div) {
+console.log("UPDATE INNERHTML WITH TEMPLATE");
+//      div.innerHTML = LeagueLeaderboardTemplate(app, mod);
+//    }
+
+  }
+
+  attachEvents(app, mod){
+    document.querySelectorAll(`.league-leaderboard-ranking`).forEach((el) =>{
+	    el.onclick = function (e) {
+alert("CLICK");
+//	      let league_id = e.currentTarget.getAttribute("id").replace("league_", "");
+//	      app.connection.emit("view-league-details", league_id);
+	      }
+    });
+  }
+};
+
+module.exports = LeagueLeaderboard;
+
