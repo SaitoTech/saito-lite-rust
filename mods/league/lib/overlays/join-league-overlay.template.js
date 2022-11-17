9bba89632843ae38d3debf64394b26c29831862d
Umair Khan <umair@saito.tech>
2022-11-17 21:32:55 +0500

    Restyling

--- mods/league/lib/overlays/join-league-overlay.template.js
+++ mods/league/lib/overlays/join-league-overlay.template.js
@@ -6,30 +6,20 @@ module.exports = JoinLeagueTemplate = (app, mod, league) => {
 	let html = 	`
     	<div class="league-join-overlay-box">
 
+    		<img src="/poker/img/arcade/arcade.jpg">
+    		<div class="title-box">
+			<div class="title">${league.name}</div> ${info_html}
+		</div>
+    		<div class="league-join-controls">
 
-
-    	<div class="league-join-info-box">
-    		<div class="saito-module " data-payload="">
-		      <div class="saito-module-image-box">
-		         <div class="saito-module-image" style="background-image: url('/${game}/img/arcade/arcade.jpg')"></div>
-		      </div>
-		      <div class="saito-module-details-box">
-		        <div class="saito-module-title">${league.name} ${info_html}</div>
-		        <div class="saito-module-description">
-		        	
-		        	<p>${app.browser.stripHtml(league.description)}</p>
-		        </div>
-		      </div>  
-		    </div>
-
+		    <p class="league-join-email-note">Joining a league requires an email address</p>
+    
+		    <input type="text" placeholder="Enter your email...">
+		<button class="saito-button-primary small league-join-btn" id="league-join-btn" data-cmd="join" data-league-id="">JOIN LEAGUE</button> 
 		</div>
 
-		<div class="league-join-controls">
-		    	<input type="text" placeholder="Enter your email...">
-			<button class="saito-button-secondary league-join-btn" id="league-join-btn" data-cmd="join" data-league-id="">JOIN LEAGUE</button> 
-		</div>
+        <div>
 
-        </div>
    	`;
  
    	return html;
