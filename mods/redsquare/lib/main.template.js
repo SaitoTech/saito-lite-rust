d83763a50191f4bf00f99621f584b4d26478ca36
davikstone2 <onyeji@saito.tech>
2022-12-06 13:33:09 +0100

    update

--- mods/redsquare/lib/main.template.js
+++ mods/redsquare/lib/main.template.js
@@ -3,12 +3,16 @@ module.exports = () => {
 
    return `
     <div id="saito-container" class="saito-container">
+    <div class="saito-sidebar-container">
       <div class="saito-sidebar left">
+      </div>
       </div>
       <div class="saito-main">
       </div>
+     
       <div class="saito-sidebar right">
       </div>
+      
     </div>
   `;
 }
