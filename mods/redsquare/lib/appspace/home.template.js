cd4bd9ba0e50e338bcf8a34ec866769389917aa6
davikstone2 <onyeji@saito.tech>
2022-12-12 00:13:09 +0100

    css cleanup

--- mods/redsquare/lib/appspace/home.template.js
+++ mods/redsquare/lib/appspace/home.template.js
@@ -1,11 +1,11 @@
 module.exports = () => {
 
-   return `
+  return `
       <div class="redsquare-home">
         <div class="redsquare-appspace-header">
-          <div class="saito-redsquare-actions-buttons">
-            <div class="saito-redsquare-actions-buttons-icon"></div>
-            <div id="saito-page-header-title" class="saito-page-header-title">🟥 RED SQUARE</div>
+          <div class="redsquare-actions-buttons">
+            <div class="redsquare-actions-buttons-icon"></div>
+            <div id="redsquare-page-header-title" class="redsquare-page-header-title">🟥 RED SQUARE</div>
             <div class="redsquare-actions-container">
               <div id="redsquare-tweet" class="saito-button-secondary small">New Post</div>
               <div id="redsquare-profile" class="saito-button-secondary small">My Profile</div>
