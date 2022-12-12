5895b131ac4c901bb576e8d4227ab565a0089ff5
David Lancashire <david.lancashire@gmail.com>
2022-12-12 16:24:05 +0700

    fix: typo

--- mods/redsquare/lib/post.js
+++ mods/redsquare/lib/post.js
@@ -1,4 +1,4 @@
-eonst PostTemplate = require("./post.template");
+const PostTemplate = require("./post.template");
 const SaitoOverlay = require("./../../../lib/saito/ui/saito-overlay/saito-overlay");
 const SaitoEmoji = require("./../../../lib/saito/ui/saito-emoji/saito-emoji");
 
