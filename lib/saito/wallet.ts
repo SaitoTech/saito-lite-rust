ba12e9cde2e69a973991ad820ad775f41161d381
David Lancashire <david.lancashire@gmail.com>
2022-12-19 10:39:39 +0700

    feat: wallet upgrade

--- lib/saito/wallet.ts
+++ lib/saito/wallet.ts
@@ -29,7 +29,7 @@ export default class Wallet {
     spends: [], // TODO -- replace with hashmap using UUID. currently array mapping inputs -> 0/1 whether spent
     pending: [], // slips pending broadcast
     default_fee: 2,
-    version: 4.583,
+    version: 4.585,
   };
   public inputs_hmap: Map<string, boolean>;
   public inputs_hmap_counter: number;
