8aa227843f283e462a9fbfad42402df1e0a4ea3e
David Lancashire <david.lancashire@gmail.com>
2022-12-19 18:27:59 +0700

    fix: league overlay

--- mods/league/lib/overlays/league.template.js
+++ mods/league/lib/overlays/league.template.js
@@ -8,10 +8,6 @@ module.exports = LeagueOverlayTemplate = () => {
         <div class="league-overlay-header-title-box">
             <div class="league-overlay-header-title-box-title">Twilight Struggle</div>
             <div class="league-overlay-header-title-box-desc">Public League</div>
-            <div class="league-overlay-description">Twilight Struggle is a card-driven strategy game for two
-            players, with its theme taken from the Cold War. One player plays the United States (US), and the other
-            plays the Soviet Union (USSR).
-        </div>    
         </div>
         <div class="league-over-header-controls">
             <div class="league-overlay-create-game-button saito-button-secondary">create game</div>
@@ -19,6 +15,7 @@ module.exports = LeagueOverlayTemplate = () => {
     </div>
     <div class="league-overlay-body">
         <div class="league-overlay-league-body-games">
+            <div class="league-overlay-description">Twilight Struggle is a card-driven strategy game for two players, with its theme taken from the Cold War. One player plays the United States (US), and the other plays the Soviet Union (USSR). </div>    
             <div class="league-overlay-league-body-games-list">
                 <h6>Upcoming Games</h6>
                 <div class="saito-table">
