99d9e549ce11bd5f0785799bd940ec51fcce43e6
David Lancashire <david.lancashire@gmail.com>
2022-12-28 14:16:42 +0700

    fix: loading image in join game overlay dynamically

--- mods/arcade/lib/overlays/join-game.template.js
+++ mods/arcade/lib/overlays/join-game.template.js
@@ -1,14 +1,12 @@
 module.exports = JoinGameOverlayTemplate = (app, mod, invite_tx) => {
 
   let txmsg = invite_tx.returnMessage();
-console.log(JSON.stringify(txmsg));
   let game_mod = app.modules.returnModuleByName(txmsg.name);
-console.log(game_mod.returnName());
 
   let html = `
   <div class="arcade-game-overlay">
   <div class="arcade-game-overlay-header">
-	  <div class="arcade-game-overlay-header-image">
+	  <div class="arcade-game-overlay-header-image" style="background-image: url('/${game_mod.returnSlug()}/img/arcade/arcade.jpg')">
 	  </div>
 	  <div class="arcade-game-overlay-header-title-box">
 		  <div class="arcade-game-overlay-header-title-box-title">${game_mod.returnName()}</div>
@@ -31,7 +29,7 @@ console.log(game_mod.returnName());
 					  </div>
 					  <div class="saito-username">fkBBy46Rjw3gAtzSa8o1kHKn9HeCzAibiFvnMqyS5A2b</div>
 				  </div>
-				  <div class="saito-table-row arcade-game-player-row">
+				  <div class="saito-table-row arcade-game-player-row" style="margin-bottom: 2rem">
 					  <div class="saito-identicon-box">
 						  <img class="saito-identicon"
 							  src="${app.keys.returnIdenticon('fkBBy46Rjw3gAtzSa8o1kHKn9HeCzAibiFvnMqyS5A2b')}">
