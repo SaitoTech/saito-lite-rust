aed20411bc88e0f685b6f937ce15bade509b4e96
David Lancashire <david.lancashire@gmail.com>
2022-12-29 11:51:16 +0700

    fix: invites

--- mods/invites/lib/appspace/main.template.js
+++ mods/invites/lib/appspace/main.template.js
@@ -7,8 +7,7 @@ module.exports = InvitesAppspaceTemplate = (app, mod) => {
 
     <div class="saito-page-header">
       <div id="invites-new-invite" class="saito-button-secondary small" style="float: right;">Create Invite</div>
-      <div id="saito-page-header-title" class="saito-page-header-title">INVITATIONS
-      </div>
+      <div id="saito-page-header-title" class="saito-page-header-title">INVITATIONS</div>
     </div>
 
     <div id="invites-list" class="invites-list">
