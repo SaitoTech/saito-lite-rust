dc624b85422d3ea15aa17dce6d03047cd3306f8c
Umair Khan <umair@saito.tech>
2023-01-23 18:04:15 +0500

    rankings changes

--- mods/league/lib/leaderboard.js
+++ mods/league/lib/leaderboard.js
@@ -10,10 +10,10 @@ class Leaderboard {
     this.container = container;
     this.league = null;
 
+
     app.connection.on('league-leaderboard-render-request', (league) => {
       this.render(league);
     });
-
   }
 
 
@@ -25,36 +25,17 @@ class Leaderboard {
       this.app.browser.addElementToSelectorOrDom(LeaderboardTemplate(this.app, this.mod), this.container);
     }  
 
-    console.log("THIS LEAGUE");
-    console.log(this.league);
     
-    let players = null;
-    let html = null;
-
-    if (this.league != null) {
-      players = this.loadLeaderboard(this.league);
-      if (players) {
-        for (let r of players){
-          html += `
-            <div class="saito-table-row">
-              <div class="center-align">3</div>
-              <div class="saito-address saito-address-${player.pkey}" data-id="${player.pkey}">${player.pkey}</div>
-              <div class="right-align">${Math.round(player.score)}</div>
-            </div>    
-          `;
-        }
-      } 
+    if (this.league != null) {  
+      this.loadLeaderboard(this.league);     
     }
-
-    html = (players == null || html == null) ? `<div class="league-error">No ranking stats for this league</div>` : html;
-    this.app.browser.addElementToSelector(html, ".league-leaderboard .saito-table-body");
   }
 
 
   /**
   * Query the league.players for the latest stats
   */
-  loadLeaderboard(league){
+  async loadLeaderboard(league){
     let leaderboard = [];
     let players = [];
 
@@ -62,33 +43,51 @@ class Leaderboard {
     let pid = this.app.wallet.returnPublicKey();
     league.myRank = -1;
 
-    this.mod.sendPeerDatabaseRequestWithFilter("League" , `SELECT * FROM players WHERE league_id = '${league.id}' ORDER BY score DESC, games_won DESC, games_tied DESC, games_finished DESC` ,
-      (res) => {
-        league.playerCnt = 0;
-        
-        console.log("result");
-        console.log(res);
+    return this.mod.sendPeerDatabaseRequestWithFilter("League" , `SELECT * FROM players WHERE league_id = '${league.id}' 
+      ORDER BY score DESC, games_won DESC, games_tied DESC, games_finished DESC` ,
+    (res) => {
+      league.playerCnt = 0;
+      
+      console.log("result");
+      console.log(res);
 
-        if (res.rows) {
-          let cnt = 0;
-          for (let p of res.rows){
-            cnt++;
-            leaderboard.push(p);
-            if (p.pkey == pid){
-              league.myRank = cnt; //I am the cnt player in the leaderboard
-            }else{
-              players.push(p.pkey);
-            }
+      if (res.rows) {
+        let cnt = 0;
+        for (let p of res.rows){
+          cnt++;
+          leaderboard.push(p);
+          if (p.pkey == pid){
+            league.myRank = cnt; //I am the cnt player in the leaderboard
+          }else{
+            players.push(p.pkey);
           }
-          league.playerCnt = cnt;
         }
+        league.playerCnt = cnt;
+      }
 
-        if (leaderboard.length > 0){
-          return leaderboard;
+    
+      let html = ``;
+      if (leaderboard.length > 0) {        
+        for (let i=0; i<leaderboard.length; i++){
+          console.log("rankkk");
+          console.log(leaderboard[i]);
+
+          html += `
+            <div class="saito-table-row">
+              <div class="center-align">${i+1}</div>
+              <div class="saito-address saito-address-${leaderboard[i].pkey}" data-id="${leaderboard[i].pkey}">${leaderboard[i].pkey}</div>
+              <div class="right-align">${Math.round(leaderboard[i].score)}</div>
+            </div>    
+          `;
         }
+      }
 
-        return false;
-      });
+      html = (players == null || html == null) ? `<div class="league-error">No ranking stats for this league</div>` : html;
+      if (html != ``) {
+       this.app.browser.addElementToSelector(html, ".league-leaderboard .saito-table-body");      
+      }
+    });
+
   }
 
 
