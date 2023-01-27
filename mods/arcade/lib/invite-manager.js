65ed90b42719330cac99f257473e3685f9d83690
Umair Khan <umair@saito.tech>
2023-01-27 20:01:43 +0500

    Game loader inside RS

--- mods/arcade/lib/invite-manager.js
+++ mods/arcade/lib/invite-manager.js
@@ -1,30 +1,46 @@
 const Invite = require("./invite");
 const InviteManagerTemplate = require("./invite-manager.template");
 const JSON = require('json-bigint');
+const ArcadeInitializer = require("./main/initializer");
+const SaitoOverlay = require('./../../../lib/saito/ui/saito-overlay/saito-overlay');
 
 class InviteManager {
 
 	constructor(app, mod, container="") {
 
-	  this.app = app;
-	  this.mod = mod;
-	  this.container = container;
-	  this.name = "InviteManager";
-	  this.type = "short";
-	  this.list = "all";
+		this.app = app;
+		this.mod = mod;
+		this.container = container;
+		this.name = "InviteManager";
+		this.type = "short";
+		this.list = "all";
 
-	  this.lists = ["mine","open"];
+		this.lists = ["mine","open"];
 
-	  this.invites = {};
+		this.invites = {};
+		this.loader_overlay = new SaitoOverlay(app, mod, false, true);
 
-	  //
-	  // handle requests to re-render invite manager
-	  //
-	  this.app.connection.on("arcade-invite-manager-render-request", () => {
-            if (!this.mod.is_game_initializing) {
-	      this.render();
-	    }
-	  });
+		//
+		// handle requests to re-render invite manager
+		//
+		this.app.connection.on("arcade-invite-manager-render-request", () => {
+		    if (!this.mod.is_game_initializing) {
+		  this.render();
+		}
+		});
+
+		app.connection.on("arcade-game-initialize-render-request", (game_id) => {
+			if (this.mod.browser_active == 1) { // dont add overlay if arcade
+			  return; 
+			} else {
+			
+				this.loader_overlay.hide();	  	
+				this.loader_overlay.show('<div class="arcade_game_overlay_loader"></div>');
+				let game_loader = new ArcadeInitializer(app, mod, ".arcade_game_overlay_loader");
+				game_loader.render(game_id);	
+			
+			}
+		});
 
 	}
 
