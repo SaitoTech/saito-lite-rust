d5936727b70ed4ac7a20df3ab00a15d034cdc8e4
David Lancashire <david.lancashire@gmail.com>
2023-02-02 16:22:23 +0700

    fix: giphy loader

--- mods/giphy/giphy.js
+++ mods/giphy/giphy.js
@@ -12,7 +12,7 @@ const { GiphyFetch } = require('@giphy/js-fetch-api')
 const { renderGif, renderGrid } = require('@giphy/js-components');
 const SaitoOverlay = require("./../../lib/saito/ui/saito-overlay/saito-overlay");
 const saitoGifTemplate = require('./lib/giphy.template');
-const SaitoLoader = require('./../../lib/saito/new-ui/saito-loader/saito-loader');
+const SaitoLoader = require('./../../lib/saito/ui/saito-loader/saito-loader');
 const ModTemplate = require('../../lib/templates/modtemplate');
 
 class Giphy extends ModTemplate {
