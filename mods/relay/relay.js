20baf3f48a28e2425faaa32d3e37b5cb9a728b48
David Lancashire <david.lancashire@gmail.com>
2023-02-11 16:10:25 +0700

    fix: message removal

--- mods/relay/relay.js
+++ mods/relay/relay.js
@@ -56,10 +56,6 @@ class Relay extends ModTemplate {
             recipients.push(recipient);
         }
 
-console.log("RECIPIENTS: " + JSON.stringify(recipients));
-console.log("MESSAGE_REQUEST: " + JSON.stringify(message_request));
-console.log("MESSAGE_DATA: " + JSON.stringify(message_data));
-
         //
         // transaction to end-user, containing msg.request / msg.data is
         //
