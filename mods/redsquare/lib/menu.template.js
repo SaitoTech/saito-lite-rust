950d241269ef0f9d2ef220646a4e2a5826ef4061
David Lancashire <david.lancashire@gmail.com>
2023-02-21 19:22:19 +0700

    fix: new tweet button works

--- mods/redsquare/lib/menu.template.js
+++ mods/redsquare/lib/menu.template.js
@@ -3,7 +3,7 @@ module.exports = () => {
    return `
       <div class="saito-menu redsquare-menu">
 
-        <div id="new-tweet" class="saito-button-primary">New Tweet</div>
+        <div id="new-tweet" class="saito-button-primary">New Post</div>
 
         <ul class="saito-menu-list">
           <li class="redsquare-menu-home">
