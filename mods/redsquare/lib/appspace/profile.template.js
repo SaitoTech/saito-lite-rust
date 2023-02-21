0656d14fbee7d0758897c06acd23fe57e19eceef
David Lancashire <david.lancashire@gmail.com>
2023-02-21 19:02:57 +0700

    fix: profile update

--- mods/redsquare/lib/appspace/profile.template.js
+++ mods/redsquare/lib/appspace/profile.template.js
@@ -1,36 +1,55 @@
-module.exports = () => {
+module.exports = (app, publickey="") => {
 
-   return `
+   let key = app.keychain.returnKey(publickey);
+   if (!key) { key = {}; }
+
+   let email  = key.email || "";
+   let identifier = key.identifier || "";
+   let telegram = key.telegram || "";
+   
+   let html = '';
+   html += `
       <div class="redsquare-profile">
         <div class="redsquare-appspace-header">
           <div class="redsquare-actions-buttons">
             <div class="redsquare-actions-buttons-icon"></div>
-            <div id="redsquare-page-header-title" class="redsquare-page-header-title">🟥 RED SQUARE</div>
+            <div id="redsquare-page-header-title" class="redsquare-page-header-title">🟥 PROFILE</div>
             <div class="redsquare-actions-container"></div>
           </div>
         </div>
         <div class="redsquare-appspace-body">
 
-          <h6>Wallet</h6>
-          <div class="settings-appspace-user-details">
-            <div class="saito-black">Email:</div>
-            <div>${email_registered}</div>
-            <div id="register-identifier-btn-label" class="saito-black">Username:</div>
-            <div>${identifier_registered}</div>
-            <div class="saito-black">Public Key:</div>
-            <div class="saito-address">${app.wallet.returnPublicKey()} <span style="margin-left: .5rem;" class="copy-public-key">  <i class="fas fa-copy"></i></span></div>
-            <div class="saito-black">Private Key:</div>
-            <div class="settings-appspace-privatekey saito-password">
-              ${app.wallet.returnPrivateKey()}
-              <i class="settings-appspace-see-privatekey fas fa-eye" id="settings-appspace-see-privatekey"></i>
-            </div>
+          <div class="redsquare-appspace-profile">
+	`;
+	if (email) {
+	  html += `	
+            <div>Email:</div>
+	    <div>${email}</div>
+	  `;
+	}
+	if (identifier) {
+	  html += `
+            <div>Username:</div>
+            <div>${identifier}</div>
+	  `;
+	}
+	if (publickey) {
+	  html += `
+            <div>Public Key:</div>
+            <div>${publickey} <span style="margin-left: .5rem;" class="copy-public-key">  <i class="fas fa-copy"></i></span></div>
+	  `;
+	}
+
+	html += `
           </div>
 
-	  <div class="redsquare-profile-tweets"></div>
+	  <div class="redsquare-profile-tweets"><div style="padding-left:2rem">loading recent tweets...</div></div>
 
         </div>
       </div>
-  `;
+   `;
+
+    return html;
 
 }
 
