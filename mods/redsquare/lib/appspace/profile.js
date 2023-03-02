68896ea1498ac3087f28ca26334525627f327432
arpee <richard@saito.tech>
2023-03-02 14:31:21 +1100

    fix for notifications and profile not being at top of page on click. #21704828

--- mods/redsquare/lib/appspace/profile.js
+++ mods/redsquare/lib/appspace/profile.js
@@ -13,7 +13,7 @@ class AppspaceProfile {
   }
 
 
-  render(publickey="") {
+  render(publickey = "") {
 
 
     if (publickey == "") { if (this.publickey != "") { publickey = this.publickey; } }
@@ -27,25 +27,25 @@ class AppspaceProfile {
     }
 
     let sql = `SELECT * FROM tweets WHERE publickey = '${publickey}' ORDER BY created_at DESC;`;
-    for (let i = 0; i < this.mod.peers_for_tweets.length; i++) {   
+    for (let i = 0; i < this.mod.peers_for_tweets.length; i++) {
       this.mod.loadTweetsFromPeerAndReturn(this.mod.peers_for_tweets[i], sql, (txs) => {
-	document.querySelector(".redsquare-profile-tweets").innerHTML = "";
+        document.querySelector(".redsquare-profile-tweets").innerHTML = "";
         for (let z = 0; z < txs.length; z++) {
-	  let tweet = new Tweet(this.app, this.mod, ".redsquare-profile-tweets", txs[z]);
-  	  tweet.render();
+          let tweet = new Tweet(this.app, this.mod, ".redsquare-profile-tweets", txs[z]);
+          tweet.render();
         }
         this.attachEvents();
       }, false, false);
     }
-
-  }  
+    document.querySelector('.saito-container').scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }
 
   attachEvents() {
 
-      document.querySelector('.copy-public-key').onclick = (e) =>{
-        navigator.clipboard.writeText(this.publickey);
-        salert("Public key copied");
-      }
+    document.querySelector('.copy-public-key').onclick = (e) => {
+      navigator.clipboard.writeText(this.publickey);
+      salert("Public key copied");
+    }
 
   }
 
