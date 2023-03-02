5cfa3d71f055a124cbf587e8011867cbdf6f0b21
davikstone2 <onyeji@saito.tech>
2023-03-02 21:38:01 +0100

    update

--- mods/stun/stun.js
+++ mods/stun/stun.js
@@ -624,7 +624,7 @@ class Stun extends ModTemplate {
                         case "failed":
                             console.log("connection state ", pc.connectionState)
                             this.app.connection.emit('change-connection-state-request', offer_creator, pc.connectionState, offer.ui_type, offer.call_type, room_code);
-                            this.createMediaChannelConnectionWithPeers([publicKey], offer.ui_type, offer.call_type, room_code);
+                            this.createMediaChannelConnectionWithPeers([offer_creator], offer.ui_type, offer.call_type, room_code);
                             break;
                         default:
                             ""
