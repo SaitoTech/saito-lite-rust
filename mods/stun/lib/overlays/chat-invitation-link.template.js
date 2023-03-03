2b7e237bc97654253593808856aa40a877897d52
davikstone2 <onyeji@saito.tech>
2023-03-03 13:41:50 +0100

    #240 fix broken link

--- mods/stun/lib/overlays/chat-invitation-link.template.js
+++ mods/stun/lib/overlays/chat-invitation-link.template.js
@@ -3,7 +3,7 @@ module.exports = (app, mod, link) => {
     return `
       <div class="stun-private-link-overlay saito-modal saito-modal-add-contact">
          <div class="saito-modal-content">
-           <div id="copy-invite-link" class="saito-modal-menu-option" data-link='${link}'><i class="fas fa-link"></i><div>Copy Video Chat Invitation Link</div></div>
+           <div id="copy-invite-link" class="saito-modal-menu-option" data-link='${link}'><i class="fas fa-link"></i><div>Copy Invitation Link</div></div>
          </div>
       </div>
     `;
