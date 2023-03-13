fd7b591343fb655ae79ed60761a8fce13538e998
Umair Khan <umair@saito.tech>
2023-03-13 21:11:02 +0500

    dynamic values

--- mods/mixin/lib/appspace/mixin-withdraw.js
+++ mods/mixin/lib/appspace/mixin-withdraw.js
@@ -20,8 +20,56 @@ class MixinWithdraw {
 
   render() {
     this.overlay.show(MixinWithdrawTemplate(this.app, this.mod, this));
+    
+    this.loadCryptos();
+
     this.attachEvents();
   }  
+
+
+  loadCryptos() {
+    let available_cryptos = this.app.wallet.returnInstalledCryptos();
+    let preferred_crypto = this.app.wallet.returnPreferredCrypto();
+    let add = preferred_crypto.returnAddress();
+    let fee = 0;
+    let balance = 0;
+
+    console.log("withdraw //////////");
+    console.log(available_cryptos);
+    console.log(preferred_crypto);
+    console.log(add);
+
+
+    // add crytpo dropdowns
+    document.querySelector("#withdraw-select-crypto").innerHTML = ""; 
+    let html = '';
+    for (let i = 0; i < available_cryptos.length; i++) {
+      let crypto_mod = available_cryptos[i];
+      html = `<option ${(crypto_mod.name == preferred_crypto.name) ? 'selected' : ``} 
+      id="crypto-option-${crypto_mod.name}" value="${crypto_mod.ticker}">${crypto_mod.ticker}</option>`;
+
+      this.app.browser.addElementToElement(html, document.querySelector("#withdraw-select-crypto"));
+    }
+
+
+    // calculate fee & balance
+    if (preferred_crypto.ticker == "SAITO") {
+      fee = this.app.wallet.wallet.default_fee;
+      balance = this.app.wallet.wallet.balance;
+    } else {
+      mod.checkWithdrawalFee(asset_id, function(fee) {
+        document.getElementById("confirm-fee-text").innerHTML = 'Withdrawal fee is '+fee+'. Continue with withdrawal?';
+        document.querySelector("#withdrawl-sent-cont").style.display = 'block';
+      });
+      
+      balance = preferred_crypto.balance;
+    }
+
+    // show fee & balance
+    document.querySelector(".withdraw-info-value.fee").innerHTML = fee + ` ${preferred_crypto.ticker}`;
+    document.querySelector(".withdraw-info-value.balance").innerHTML = balance + ` ${preferred_crypto.ticker}`;
+  }
+
 
   attachEvents() {    
 
@@ -96,52 +144,50 @@ class MixinWithdraw {
 
           // }
 
+
           document.querySelector("#withdrawal-form").onsubmit = (e) => {
             e.preventDefault();
-
-            document.querySelectorAll(".saito-overlay-form-input, .input-elements-container, .form-submit-container, .dropdown-cont").forEach(function(item, key){
-              item.classList.toggle("hide-element");
-            });
-
-            document.querySelectorAll(".confirm-msg-container, .confirm-btn-container, .confirm-submit").forEach(function(item, key){
-              item.classList.toggle("activated"); 
-            });
-
+            document.querySelector("#withdraw-step-one").classList.toggle("hide-element");
+            document.querySelector("#withdraw-step-two").classList.toggle("hide-element");
           }
 
           document.querySelector("#withdraw-cancel").onclick = (e) => {
             e.preventDefault();
-
-            document.querySelectorAll(".saito-overlay-form-input, .input-elements-container, .form-submit-container, .dropdown-cont").forEach(function(item, key){
-              item.classList.toggle("hide-element");
-            });
-
-            document.querySelectorAll(".confirm-msg-container, .confirm-btn-container, .confirm-submit").forEach(function(item, key){
-              item.classList.toggle("activated"); 
-            });
+            document.querySelector("#withdraw-step-one").classList.toggle("hide-element");
+            document.querySelector("#withdraw-step-two").classList.toggle("hide-element");
           }
+        
 
           document.querySelector("#withdraw-confirm").onclick = (e) => {
             e.preventDefault();
 
-            document.querySelector(".withdraw-msg-icon").classList.toggle("fa-circle-exclamation");
-            document.querySelector(".withdraw-msg-icon").classList.toggle("fa-circle-notch");
-            document.querySelector(".confirm-submit").style.opacity = 0;
-            document.querySelector(".withdraw-msg-text").innerText = "Sending";
-            document.querySelector(".withdraw-msg-question").innerText = "...";
-
-            setTimeout(function() {
-              document.querySelector(".confirm-msg").innerHTML = "Request Successful";
+            try {
+              document.querySelector(".withdraw-msg-icon").classList.toggle("fa-circle-exclamation");
               document.querySelector(".withdraw-msg-icon").classList.toggle("fa-circle-notch");
-              document.querySelector(".withdraw-msg-icon").classList.toggle("fa-circle-check");
-            }, 3000);
+              document.querySelector(".confirm-submit").style.opacity = 0;
+              document.querySelector(".withdraw-msg-text").innerText = "Sending";
+              document.querySelector(".withdraw-msg-question").innerText = "...";
+
+              setTimeout(function() {
+                document.querySelector(".confirm-msg").innerHTML = `Transfer request successful <br > Please check transaction history for confirmation`;
+                document.querySelector(".withdraw-msg-icon").classList.toggle("fa-circle-notch");
+                document.querySelector(".withdraw-msg-icon").classList.toggle("fa-circle-check");
+              }, 3000);
+            } catch(err) {
+              document.querySelector(".confirm-msg").innerHTML = `Transfer request unsuccessful <br > Please try again`;
+              document.querySelector(".withdraw-msg-icon").classList.remove("fa-circle-notch");
+              document.querySelector(".withdraw-msg-icon").classList.remove("fa-circle-check");
+              document.querySelector(".withdraw-msg-icon").classList.toggle("fa-circle-xmark");
+            }
           }
+
         }
 
-        if (document.querySelector("#max-amount-btn") != null) {
-          document.querySelector("#max-amount-btn").onclick = (e) => {
-            let amount_avl = document.querySelector("#amount-avl").getAttribute('data-amount-avl');
-            document.querySelector("#withdraw_amount").value = amount_avl;
+
+        if (document.querySelector("#withdraw-max-btn") != null) {
+          document.querySelector("#withdraw-max-btn").onclick = (e) => {
+            let amount_avl = document.querySelector("#withdraw-input-amount").getAttribute('data-amount-avl');
+            document.querySelector("#withdraw-input-amount").value = amount_avl;
           }
         }
 
