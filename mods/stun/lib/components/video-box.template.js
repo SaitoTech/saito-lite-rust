848833bfe9d899414b2095baffb1d654224781fc
davikstone2 <onyeji@saito.tech>
2023-03-16 04:48:19 +0100

    upddate ui type

--- mods/stun/lib/components/video-box.template.js
+++ mods/stun/lib/components/video-box.template.js
@@ -1,6 +1,6 @@
 module.exports = (streamId, muted) => {
   return `
-    <div id="stream${streamId}"  class=" "video-box-container-large">
+    <div id="stream${streamId}"  class="video-box-container-large">
       <video ${muted ? "muted" : " "} autoplay playsinline="true" class="video-box"></video>
       <canvas id="output_canvas"></canvas>
       <div id="reconnect-button"><button>reconnect</button> </div>
