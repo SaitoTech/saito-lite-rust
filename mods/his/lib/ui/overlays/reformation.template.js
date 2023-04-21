cca7efb66338404eb734e5f9ca7ad37c6c857765
David Lancashire <david.lancashire@gmail.com>
2023-04-21 07:55:05 +0700

    fix: Duke's updates to Reformation Box

--- mods/his/lib/ui/overlays/reformation.template.js
+++ mods/his/lib/ui/overlays/reformation.template.js
@@ -2,61 +2,35 @@ module.exports = () => {
 
   let html = `
       <div class="reformation-overlay" id="reformation-overlay">
-
         <div class="title">reformation in strasbourg</div>
-
 	<div class="reformation-box">
 	  <div class="protestant">
 	    <div class="title">Protestants</div>
-  	    <div class="reform-table">
-              <div class="reform-box-description">Kessel <div class="adjacency">Adjacency</div></div>
- 	      <div class="reform-element">
-                <div class="reform-box">
-                   <div class="reform-box-num">5</div>
-                </div>
-	      </div>
-              <div class="reform-box-description">Strasbourg <div class="adjacency">Adjacency</div></div>
-	      <div class="reform-element">
-                <div class="reform-box">
-                    <div class="reform-box-num">5</div>
-                </div>
-	      </div>
-              <div class="reform-box-description">Leipzig <div class="adjacency">Adjacency</div></div>
-	      <div class="reform-element">
-                <div class="reform-box">
-                    <div class="reform-box-num">5</div>
-                </div>
-	      </div>
-              <div class="reform-box-description">Kessel <div class="adjacency">Adjacency</div></div>
-	      <div class="reform-element">
-                <div class="reform-box">
-                   <div class="reform-box-num">5</div>
-                </div>
-	      </div>
-	    </div>
+            <div class="reform-box-description">Strasbourg <div class="adjacency">Adjacency</div></div>
+            <div class="reform-roll">5</div>
+            <div class="reform-box-description">Leipzig <div class="adjacency">Adjacency</div></div>
+            <div class="reform-roll">5</div>
+            <div class="reform-box-description">Kessel <div class="adjacency">Adjacency</div></div>
+	    <div class="reform-roll">5</div>
+	    <div class="reform-box-description">Kessel <div class="adjacency">Adjacency</div></div>
+	    <div class="reform-roll">5</div>
+	    <div class="reform-box-description">Kessel <div class="adjacency">Adjacency</div></div>
+	    <div class="reform-roll">5</div>
+	    <div class="reform-box-description">Kessel <div class="adjacency">Adjacency</div></div>
+	    <div class="reform-roll">5</div>	               
 	  </div>
-
 	  <div class="papacy">	    
             <div class="title">Papacy</div>
-	    <div class="reform-table">
               <div class="reform-box-description">Kessel <div class="adjacency">Adjacency</div></div>
-	      <div class="reform-element">
-                <div class="reform-box">
-                   <div class="reform-box-num">5</div>
-                </div>
-	      </div>
+	      <div class="reform-roll">5</div>
               <div class="reform-box-description">Strasbourg <div class="adjacency">Adjacency</div></div>
-	      <div class="reform-element">
-                <div class="reform-box">
-                  <div class="reform-box-num">5</div>
-                </div>
-	      </div>
-	    </div>
-	  </div>
+	      <div class="reform-roll">5</div>
+              <div class="reform-box-description">Leipzig <div class="adjacency">Adjacency</div></div>
+	      <div class="reform-roll">5</div>
+            </div>
+          </div>
         </div>
       </div>
-  </div>
-
   `;
   return html;
 
