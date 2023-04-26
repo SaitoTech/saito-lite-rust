ed523a70f7cb054d7a11bdcb3c3a84b7f0cba4c7
Umair Khan <umair@saito.tech>
2023-04-26 09:32:02 +0500

    Fix twlight play as dropdown

--- mods/twilight/lib/twilight-singular-game-options.template.js
+++ mods/twilight/lib/twilight-singular-game-options.template.js
@@ -1,11 +1,13 @@
 module.exports = () => {
 
-  let html =  `<div><label for="player1">Play as:</label>
-            <select name="player1">
-              <option value="random" selected>random</option>
-              <option value="ussr">USSR</option>
-              <option value="us">US</option>
-            </select></div>
+  let html =  `
+            <div class="overlay-input">
+              <select name="player1">
+                <option value="random" selected>play as random</option>
+                <option value="ussr">play as USSR</option>
+                <option value="us">play as US</option>
+              </select>
+            </div>
           `;
     return html;
 
