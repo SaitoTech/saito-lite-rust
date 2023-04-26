d7dce0880dd5c3b69241ffea206d3eb551c55dd9
Umair Khan <umair@saito.tech>
2023-04-26 09:38:05 +0500

    Change option text

--- mods/twilight/lib/twilight-singular-game-options.template.js
+++ mods/twilight/lib/twilight-singular-game-options.template.js
@@ -3,7 +3,7 @@ module.exports = () => {
   let html =  `
             <div class="overlay-input">
               <select name="player1">
-                <option value="random" selected>play as random</option>
+                <option value="random" selected>random sides</option>
                 <option value="ussr">play as USSR</option>
                 <option value="us">play as US</option>
               </select>
