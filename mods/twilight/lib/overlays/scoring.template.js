41bf9444f5cc4917da543e7ba4efcc962fa8e18a
David Lancashire <david.lancashire@gmail.com>
2023-05-11 00:48:08 +0700

    fix: twilight

--- mods/twilight/lib/overlays/scoring.template.js
+++ mods/twilight/lib/overlays/scoring.template.js
@@ -4,8 +4,6 @@ module.exports = (region, sobj) => {
   let bonus_events = "";
   let winner_css = "winner-tied";
 
-console.log(JSON.stringify(sobj));
-
   let us_bg  = `${sobj.us.bg} battleground`;
   let us_nbg = `${sobj.us.total - sobj.us.bg} non-battleground`;
   let ussr_bg  = `${sobj.ussr.bg} battleground`;
@@ -16,13 +14,24 @@ console.log(JSON.stringify(sobj));
   if ((sobj.ussr.total-sobj.ussr.bg) != 1) { ussr_nbg += "s"; }
 
   if (sobj.us.vp > sobj.ussr.vp) { results_text = `US wins +${sobj.us.vp-sobj.ussr.vp} VP`; winner_css = "winner-us"; }
-  if (sobj.us.vp < sobj.ussr.vp) { results_text = `USSR wins +${sobj.ussr.vp-sobj.us.vp} VP`; winner_css = "winner_ussr"; }
+  if (sobj.us.vp < sobj.ussr.vp) { results_text = `USSR wins +${sobj.ussr.vp-sobj.us.vp} VP`; winner_css = "winner-ussr"; }
   if (sobj.bonus.length > 0) {
     bonus_events = `
       <div class="scoring-bonus-title">Bonus Points</div>  
       <div class="scoring-event-grid"></div>
     `;
   }
+
+  let usc = `US controls ${us_bg} and ${us_nbg} for ${sobj.us.status}`;
+  let ussrc = `USSR controls ${ussr_bg} and ${ussr_nbg} for ${sobj.ussr.status}`;
+
+  usc = usc.replace("and 0 non-battlegrounds ", "");
+  ussrc = ussrc.replace("and 0 non-battlegrounds ", "");
+  usc = usc.replace("0 battlegrounds and ", "");
+  ussrc = ussrc.replace("0 battlegrounds and ", "");
+  usc = usc.replace("controls 0 battlegrounds for None", "has No Presence");
+  ussrc = ussrc.replace("controls 0 battlegrounds for None", "has No Presence");
+
 
   let html = `
 
@@ -32,8 +41,8 @@ console.log(JSON.stringify(sobj));
      
         <div class="scoring-box">
           <div class="scoring-result-title">${results_text}</div>   
-	  <div class="scoring-text">US controls ${us_bg} and ${us_nbg} for ${sobj.us.status}</div>
-	  <div class="scoring-text">USSR controls ${ussr_bg} ${ussr_nbg} for ${sobj.ussr.status}</div>
+	  <div class="scoring-text">${usc}</div>
+	  <div class="scoring-text">${ussrc}</div>
           ${bonus_events}
 	</div>
 
