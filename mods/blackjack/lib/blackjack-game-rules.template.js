41166c9eafd0d1b1180224508669fa3d8536d31a
The1Duke <61957527+The1Duke@users.noreply.github.com>
2023-05-15 12:58:43 -0400

    Update blackjack-game-rules.template.js

--- mods/blackjack/lib/blackjack-game-rules.template.js
+++ mods/blackjack/lib/blackjack-game-rules.template.js
@@ -1,13 +1,43 @@
 module.exports = (app, mod) => {
 
-    return `<div class="rules-overlay">
-    <div class="h1">Homestyle Blackjack</div>
-    <p><strong>Homestyle Blackjack is quite different from Casino Blackjack. </strong></p>
-    <p>The game is played with a single deck (shuffled between each round) and <strong>each player takes turns as dealer</strong>, acting as the house against the other players. This means the dealer stakes the bets of all the other players. <strong>Each player is dealt a single card and given the chance to place a bet.</strong> After all the players (excluding the dealer) have placed their bets, one more card is dealt face up and gameplay begins.</p>
-    <p>Beginning to the left of the dealer, each player takes a turn, at which time all other players may view their full hand. Players may hit (take another card) or stand (end their turn). Players may hit as many times as they like, but they lose if they exceed 21 points (bust).
-    Cards are scored as usual per the number value and with J, Q, and K counting as 10. Aces count as either 1 or 11. If the player busts, the dealer immediately collects their bet and the player loses. The dealer is the last to play and may use their discretion, i.e. no mandatory casino rule of hitting below 17. If the dealer busts, remaining players win automatically. Any player with a higher score than the dealer wins their bet. <strong>The dealer wins all ties.</strong></p>
-    <p>Blackjacks--21 points with two cards--<strong>pay 2:1!</strong></p>
-    </div>`;
+    return `<div class="blackjack-rules-overlay">
+	<div class="h2">Blackjack Card Values</div>
+
+  <div class="blackjack-modal-table">
+	  <div class="blackjack-box-description">Numbered cards - Their worth is equal to their number 
+      <div class="blackjack-modal-cardbox">
+        <img class="bcard" src="/blackjack/img/cards/S2.png">
+        <img class="bcard" src="/blackjack/img/cards/S3.png">
+        <img class="bcard" src="/blackjack/img/cards/S4.png">
+        <img class="bcard" src="/blackjack/img/cards/S5.png">
+        <img class="bcard" src="/blackjack/img/cards/S6.png">
+        <img class="bcard" src="/blackjack/img/cards/S7.png">
+        <img class="bcard" src="/blackjack/img/cards/S8.png">
+        <img class="bcard" src="/blackjack/img/cards/S9.png">
+        <img class="bcard" src="/blackjack/img/cards/S10.png">
+      </div>
+
+    </div>
+		<div class="blackjack-box-description">Face Cards - They are count as a 10 
+			<div class="blackjack-modal-cardbox">
+				<img class="bcard" src="/blackjack/img/cards/D11.png">
+				<img class="bcard" src="/blackjack/img/cards/D12.png">
+				<img class="bcard" src="/blackjack/img/cards/D13.png">
+			</div>
+
+    </div>
+		<div class="blackjack-box-description">Aces - They count as either a 1 or a 10, whichever is better 
+			<div class="blackjack-modal-cardbox">
+				<img class="bcard" src="/blackjack/img/cards/H1.png">
+      </div>
+		</div>
+    <div class="blackjack-explanation">
+  The goal is to get the sum of your cards as close to 21 as possible. If you go over, you lose.<br>
+  You can hit to get a new card until you go over and double down to get a single card and double your bets.<br>
+  Players take turns to be the dealer. The dealer wins all ties.<br>
+    </div>
+	</div>
+</div>`;
 
 }
 
