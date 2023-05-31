4bb80b794577c4ad0a59e3c9e89f6d1d5352616f
arpee <richard@saito.tech>
2023-05-31 16:33:11 +1000

    fixed ore card

--- apps/core/hash-loader.js
+++ apps/core/hash-loader.js
@@ -1,4 +1,4 @@
-export default async (saito) => {
+export default async (saito) => {wa
   if (!saito.BROWSER) {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const blake3 = require("blake3");
