4763afdf173fdfd3344605786cbb92497157f887
arpee <richard@saito.tech>
2023-06-08 17:16:09 +1000

    chat icons swapped out and input height limited

--- mods/chat/lib/chat-manager/popup.template.js
+++ mods/chat/lib/chat-manager/popup.template.js
@@ -1,8 +1,10 @@
-
-module.exports = (app, mod, group, isStatic=false) => {
-
-  if (!group) { return ""; }
-  if (!group.name) { group.name = ""; }
+module.exports = (app, mod, group, isStatic = false) => {
+  if (!group) {
+    return "";
+  }
+  if (!group.name) {
+    group.name = "";
+  }
 
   let class_name = "chat-container";
 
@@ -15,22 +17,25 @@ module.exports = (app, mod, group, isStatic=false) => {
 
         <div class="chat-header" id="chat-header-${group.id}">
           <i  class="far fa-comment-dots"></i>
-          <div id="chat-group-${group.id}" class="chat-group active-chat-tab saito-address" data-id="${group.name}">${group.name}</div>
+          <div id="chat-group-${
+            group.id
+          }" class="chat-group active-chat-tab saito-address" data-id="${group.name}">${
+    group.name
+  }</div>
           <i id="chat-container-close" class="chat-container-close fas fa-times"></i>
         </div>
 
         <div class="chat-body">${mod.returnChatBody(group.id)}</div>
 
         <div class="chat-footer">
-          <div name="chat-input" class="chat-input" id="chat-input-${group.id}" type="text" value="" autocomplete="off" placeholder="Type something..." contenteditable="true"></div>
-          <i class="fas fa-paper-plane chat-input-submit" id="chat-input-submit"></i>
+          <div name="chat-input" class="chat-input hide-scrollbar" id="chat-input-${
+            group.id
+          }" type="text" value="" autocomplete="off" placeholder="Type something..." contenteditable="true"></div>
+          <i class="fa-regular fa-paper-plane chat-input-submit" id="chat-input-submit"></i>
         </div>
 
       </div>
   `;
 
   return html;
-
-}
-
-
+};
