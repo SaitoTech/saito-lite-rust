429ca717b512efeba360a5215216bf09072213de
David Lancashire <david.lancashire@gmail.com>
2023-06-13 08:02:49 +0700

    fix: shogun refactor

---
+++ mods/shogun/lib/overlays/attack.template.js
@@ -0,0 +1,11 @@
+module.exports = AttackOverlayTemplate = (title) => {
+
+  return `
+    <div class="attack-overlay">
+      <div class="h2">Attack! -- ${this.deck[card].name}</div>
+      <div class="overlay-img">${this.returnCardImage(card)}</div>
+      <div class="attack_details"></div>
+    </div>
+  `;
+
+}
