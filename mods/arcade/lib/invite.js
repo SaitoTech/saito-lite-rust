f1a0a6dcaa1aceb48a7250be5ac2de73954d63f4
davik20 <onyejivic@gmail.com>
2023-06-28 02:14:11 +0100

    update

--- mods/arcade/lib/invite.js
+++ mods/arcade/lib/invite.js
@@ -1,23 +1,21 @@
 const JoinGameOverlay = require("./overlays/join-game");
 const InviteTemplate = require("./invite.template");
 const InviteTemplateSparse = require("./invite.template.sparse");
-const JSON = require('json-bigint');
+const JSON = require("json-bigint");
 
 class Invite {
-	
-  constructor(app, mod, container, type, tx=null) {
-
+  constructor(app, mod, container, type, tx = null) {
     this.app = app;
     this.mod = mod;
     this.container = container;
     this.type = type;
 
     //
-    // information may be stored in different places, so we 
+    // information may be stored in different places, so we
     // save these variables in our invite, and use the invite
     // version to display our overlay templates.
     //
-    this.invite_data = { 
+    this.invite_data = {
       tx,
       game_id: "",
       game_name: "",
@@ -26,7 +24,7 @@ class Invite {
       players: [],
       players_needed: 0,
       desired_opponent_publickeys: [],
-      options: {}, 
+      options: {},
       game_mod: null,
       game_slug: "",
       game_type: "standard game",
@@ -40,50 +38,61 @@ class Invite {
     // if we have a valid tx, parse the data, so that the UI components have easy access to info to render
     //
     if (tx) {
-
       let txmsg = tx.returnMessage();
 
-      this.invite_data.game_id = tx.transaction.sig;
+      this.invite_data.game_id = tx.signature;
       this.invite_data.game_name = txmsg.game;
       this.invite_data.game_status = txmsg.request;
       this.invite_data.originator = txmsg.originator;
       this.invite_data.players = txmsg.players;
       this.invite_data.players_needed = txmsg.players_needed;
-      this.invite_data.time_created = tx.transaction.ts;
+      this.invite_data.time_created = tx.timestamp;
 
-      if (txmsg.winner) { this.invite_data.winner = txmsg.winner; }
-      if (txmsg.method) { this.invite_data.method = txmsg.method; }
-      if (txmsg.time_finished) { this.invite_data.time_finished = txmsg.time_finished; }
-      if (txmsg.step) {this.invite_data.step = txmsg.step; }
-      if (txmsg.ts) {this.invite_data.ts = txmsg.ts; }
+      if (txmsg.winner) {
+        this.invite_data.winner = txmsg.winner;
+      }
+      if (txmsg.method) {
+        this.invite_data.method = txmsg.method;
+      }
+      if (txmsg.time_finished) {
+        this.invite_data.time_finished = txmsg.time_finished;
+      }
+      if (txmsg.step) {
+        this.invite_data.step = txmsg.step;
+      }
+      if (txmsg.timestamp) {
+        this.invite_data.ts = txmsg.timestamp;
+      }
 
       let alt_game_type = "";
 
       //We still don't know the exact data structures for specified invite(s)
       //But it isn't going to be a single string pushed into an array!
-      if (txmsg.options?.desired_opponent_publickey){
-        if (!this.invite_data.players.includes(txmsg.options.desired_opponent_publickey)){
-          this.invite_data.desired_opponent_publickeys.push(txmsg.options.desired_opponent_publickey);            
+      if (txmsg.options?.desired_opponent_publickey) {
+        if (!this.invite_data.players.includes(txmsg.options.desired_opponent_publickey)) {
+          this.invite_data.desired_opponent_publickeys.push(
+            txmsg.options.desired_opponent_publickey
+          );
         }
 
         //Invitation / Challenge ?
-        if (app.wallet.returnPublicKey() == txmsg.options.desired_opponent_publickey){
-         alt_game_type = "direct invite ";
-         this.invite_data.game_type = "direct invite"; 
+        if (app.wallet.returnPublicKey() == txmsg.options.desired_opponent_publickey) {
+          alt_game_type = "direct invite ";
+          this.invite_data.game_type = "direct invite";
         }
       }
-     
+
       this.invite_data.options = txmsg.options;
 
       let game_mod = app.modules.returnModule(txmsg.game);
       if (game_mod) {
         this.invite_data.game_mod = game_mod;
-        this.invite_data.game_slug = game_mod.returnSlug();  
-        this.invite_data.game_name = game_mod.returnName();  
+        this.invite_data.game_slug = game_mod.returnSlug();
+        this.invite_data.game_name = game_mod.returnName();
       } else {
         this.invite_data.game_slug = this.invite_data.game_name.toLowerCase();
       }
-      
+
       //
       //Figure out what kind of game invite for convenient display
       //
@@ -94,16 +103,16 @@ class Invite {
       let defaultKeys = Object.keys(defaultOptions);
       let inviteKeys = Object.keys(txmsg.options);
 
-      if (defaultKeys.length == inviteKeys.length){
-        for (const key of defaultKeys){
-          if (defaultOptions[key] !== txmsg.options[key] && !key.includes("game-wizard-players")){
-            alt_game_type += "custom "
+      if (defaultKeys.length == inviteKeys.length) {
+        for (const key of defaultKeys) {
+          if (defaultOptions[key] !== txmsg.options[key] && !key.includes("game-wizard-players")) {
+            alt_game_type += "custom ";
             this.invite_data.game_type = "custom game";
             break;
           }
         }
-      }else{
-        alt_game_type += "custom "
+      } else {
+        alt_game_type += "custom ";
         this.invite_data.game_type = "custom game";
       }
 
@@ -114,42 +123,41 @@ class Invite {
       }
 
       //League
-      if (txmsg.options?.league_id){
+      if (txmsg.options?.league_id) {
         this.invite_data.league = txmsg.options.league_id;
         alt_game_type += "league ";
-        this.invite_data.game_type = "league game"; 
+        this.invite_data.game_type = "league game";
       }
 
       //Private (only shown to the originator)
-      if (txmsg.request === "private"){
+      if (txmsg.request === "private") {
         alt_game_type += "private ";
-        this.invite_data.game_type = "private game"; 
+        this.invite_data.game_type = "private game";
       }
       alt_game_type += "game";
-      if (alt_game_type == "game"){
+      if (alt_game_type == "game") {
         this.invite_data.verbose_game_type = "standard game open invitation";
-      }else{
+      } else {
         this.invite_data.verbose_game_type = alt_game_type;
       }
-
     }
 
     // calculate empty slots
-    this.invite_data.empty_slots = Math.max(0, this.invite_data.players_needed - this.invite_data.players.length);
+    this.invite_data.empty_slots = Math.max(
+      0,
+      this.invite_data.players_needed - this.invite_data.players.length
+    );
 
     // remove empty slots if any players are requested
     // because we will pre-fill in the invitees
     this.invite_data.empty_slots -= this.invite_data.desired_opponent_publickeys.length;
-
-
   }
-
 
   render() {
     let html = "";
-    if (this.type == "sparse"){
+    if (this.type == "sparse") {
       html = InviteTemplateSparse(this.app, this.mod, this.invite_data);
-    }else{
+    } else {
       html = InviteTemplate(this.app, this.mod, this.invite_data);
     }
 
@@ -161,24 +169,23 @@ class Invite {
     this.attachEvents();
   }
 
-
   attachEvents() {
-
     let qs = `#saito-game-${this.invite_data.game_id}`;
 
     document.querySelector(qs).onclick = (e) => {
       e.stopImmediatePropagation();
 
-      this.app.browser.logMatomoEvent("GameInvite", this.invite_data.game_status, this.invite_data.game_mod.name);
+      this.app.browser.logMatomoEvent(
+        "GameInvite",
+        this.invite_data.game_status,
+        this.invite_data.game_mod.name
+      );
       let game_overlay = new JoinGameOverlay(this.app, this.mod, this.invite_data);
       game_overlay.render();
 
-  	  return;
+      return;
     };
-
   }
-
-};
+}
 
 module.exports = Invite;
-
--- mods/arcade/lib/overlays/join-game.template.js
+++ mods/arcade/lib/overlays/join-game.template.js
@@ -1,25 +1,27 @@
 module.exports = JoinGameOverlayTemplate = (app, mod, invite) => {
-	if (mod.debug) {
-		//console.log("INVITATION DETAILS: ", invite);
-	}
+  if (mod.debug) {
+    //console.log("INVITATION DETAILS: ", invite);
+  }
 
-	//Uncreated games
-	let desc = invite.verbose_game_type;
-			//	invite?.desired_opponent_publickeys?.length > 0 || invite.game_status == "private"
-			//			? "private invitation"
-			//		: "open invitation";
-	//If created
-	if (mod.isAcceptedGame(invite.game_id)) {
-		desc = "active game";
-	}
-	if (invite.time_finished) {
-		desc = "finished game";
-	}
+  //Uncreated games
+  let desc = invite.verbose_game_type;
+  //	invite?.desired_opponent_publickeys?.length > 0 || invite.game_status == "private"
+  //			? "private invitation"
+  //		: "open invitation";
+  //If created
+  if (mod.isAcceptedGame(invite.game_id)) {
+    desc = "active game";
+  }
+  if (invite.time_finished) {
+    desc = "finished game";
+  }
 
-	let html = `
+  let html = `
   <div class="arcade-game-overlay">
   <div class="arcade-game-overlay-header">
-	  <div class="arcade-game-overlay-header-image" style="background-image: url('${invite.game_mod.respondTo("arcade-games").image}')">
+	  <div class="arcade-game-overlay-header-image" style="background-image: url('${
+      invite.game_mod.respondTo("arcade-games").image
+    }')">
 	  </div>
 	  <div class="arcade-game-overlay-header-title-box">
 		  <div class="arcade-game-overlay-header-title-box-title">${invite.game_name}</div>
@@ -31,45 +33,47 @@ module.exports = JoinGameOverlayTemplate = (app, mod, invite) => {
 	    <div class="arcade-game-players">
 	`;
 
-	// render players who have joined
-	for (let i = 0; i < invite.players.length; i++) {
-		html += `
+  // render players who have joined
+  for (let i = 0; i < invite.players.length; i++) {
+    html += `
 		  <div class="arcade-game-playerbox saito-table-row">
-		    <div class="saito-identicon-box${invite.winner?.includes(invite.players[i])?" winner":""}"><img class="saito-identicon" src="${app.keychain.returnIdenticon(
-					invite.players[i]
-				)}"></div>
+		    <div class="saito-identicon-box${
+          invite.winner?.includes(invite.players[i]) ? " winner" : ""
+        }"><img class="saito-identicon" src="${app.keychain.returnIdenticon(
+      invite.players[i]
+    )}"></div>
 		    ${app.browser.returnAddressHTML(invite.players[i])}
 		  </div>					  	  
 			`;
-	}
+  }
 
-	// render players who are requested to join (their slot isnt empty)
-	for (let i = 0; i < invite.desired_opponent_publickeys.length; i++) {
-		html += `
+  // render players who are requested to join (their slot isnt empty)
+  for (let i = 0; i < invite.desired_opponent_publickeys.length; i++) {
+    html += `
 
       <div class="arcade-game-playerbox empty saito-table-row requested_player">
 	      <div class="saito-identicon-box"><img class="saito-identicon" src="${app.keychain.returnIdenticon(
-					invite.desired_opponent_publickeys[i]
-				)}"></div>
+          invite.desired_opponent_publickeys[i]
+        )}"></div>
  	      ${app.browser.returnAddressHTML(invite.desired_opponent_publickeys[i])}
 	    </div>
 
       `;
-	}
+  }
 
-	// render empty slots; empty slots =  players needed - (players joined + players requested)
-	for (let i = 0; i < invite.empty_slots; i++) {
-		html += `
+  // render empty slots; empty slots =  players needed - (players joined + players requested)
+  for (let i = 0; i < invite.empty_slots; i++) {
+    html += `
 	        <div class="arcade-game-playerbox saito-table-row${
-						app.wallet.returnPublicKey() === invite.originator ? " available_slot" : ""
-					}">  
+            app.wallet.publicKey === invite.originator ? " available_slot" : ""
+          }">  
 	      		<div class="saito-identicon-box empty-slot"></div>
 	    			<div class="saito-address">open player slot</div>	
 	  			</div>
 		    `;
-	}
+  }
 
-	html += `  
+  html += `  
 		  	
 	      
 	      </div>
@@ -78,28 +82,28 @@ module.exports = JoinGameOverlayTemplate = (app, mod, invite) => {
 			  <div class="saito-table-body arcade-overlay-game-options">
 	`;
 
-	html += formatOptions(invite.game_mod.returnShortGameOptionsArray(invite.options));
-	if (invite.time_finished) {
+  html += formatOptions(invite.game_mod.returnShortGameOptionsArray(invite.options));
+  if (invite.time_finished) {
     let datetime = app.browser.formatDate(invite.time_finished);
     html += addTimeStamp("finished at", datetime);
-	} else if (invite.time_created) {
-		let datetime = app.browser.formatDate(invite.time_created);
+  } else if (invite.time_created) {
+    let datetime = app.browser.formatDate(invite.time_created);
     html += addTimeStamp("created at", datetime);
-	}
-	if (invite?.step >= 0){
-		html += `<div class="saito-table-row">
+  }
+  if (invite?.step >= 0) {
+    html += `<div class="saito-table-row">
               <div class="arcade-game-options-key">game moves</div>
 							<div class="arcade-game-options-value">${invite.step}</div>
-					</div>`
-	}
-	if (invite?.method){
-		html += `<div class="saito-table-row">
+					</div>`;
+  }
+  if (invite?.method) {
+    html += `<div class="saito-table-row">
               <div class="arcade-game-options-key">game ending</div>
 							<div class="arcade-game-options-value">${invite.method}</div>
-					</div>`
-	}
+					</div>`;
+  }
 
-	html += `
+  html += `
 			  </div>
 		  </div>
 	    </div>
@@ -107,61 +111,63 @@ module.exports = JoinGameOverlayTemplate = (app, mod, invite) => {
 	  </div>
 	  <div class="arcade-game-controls">`;
 
-	if (!invite.time_finished) {
-		if (mod.isAcceptedGame(invite.game_id)) {
-			if (mod.isMyGame(mod.returnGame(invite.game_id))) {
-				html += `<div id="arcade-game-controls-continue-game" class="saito-button saito-button-primary">continue game</div>`;
-				if (invite.players.length > 1) {
-					html += `<div id="arcade-game-controls-forfeit-game" class="saito-button saito-button-primary">forfeit game</div>`;
-				}
-				html += `<div id="arcade-game-controls-close-game" class="saito-button saito-button-primary">close game</div>`;
-			} else if (invite.game_mod.enable_observer) {
-				//Observer mode -- ongoing
-				html += `<div id="arcade-game-controls-watch-game" class="saito-button saito-button-primary">watch game</div>`;
-			}
-		} else {
-			if (invite.players.includes(app.wallet.returnPublicKey())) {
-				if (app.wallet.returnPublicKey() === invite.originator) {
-					html += `<div id="arcade-game-controls-cancel-join" class="saito-button saito-button-primary">cancel invite</div>`;
-				} else {
-					html += `<div id="arcade-game-controls-cancel-join" class="saito-button saito-button-primary">leave invite</div>`;
-				}
-			} else if (invite.empty_slots > 0 || invite.desired_opponent_publickeys.includes(app.wallet.returnPublicKey())) {
-				html += `<div id="arcade-game-controls-join-game" class="saito-button saito-button-primary">join game</div>`;
-			}
-		}
-	} else if (invite.game_mod.enable_observer && invite?.step > 0) {
-		//Observer mode -- finished
-		html += `<div id="arcade-game-controls-review-game" class="saito-button saito-button-primary">review game</div>`;
-	}
-	html += `
+  if (!invite.time_finished) {
+    if (mod.isAcceptedGame(invite.game_id)) {
+      if (mod.isMyGame(mod.returnGame(invite.game_id))) {
+        html += `<div id="arcade-game-controls-continue-game" class="saito-button saito-button-primary">continue game</div>`;
+        if (invite.players.length > 1) {
+          html += `<div id="arcade-game-controls-forfeit-game" class="saito-button saito-button-primary">forfeit game</div>`;
+        }
+        html += `<div id="arcade-game-controls-close-game" class="saito-button saito-button-primary">close game</div>`;
+      } else if (invite.game_mod.enable_observer) {
+        //Observer mode -- ongoing
+        html += `<div id="arcade-game-controls-watch-game" class="saito-button saito-button-primary">watch game</div>`;
+      }
+    } else {
+      if (invite.players.includes(app.wallet.publicKey)) {
+        if (app.wallet.publicKey === invite.originator) {
+          html += `<div id="arcade-game-controls-cancel-join" class="saito-button saito-button-primary">cancel invite</div>`;
+        } else {
+          html += `<div id="arcade-game-controls-cancel-join" class="saito-button saito-button-primary">leave invite</div>`;
+        }
+      } else if (
+        invite.empty_slots > 0 ||
+        invite.desired_opponent_publickeys.includes(app.wallet.publicKey)
+      ) {
+        html += `<div id="arcade-game-controls-join-game" class="saito-button saito-button-primary">join game</div>`;
+      }
+    }
+  } else if (invite.game_mod.enable_observer && invite?.step > 0) {
+    //Observer mode -- finished
+    html += `<div id="arcade-game-controls-review-game" class="saito-button saito-button-primary">review game</div>`;
+  }
+  html += `
 	  </div>
 </div>
   `;
 
-	return html;
+  return html;
 };
 
 const formatOptions = (sgoa) => {
-	let html = "";
-	let cnt = 1;
+  let html = "";
+  let cnt = 1;
 
-	for (let i in sgoa) {
-		html += `<div class="saito-table-row">
+  for (let i in sgoa) {
+    html += `<div class="saito-table-row">
                 <div class="arcade-game-options-key">${i.replace(/_/g, " ")}</div>`;
-		if (sgoa[i] !== null) {
-			html += `<div class="arcade-game-options-value">${sgoa[i]}</div>`;
-		}
-		html += "</div>";
-	}
+    if (sgoa[i] !== null) {
+      html += `<div class="arcade-game-options-value">${sgoa[i]}</div>`;
+    }
+    html += "</div>";
+  }
 
-	return html;
+  return html;
 };
 
 const addTimeStamp = (label, datetime) => {
-	return 	`<div class="saito-table-row">
+  return `<div class="saito-table-row">
               <div class="arcade-game-options-key">${label}</div>
 							<div class="arcade-game-options-value">${datetime.hours}:${datetime.minutes}, ${datetime.day} ${datetime.month}</div>
 					</div>`;
-
-}
+};
