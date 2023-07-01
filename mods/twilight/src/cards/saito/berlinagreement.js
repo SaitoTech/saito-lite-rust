2900da538bca41fda68d66832b6d73712b4bbc76
David Lancashire <david.lancashire@gmail.com>
2023-07-01 11:31:19 +0700

    feat: new ts cards

---
+++ mods/twilight/src/cards/saito/berlinagreement.js
@@ -0,0 +1,28 @@
+
+    if (card == "berlinagreement") {
+
+      this.game.state.event.berlinagreement = 1;
+
+      if (this.countries["westgermany"].us > 0){
+        this.countries["westgermany"].us--;
+        this.updateLog(`Berlin Agreement: 1 US influence removed in West Germany`);
+      } else {
+        this.updateLog(`Berlin Agreement: no US influence in West Germany to remove`);
+      }
+
+      if (this.countries["eastgermany"].ussr > 0){
+        this.countries["eastgermany"].ussr--;
+        this.updateLog(`Berlin Agreement: 1 USSR influence removed in East Germany`);
+      } else {
+        this.updateLog(`Berlin Agreement: no USSR influence in East Germany to remove`);
+      }
+       
+      this.game.state.events.berlinagreement = 1;
+
+      return 1;
+    }
+
+
+
+
+
---
+++ mods/twilight/src/cards/saito/iranianultimatum.js
@@ -0,0 +1,68 @@
+
+    if (card == "iranianultimatum") {
+
+      this.game.state.event.iranianultimatum = 1;
+
+      this.countries["iran"].us += 2;
+      this.updateLog(`${this.cardToText(card)}: 2 US influence added to Iran`);
+       
+
+      let ussrtroops = 0;
+      for (var i in this.countries) {
+        if (this.countries[i].region == "mideast") {
+          ussrtroops += this.countries[i].ussr;
+        }
+      }
+
+      if (ustroops == 0) {
+        this.updateLog("USSR has no influence in the Middle-East");
+        return 1;
+      }
+
+
+      if (this.game.player == 2) {
+
+        this.startClock();
+
+        this.addMove("resolve\tmarine");
+
+        var twilight_self = this;
+        twilight_self.playerFinishedPlacingInfluence();
+
+        var ops_available = 0;
+        for (var i in this.countries) {
+          if (this.countries[i].region == "mideast") {
+            if (this.countries[i].ussr > 0) {
+              $("#"+i).addClass("easterneurope");
+              this.countries[i].place = 1;
+            }
+          }
+        }
+
+        let ops_to_purge = Math.min(1, ops_available);
+
+        this.updateStatus("Remove 1 USSR influence from the Middle East");
+        
+        $(".easterneurope").off();
+        $(".easterneurope").on('click', function() {
+
+          let c = $(this).attr('id');
+
+          if (twilight_self.countries[c].place != 1 || twilight_self.countries[c].ussr == 0) {
+            twilight_self.displayModal("Invalid Country");
+          } else {
+            twilight_self.removeInfluence(c, 1, "ussr");
+            twilight_self.addMove("remove\tus\tussr\t"+c+"\t1");
+            twilight_self.playerFinishedPlacingInfluence();
+            twilight_self.endTurn();
+          }
+        });
+
+      }
+      return 0;
+    }
+
+
+
+
+
