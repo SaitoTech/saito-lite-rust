da06e29b47de1136af3730081faf0a25f6ca7f98
Daniel Worlton <f8daniel@gmail.com>
2023-07-10 10:55:50 +1000

    append http to www links in sanitize function so that the href opens correctly

--- lib/saito/browser.ts
+++ lib/saito/browser.ts
@@ -1449,7 +1449,7 @@ class Browser {
       let urlPattern =
         /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\z`!()\[\]{};:'".,<>?«»“”‘’]))/gi;
       text = text.replace(urlPattern, function (url) {
-        return `<a target="_blank" class="saito-treated-link" href="${url.trim()}">${url.trim()}</a>`;
+        return `<a target="_blank" class="saito-treated-link" href="${url.includes("www") && !url.includes("http") ? `http://${url.trim()}` : url.trim()}">${url.trim()}</a>`;
       });
 
       text = emoji.emojify(text);
