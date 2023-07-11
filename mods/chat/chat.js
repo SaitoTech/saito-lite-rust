87b28161876e6ab0e524e807143c52c873e7e95e
Daniel Worlton <f8daniel@gmail.com>
2023-07-11 11:13:27 +1000

    fix league page for mobile

--- mods/chat/chat.js
+++ mods/chat/chat.js
@@ -322,8 +322,10 @@ class Chat extends ModTemplate {
         //TODO:
         //Since the left-sidebar chat-manager disappears at screens less than 1200px wide
         //We need another way to display/open it...
-        if (this.app.browser.isMobileBrowser() || (this.app.BROWSER && window.innerWidth < 600)) {
-          chat_self.chat_manager.render_popups_to_screen = 0;
+        if (this.chat_manger && (this.app.browser.isMobileBrowser() || (this.app.BROWSER && window.innerWidth < 600))) {
+          
+          chat_self.chat_manager.render_popups_to_screen = 0;  
+          
           if (this.chat_manager_overlay == null) {
             this.chat_manager_overlay = new ChatManagerOverlay(this.app, this);
           }  
