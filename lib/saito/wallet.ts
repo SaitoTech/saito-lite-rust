702f6619cdae8df641b6fba64d7e1129ee2b9892
Sanka Darshana <sankadarshana@gmail.com>
2023-08-02 07:10:58 +0530

    merging to staging

--- lib/saito/wallet.ts
+++ lib/saito/wallet.ts
@@ -20,7 +20,7 @@ export default class Wallet extends SaitoWallet {
 
   default_fee = 2;
   version = 5.1;
-
+  cryptos = new Map<string, any>();
   public saitoCrypto: any;
 
   public async createUnsignedTransactionWithDefaultFee(
