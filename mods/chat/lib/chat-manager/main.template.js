0bc6c2e46aaa0c869c2c47653595cc30e7d17d29
Daniel Worlton <f8daniel@gmail.com>
2023-08-08 12:45:25 +1200

    enable system notifications for chat

--- mods/chat/lib/chat-manager/main.template.js
+++ mods/chat/lib/chat-manager/main.template.js
@@ -4,8 +4,9 @@ module.exports = ChatManagerTemplate = (app, mod) => {
   <div class="chat-manager hide-scrollbar">
     <div class="chat-manager-header">
       <div class="close-chat-manager mobile-overlay-only"><i class="fa-solid fa-arrow-left"></i></div>
-      <div class="refresh-contacts"><i class="fa-solid fa-rotate-right"></i></div>
-      <div class="add-contacts"><i class="fa-solid fa-plus"></i></div>
+      ${("Notification" in window) ? `<div class="toggle-notifications" title="enable notifications"><i class="fa-solid fa-bell-slash"></i></div>` : ""}
+      <div class="refresh-contacts" title="refresh contacts"><i class="fa-solid fa-rotate-right"></i></div>
+      <div class="add-contacts" title="create new chat group"><i class="fa-solid fa-plus"></i></div>
     </div>
     <div class="chat-manager-list">
     </div>
