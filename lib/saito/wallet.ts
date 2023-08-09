303537c51b145109890e4cb4883b598555c612c1
arpee <richard@saito.tech>
2023-08-09 14:42:31 +1000

    wallet ug

--- lib/saito/wallet.ts
+++ lib/saito/wallet.ts
@@ -19,7 +19,7 @@ export default class Wallet extends SaitoWallet {
   preferred_txs = [];
 
   default_fee = 2;
-  version = 5.210;
+  version = 5.211;
   cryptos = new Map<string, any>();
   public saitoCrypto: any;
 
