c87b8e0a1b77207f7f279edaa03f78ee618bb1b6
Umair Khan <umair@saito.tech>
2023-09-23 04:58:41 +0500

    Tool for generating spam tx for testing purposes

---
+++ mods/spam/spam.js
@@ -0,0 +1,152 @@
+const ModTemplate = require("./../../lib/templates/modtemplate");
+const PeerService = require("saito-js/lib/peer_service").default;
+
+class Spam extends ModTemplate {
+
+  constructor(app) {
+
+    super(app);
+
+    this.app = app;
+    this.slug = 'spam';
+    this.name = "spam";
+    this.description = "Tool to generate spam txs";
+    this.categories = "Core Utilities Messaging";
+    this.publickey = "";
+    this.loop_start = 0;
+    this.frequency = 1; //no of tx per second
+    this.interval = null;
+    this.loop_count = 0;
+  
+    this.styles = ['/spam/style.css'];
+
+    return this;
+  }
+
+
+  initialize(app) {
+    super.initialize(app);
+    if (app.BROWSER == 1) { this.attachStyleSheets(); }
+  }
+
+  async render() {
+
+    if (!this.browser_active) { return; }
+    let this_mod = this;
+
+    let start = document.querySelector(".start");
+    start.onclick = (e) => {
+
+    	this_mod.frequency = document.querySelector("#frequency").value;
+
+    	document.querySelector(".loop-count").innerHTML = this_mod.loop_count;
+    	document.querySelector(".loop-dot").style.backgroundColor = "green";
+
+    	this_mod.loop_start = 1;
+    	this_mod.changeLoopStatus();
+    }
+
+    let stop = document.querySelector(".stop");
+    stop.onclick = (e) => {
+
+    	this_mod.loop_start = 0;
+    	document.querySelector(".loop-dot").style.backgroundColor = "red";
+    	this_mod.changeLoopStatus();
+    }
+
+    let reset = document.querySelector(".reset");
+    reset.onclick = (e) => {
+
+    	this_mod.loop_start = 0;
+	    this_mod.frequency = 1;
+	    this_mod.interval = null;
+	    this_mod.loop_count = 0;
+
+    	document.querySelector(".loop-count").innerHTML = "0";
+    	document.querySelector(".loop-dot").style.backgroundColor = "red";
+    	document.querySelector("#frequency").value = 1;
+    	this_mod.changeLoopStatus();
+    }
+
+  }
+
+  changeLoopStatus(){
+  	let this_mod = this;
+  	if (this.loop_start == 1) {
+
+  		console.log("starting loop ..");
+  		console.log("txs per second: " + Math.floor(1000/this_mod.frequency));
+
+			this.interval = setInterval(function(){
+				document.querySelector(".loop-count").innerHTML = this_mod.loop_count;
+				this_mod.sendSpamTransaction(this_mod.app, this_mod.mod, {tx_num: this_mod.loop_count});
+				this_mod.loop_count++;
+			}, Math.floor(1000/this_mod.frequency));
+
+  	} else {
+
+  		console.log("stop loop ..");
+
+  		clearInterval(this.interval);
+  		this.interval = null;
+
+  	}
+  }
+
+  async onConfirmation(blk, tx, conf) {
+    let txmsg = tx.returnMessage();
+    try {
+      if (conf == 0) {
+        if (txmsg.request === "send spam tx") {
+          await this.receiveSpamTransaction(blk, tx, conf);
+        }
+      }
+    } catch (err) {
+      console.log("ERROR in " + this.name + " onConfirmation: " + err);
+    }
+  }
+
+  async sendSpamTransaction(app, mod, data) {
+    let obj = {
+      module: this.name,
+      request: "send spam tx",
+      data: {},
+    };
+    for (let key in data) {
+      obj.data[key] = data[key];
+    }
+
+    
+
+    let newtx = await this.app.wallet.createUnsignedTransaction();
+    newtx.msg = obj;
+    await newtx.sign();
+    console.log('tx: ' + data.tx_num);
+    console.log(newtx);
+    await this.app.network.propagateTransaction(newtx);
+
+    return newtx;
+  }
+
+  async receiveSpamTransaction(blk, tx, conf) {
+   	try {
+	    //
+	    // browsers
+	    //
+	    if (this.app.BROWSER == 1) {
+	      return;
+	    }
+
+	    console.log("Received tx: ");
+	    console.log(tx);
+
+  	} catch (err) {
+  		console.log("ERROR in saving migration data to db: " + err);
+  	}
+  }
+
+}
+
+module.exports = Spam;
+
+
