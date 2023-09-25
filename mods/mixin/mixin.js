3c8b366bfd0a81e3b91952cbbfcbb19269eae2d2
arpee <richard@saito.tech>
2023-09-25 10:18:03 +1000

    removing private credentials from git

--- mods/mixin/mixin.js
+++ mods/mixin/mixin.js
@@ -115,12 +115,9 @@ class Mixin extends ModTemplate {
         if (typeof process.env.MIXIN != "undefined") {
           m = JSON.parse(process.env.MIXIN);
         } else {
-          m = {
-            appId: "9be2f213-ca9d-4573-80ca-3b2711bb2105",
-            sessionId: "f072cd2a-7c81-495c-8945-d45b23ee6511",
-            privateKey:
-              "dN7CgCxWsqJ8wQpQSaSnrE0eGsToh7fntBuQ5QvVnguOdDbcNZwAMwsF-57MtJPtnlePrNSe7l0VibJBKD62fg",
-          };
+          // to develop locally please request a mixin key and add it as an
+          // enviromnental variable 'MIXIN'
+          console.error("MIXIN ENV variable missing.");
         }
 
         if (m.appId) {
