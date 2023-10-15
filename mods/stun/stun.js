886ae8cce5eef949cb885f00b64cc5e1a78857f5
davik20 <onyejivic@gmail.com>
2023-10-15 10:16:56 +0100

    update data

--- mods/stun/stun.js
+++ mods/stun/stun.js
@@ -412,11 +412,11 @@ class Stun extends ModTemplate {
     newtx.msg.data = _data;
     await newtx.sign();
 
-    // let data = {
-    //   recipient: server.publicKey,
-    //   request: "stun-message-broadcast",
-    //   data: newtx.toJson(),
-    // };
+    let data = {
+      recipient: server.publicKey,
+      request: "stun-message-broadcast",
+      data: newtx.toJson(),
+    };
 
     this.app.connection.emit("relay-send-message", data);
     // await this.app.network.propagateTransaction(newtx);
