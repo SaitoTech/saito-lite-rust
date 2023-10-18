469f165c5c562aaef31a76668931f2d55635f749
David Lancashire <david.lancashire@gmail.com>
2023-10-18 11:58:55 +0700

    fix: naval battles in HIS

---
+++ mods/his/lib/ui/overlays/naval-battle.template.js
@@ -0,0 +1,25 @@
+module.exports = (players=2) => {
+ 
+  let help = 'Naval Battle'; 
+ 
+  let html = `
+      <div class="naval-battle-overlay" id="naval-battle-overlay">
+	<div class="help">${help}</div>
+	<div class="naval-battle-grid">
+	  <div class="attacker">
+	    <div class="title">attacker</div>
+	  </div>
+	  <div class="defender">
+	    <div class="title">defender</div>
+	  </div>
+	</div>
+	</div>
+      </div>
+  `;
+  return html;
+
+}
+
+
+
+
