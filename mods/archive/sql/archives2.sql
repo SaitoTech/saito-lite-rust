6d7755f421c861ff306fe161a76a8f1b5604c13e
Daniel Worlton <f8daniel@gmail.com>
2023-10-27 16:01:49 +0800

    suggested archive changes

--- mods/archive/sql/archives2.sql
+++ mods/archive/sql/archives2.sql
@@ -1,7 +1,7 @@
-CREATE INDEX sig_search_idx ON modules (id, tx_id, sig);
-CREATE INDEX sig_owner_search_idx ON archive (id, tx_id, owner, sig);
-CREATE INDEX field_search_idx ON archive (id, tx_id, field1, field2, field3, owner);
-CREATE INDEX time_search_idx ON archive (id, created_at, updated_at, preserve, owner);
+CREATE INDEX sig_search_idx ON archive (sig);
+CREATE INDEX sig_owner_search_idx ON archive (owner, sig);
+CREATE INDEX field_search_idx ON archive (field1, field2, field3, owner);
+CREATE INDEX time_search_idx ON archive (created_at, updated_at, preserve, owner);
 CREATE INDEX field1_search_idx on archive (field1, owner);
 CREATE INDEX field2_search_idx on archive (field2, owner);
 CREATE INDEX field3_search_idx on archive (field3, owner);
