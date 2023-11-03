c0e74c22432569de7d07ca39742b190e93066dd8
Daniel Worlton <f8daniel@gmail.com>
2023-11-03 10:26:10 +0800

    rectify archive indices

--- mods/archive/sql/archives2.sql
+++ mods/archive/sql/archives2.sql
@@ -1,8 +1,12 @@
 CREATE INDEX sig_search_idx ON archives (sig);
 CREATE INDEX sig_owner_search_idx ON archives (owner, sig);
-CREATE INDEX field_search_idx ON archives (field1, field2, field3, owner);
-CREATE INDEX time_search_idx ON archives (created_at, updated_at, preserve, owner);
-CREATE INDEX field1_search_idx on archives (field1, owner);
-CREATE INDEX field2_search_idx on archives (field2, owner);
-CREATE INDEX field3_search_idx on archives (field3, owner);
+CREATE INDEX field1_search_idx on archives (field1);
+CREATE INDEX field2_search_idx on archives (field2);
+CREATE INDEX field3_search_idx on archives (field3);
+CREATE INDEX field1_time_search_idx on archives (field1, created_at);
+CREATE INDEX field2_time_search_idx on archives (field2, created_at);
+CREATE INDEX field3_time_search_idx on archives (field3, created_at);
+CREATE INDEX field1_with_owner_search_idx on archives (field1, owner, created_at);
+CREATE INDEX field2_with_owner_search_idx on archives (field2, owner, created_at);
+CREATE INDEX field3_with_owner_search_idx on archives (field3, owner, created_at);
 
