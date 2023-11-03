94faa06487c936a33c1e1b24fb82fbb80cf691dc
arpee <richard@saito.tech>
2023-11-03 17:09:06 +1100

    sql file management for archive

--- mods/archive/sql/archives2.sql
+++ mods/archive/sql/archives2.sql
@@ -5,4 +5,3 @@ CREATE INDEX field3_idx on archives (field3);
 CREATE INDEX created_at_idx on archives (created_at);
 CREATE INDEX updated_at_idx on archives (updated_at);
 CREATE INDEX owner_idx on archives (owner);
-
