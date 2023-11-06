e327772498ac4c0560df692e1663358a9831e085
Daniel Worlton <f8daniel@gmail.com>
2023-11-06 11:32:49 +1030

    open internal links in same tab

--- lib/saito/browser.ts
+++ lib/saito/browser.ts
@@ -1449,7 +1449,7 @@ class Browser {
       let urlPattern =
         /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\z`!()\[\]{};:'".,<>?«»“”‘’]))/gi;
       text = text.replace(urlPattern, function (url) {
-        return `<a target="_blank" class="saito-treated-link" href="${
+        return `<a ${url.includes(window.location.host)?"":'target="_blank" '}class="saito-treated-link" href="${
           url.includes("www") && !url.includes("http") ? `http://${url.trim()}` : url.trim()
         }">${url.trim()}</a>`;
       });
