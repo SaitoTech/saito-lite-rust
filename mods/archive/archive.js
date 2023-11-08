05b75f23454c6c0b1603d92d0f37dcfeff68b685
Daniel Worlton <f8daniel@gmail.com>
2023-11-08 11:28:33 +1030

    archive update

--- mods/archive/archive.js
+++ mods/archive/archive.js
@@ -20,6 +20,15 @@ const PeerService = require("saito-js/lib/peer_service").default;
 //    ---> peers receive by handlePeerTransaction();
 //    ---> peers fetch from DB, return via callback or return TX
 //
+
+/*CREATE INDEX sig_idx ON archives (sig);
+CREATE INDEX field1_idx on archives (field1);
+CREATE INDEX field2_idx on archives (field2);
+CREATE INDEX field3_idx on archives (field3);
+CREATE INDEX created_at_idx on archives (created_at);
+CREATE INDEX updated_at_idx on archives (updated_at);
+CREATE INDEX owner_idx on archives (owner);*/
+
 class Archive extends ModTemplate {
   constructor(app) {
     super(app);
