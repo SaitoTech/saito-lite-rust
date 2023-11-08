4a334c445133fef060ed446500db52b8a8223cee
Umair Khan <umair@saito.tech>
2023-11-08 18:42:01 +0500

    RS improvements and bug fixes

--- mods/redsquare/lib/tweet.js
+++ mods/redsquare/lib/tweet.js
@@ -745,6 +745,7 @@ class Tweet {
         );
         new_tweet.show_controls = 0;
         new_tweet.render();
+        document.querySelector("#post-tweet-textarea").focus();
       };
 
       /////////////
