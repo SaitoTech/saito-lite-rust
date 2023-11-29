5fb756c2e2c4bbcd3aacc955182c71ec19c71e37
Daniel Worlton <f8daniel@gmail.com>
2023-11-29 17:08:14 +0700

    improve chat popup resizing

--- mods/chat/lib/chat-manager/popup.js
+++ mods/chat/lib/chat-manager/popup.js
@@ -12,16 +12,13 @@ class ChatPopup {
     this.manually_closed = false;
     this.is_rendered = false;
 
-    this.width = "";
-    this.height = "";
-
     this.group = null;
 
     this.is_scrolling = false;
 
     this.overlay = new SaitoOverlay(app, mod);
 
-    this.dimensions = {}
+    this.dimensions = {};
 
     app.connection.on("chat-remove-fetch-button-request", (group_id) => {
       if (this.group?.id === group_id) {
@@ -189,97 +186,111 @@ class ChatPopup {
 
     let chatPopup = document.querySelector(".chat-container" + popup_qs);
 
-    if (chatPopup) {
-      chatPopup.onmouseover = (e) => {
-        document.querySelectorAll(".chat-container").forEach((el) => {
-          el.classList.remove("active");
-        });
-        e.currentTarget.classList.add("active");
-      };
+    if (!chatPopup) {
+      return;
+    }
 
-      if (!this.mod.chat_manager_overlay) {
-        //
-        // make draggable and resizable, but no in mobile/main - page
-        //
-        this.app.browser.makeDraggable(popup_id, header_id, true);
-        this.app.browser.makeResizeable(popup_qs, header_qs, group_id, () => {
-          let chat_bubble = document.querySelector(`${popup_qs} .chat-header .chat-minimizer-icon`);
-          if (chat_bubble) {
-            chat_bubble.classList.add("fa-window-minimize");
-            chat_bubble.classList.remove("fa-window-restore");
-            this.restorePopup(chatPopup);
-          }
-        });
-      }
+    chatPopup.onmouseover = (e) => {
+      document.querySelectorAll(".chat-container").forEach((el) => {
+        el.classList.remove("active");
+      });
+      e.currentTarget.classList.add("active");
+    };
 
+    if (!this.mod.chat_manager_overlay) {
       //
-      // minimize
-      let chat_bubble = document.querySelector(`${popup_qs} .chat-header .chat-minimizer-icon`);
-      if (chat_bubble) {
-        if (!this.mod.chat_manager_overlay) {
-          chat_bubble.onclick = (e) => {
-            if (chatPopup.classList.contains("minimized")) {
-              this.restorePopup(chatPopup);
-            } else {
-              //this.width = chatPopup.style.width;
-              this.height = chatPopup.style.height;
-              //chatPopup.style.width = "";
-              chatPopup.style.height = "";
-              chatPopup.classList.add("minimized");
-              chatPopup.classList.remove("active");
-            }
-          };
+      // make draggable and resizable, but no in mobile/main - page
+      //
+      this.app.browser.makeDraggable(popup_id, header_id, true);
+      this.app.browser.makeResizeable(popup_qs, header_qs, group_id, () => {
+        let chat_bubble = document.querySelector(`${popup_qs} .chat-header .chat-minimizer-icon`);
+        if (chat_bubble) {
+          chat_bubble.classList.add("fa-window-minimize");
+          chat_bubble.classList.remove("fa-window-restore");
+          this.restorePopup(chatPopup);
         }
-      }
+      });
+    }
 
+    //
+    // minimize
+    let chat_bubble = document.querySelector(`${popup_qs} .chat-header .chat-minimizer-icon`);
+    let mximize_icon = document.querySelector(`${popup_qs} .chat-header .chat-maximizer-icon`);
+
+    if (chat_bubble && mximize_icon && !this.mod.chat_manager_overlay) {
+      chat_bubble.onclick = (e) => {
+        if (chatPopup.classList.contains("minimized")) {
+          chat_bubble.classList.add("fa-window-minimize");
+          chat_bubble.classList.remove("fa-window-restore");
+          this.restorePopup(chatPopup);
+        } else {
+          if (chatPopup.classList.contains("maximized")) {
+            mximize_icon.classList.add("fa-square");
+            mximize_icon.classList.remove("fa-window-restore");
+            chatPopup.classList.remove("maximized");
+          } else {
+            //only update if not also maximized
+            this.savePopupDimensions(chatPopup);
+          }
+
+          chatPopup.style.top = "";
+          chatPopup.style.left = "";
+          chatPopup.style.bottom = "";
+          chatPopup.style.right = "";
+          chatPopup.style.width = "";
+          chatPopup.style.height = "";
+
+          chatPopup.classList.add("minimized");
+          chatPopup.classList.remove("active");
+          chat_bubble.classList.remove("fa-window-minimize");
+          chat_bubble.classList.add("fa-window-restore");
+          chatPopup.querySelector(".resize-icon").style.display = "none";
+        }
+      };
 
       //
       // maximize
-      let mximize_icon = document.querySelector(`${popup_qs} .chat-header .chat-maximizer-icon`);
-      if (mximize_icon) {
-        if (!this.mod.chat_manager_overlay) {
-          mximize_icon.onclick = (e) => {
-            
-            if (chatPopup.classList.contains("maximized")) {
-              chatPopup.style.width = this_self.dimensions.width + 'px';
-              chatPopup.style.height = this_self.dimensions.height + 'px';
-              chatPopup.style.left = this_self.dimensions.left + 'px';;
-              chatPopup.style.top = this_self.dimensions.top + 'px';;
 
-              chatPopup.classList.remove("maximized");
-              document.querySelector(".resize-icon").style.display = 'block';
-           } else {
-              this_self.savePopupDimensions(chatPopup);
+      mximize_icon.onclick = (e) => {
+        if (chatPopup.classList.contains("maximized")) {
+          mximize_icon.classList.add("fa-square");
+          mximize_icon.classList.remove("fa-window-restore");
+          this.restorePopup(chatPopup);
+        } else {
+          if (chatPopup.classList.contains("minimized")) {
+            chat_bubble.classList.add("fa-window-minimize");
+            chat_bubble.classList.remove("fa-window-restore");
+            chatPopup.classList.remove("minimized");
+          }else{
+            this.savePopupDimensions(chatPopup);            
+          }
 
-              chatPopup.style.width = '800px';
-              chatPopup.style.height = window.innerHeight + 'px';
+          mximize_icon.classList.remove("fa-square");
+          mximize_icon.classList.add("fa-window-restore");
 
-              // decide to maximize to left or right
-              if (this.dimensions.left < Math.floor(window.innerWidth/2)) {
-                chatPopup.style.left = '0px';  
-              } else {
-                chatPopup.style.left = (window.innerWidth-800) + 'px';
-              }
+          chatPopup.style.width = "800px";
+          chatPopup.style.height = window.innerHeight + "px";
 
-              chatPopup.style.top = '0px';
+          // decide to maximize to left or right
+          if (this.dimensions.left < Math.floor(window.innerWidth / 2)) {
+            chatPopup.style.left = "0px";
+          } else {
+            chatPopup.style.left = window.innerWidth - 800 + "px";
+          }
 
-              chatPopup.classList.add("maximized");
-              document.querySelector(".resize-icon").style.display = 'none';
-            }
+          chatPopup.style.top = "0px";
 
-
-          };
+          chatPopup.classList.add("maximized");
+          chatPopup.querySelector(".resize-icon").style.display = "none";
         }
-      }
-
+      };
     }
-
     // add reply functionality
 
     document.querySelectorAll(`${popup_qs} .saito-userline-reply .chat-reply`).forEach((el) => {
       el.addEventListener("click", (e) => {
         let src_obj = el.parentElement.parentElement.parentElement;
-        
+
         let quote = "";
 
         for (let child of el.parentElement.parentElement.childNodes) {
@@ -292,7 +303,7 @@ class ChatPopup {
           }
         }
 
-        if (quote.length > 30){
+        if (quote.length > 30) {
           quote = "..." + quote.slice(-30);
         }
 
@@ -324,7 +335,11 @@ class ChatPopup {
             text += child.textContent;
           }
           //We may want to also pull inner text from element nodes as long as they aren't the hidden buttons
-          if (child.nodeType === 1 && !child.classList.contains("saito-userline-reply") && child.nodeName !== "BLOCKQUOTE") {
+          if (
+            child.nodeType === 1 &&
+            !child.classList.contains("saito-userline-reply") &&
+            child.nodeName !== "BLOCKQUOTE"
+          ) {
             text += child.innerText;
           }
         }
@@ -365,102 +380,107 @@ class ChatPopup {
       });
     }
 
-      //
-      // close
-      //
-      document.querySelector(`${popup_qs} .chat-header .chat-container-close`).onclick = (e) => {
-        this.manually_closed = true;
-        this.is_rendered = false;
-        document.querySelector(`${popup_qs}`).remove();
-        this.app.connection.emit("chat-manager-render-request");
-        app.storage.saveOptions();
-      };
+    //
+    // close
+    //
+    document.querySelector(`${popup_qs} .chat-header .chat-container-close`).onclick = (e) => {
+      this.manually_closed = true;
+      this.is_rendered = false;
+      document.querySelector(`${popup_qs}`).remove();
+      this.app.connection.emit("chat-manager-render-request");
+      app.storage.saveOptions();
+    };
 
-      //
-      // submit
-      //
-      this.input.callbackOnReturn = async (message) => {
-        if (message.trim() == `${this.input.quote}`){
-          console.log("Reply with no content");
-          return;
-        }
+    //
+    // submit
+    //
+    this.input.callbackOnReturn = async (message) => {
+      if (message.trim() == `${this.input.quote}`) {
+        console.log("Reply with no content");
+        return;
+      }
 
-        let new_msg = message.replaceAll("&nbsp;", " ").replaceAll("<br>", " ");
-        if (new_msg.trim() == "") {
-          return;
-        }
+      let new_msg = message.replaceAll("&nbsp;", " ").replaceAll("<br>", " ");
+      if (new_msg.trim() == "") {
+        return;
+      }
 
-        let newtx = await mod.createChatTransaction(group_id, message, this.input.getMentions());
-        await mod.sendChatTransaction(app, newtx);
-        mod.receiveChatTransaction(newtx);
-        this.input.clear();
-        if (document.querySelector(popup_qs + " .chat-body")) {
-          this.is_scrolling = false;
-          document.querySelector(popup_qs + " .chat-body").scroll(0, 1000000000);
-        }
-      };
+      let newtx = await mod.createChatTransaction(group_id, message, this.input.getMentions());
+      await mod.sendChatTransaction(app, newtx);
+      mod.receiveChatTransaction(newtx);
+      this.input.clear();
+      if (document.querySelector(popup_qs + " .chat-body")) {
+        this.is_scrolling = false;
+        document.querySelector(popup_qs + " .chat-body").scroll(0, 1000000000);
+      }
+    };
 
-      this.input.callbackOnUpload = (filesrc) => {
+    this.input.callbackOnUpload = (filesrc) => {
+      let img = document.createElement("img");
+      img.classList.add("img-prev");
+      img.src = filesrc;
+      let msg = img.outerHTML;
+      this.input.callbackOnReturn(msg);
+    };
+
+    //
+    // submit (button)
+    //
+    document.querySelector(`${popup_qs} .chat-footer .chat-input-submit`).onclick = (e) => {
+      this.input.callbackOnReturn(this.input.getInput());
+    };
+
+    //
+    // drag and drop images into chat window
+    //
+
+    app.browser.addDragAndDropFileUploadToElement(
+      popup_id,
+      async (filesrc) => {
+        filesrc = await app.browser.resizeImg(filesrc); // (img, dimensions, quality)
+
         let img = document.createElement("img");
         img.classList.add("img-prev");
         img.src = filesrc;
         let msg = img.outerHTML;
-        this.input.callbackOnReturn(msg);
+
+        let newtx = await mod.createChatTransaction(group_id, img.outerHTML); // img into msg
+        await mod.sendChatTransaction(app, newtx);
+        mod.receiveChatTransaction(newtx);
+        this.input.clear();
+      },
+      false
+    ); // false = no drag-and-drop image click
+
+    document.querySelectorAll(`.img-prev`).forEach(function (img, key) {
+      img.onclick = (e) => {
+        e.preventDefault();
+
+        let img = e.currentTarget;
+        let src = img.getAttribute("src");
+
+        this_self.overlay.show(`<img class="chat-popup-img-enhanced" src="${src}" >`);
       };
-
-      //
-      // submit (button)
-      //
-      document.querySelector(`${popup_qs} .chat-footer .chat-input-submit`).onclick = (e) => {
-        this.input.callbackOnReturn(this.input.getInput());
-      };
-
-      //
-      // drag and drop images into chat window
-      //
-
-      app.browser.addDragAndDropFileUploadToElement(
-        popup_id,
-        async (filesrc) => {
-          filesrc = await app.browser.resizeImg(filesrc); // (img, dimensions, quality)
-
-          let img = document.createElement("img");
-          img.classList.add("img-prev");
-          img.src = filesrc;
-          let msg = img.outerHTML;
-
-          let newtx = await mod.createChatTransaction(group_id, img.outerHTML); // img into msg
-          await mod.sendChatTransaction(app, newtx);
-          mod.receiveChatTransaction(newtx);
-          this.input.clear();
-        },
-        false
-      ); // false = no drag-and-drop image click
-
-      document.querySelectorAll(`.img-prev`).forEach(function (img, key) {
-        img.onclick = (e) => {
-          e.preventDefault();
-
-          let img = e.currentTarget;
-          let src = img.getAttribute("src");
-
-          this_self.overlay.show(`<img class="chat-popup-img-enhanced" src="${src}" >`);
-        };
-      });
+    });
   }
 
   restorePopup(chatPopup) {
     chatPopup.classList.remove("minimized");
+    chatPopup.classList.remove("maximized");
     chatPopup.classList.add("active");
-    if (this.width) {
-      chatPopup.style.width = this.width;
-    }
-    if (this.height) {
-      chatPopup.style.height = this.height;
-    }
+
+    chatPopup.style.width = this.dimensions.width + "px";
+    chatPopup.style.height = this.dimensions.height + "px";
+    chatPopup.style.left = this.dimensions.left + "px";
+    chatPopup.style.top = this.dimensions.top + "px";
+
+    chatPopup.style.bottom = "unset";
+    chatPopup.style.right = "unset";
+
+    chatPopup.querySelector(".resize-icon").style.display = "block";
   }
 
-  savePopupDimensions(chatPopup){
+  savePopupDimensions(chatPopup) {
     this.dimensions = chatPopup.getBoundingClientRect();
   }
 }
