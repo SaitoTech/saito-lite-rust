ac6b0684b8953093a8df1595da2185e394b35fd8
arpee <richard@saito.tech>
2023-12-18 21:08:34 +1100

    tweak to mult image rendering

--- mods/redsquare/lib/image.template.js
+++ mods/redsquare/lib/image.template.js
@@ -1,8 +1,12 @@
 module.exports = (app, mod, images) => {
   let imgs = ``;
   let tweet_picture_height = "450px";
+  let multi_max_height = "unset";
   if (images.length > 1) {
     tweet_picture_height = "200px"
+  }
+  if (image.length > 2) {
+    multi_max_height = "150px";
   }
 
   if (images.length > 0) {
@@ -11,5 +15,5 @@ module.exports = (app, mod, images) => {
     }
   }
 
-  return `<div class="tweet-picture"><div class="saito-img-flex" style="--image-height:${tweet_picture_height}">${imgs}</div></div>`;
+  return `<div class="tweet-picture"><div class="saito-img-flex" style="--image-height:${tweet_picture_height};--multi-max-height:${multi_max_height}">${imgs}</div></div>`;
 };
