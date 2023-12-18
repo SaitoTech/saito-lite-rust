2683c57e807282462c260f8d3a367975562c99a7
arpee <richard@saito.tech>
2023-12-19 00:10:14 +1100

    added two classes: saito-highlight and saito-mouseover

--- mods/redsquare/lib/menu.template.js
+++ mods/redsquare/lib/menu.template.js
@@ -6,21 +6,21 @@ module.exports = () => {
         <div id="new-tweet" class="saito-button-primary">New Post</div>
 
         <ul class="saito-menu-list saito-sidebar-element">
-          <li class="redsquare-menu-home">
+          <li class="redsquare-menu-home saito-mouseover">
             <i class="fa-solid fa-house"></i>
             <span>Home</span>
 
           </li>
-          <li class="redsquare-menu-notifications">
+          <li class="redsquare-menu-notifications saito-mouseover">
             <i class="fas fa-bell"></i>
             <span>Notifications</span>
           </li>
-          <li class="redsquare-menu-profile">
+          <li class="redsquare-menu-profile saito-mouseover">
             <i class="fas fa-user"></i>
             <span>Profile</span>
           </li>
 <!---
-          <li class="redsquare-menu-contacts">
+          <li class="redsquare-menu-contacts saito-mouseover">
             <i class="fas fa-user"></i>
             <span>Contacts</span>
           </li>
