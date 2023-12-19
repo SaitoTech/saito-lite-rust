4bdd97f25a04c22aaf1d5fae616b2a47538e5ec2
arpee <richard@saito.tech>
2023-12-19 15:15:07 +1100

    making it obvious that providing email is optinoal

--- mods/recovery/lib/backup.template.js
+++ mods/recovery/lib/backup.template.js
@@ -10,25 +10,28 @@ module.exports = BackupTemplate = (identifier, newIdentifier) => {
   }
   let intro = "";
   if (newIdentifier) {
-    intro = `<span style="font-weight:bold">Name Registration Submitted!</span> `;
+    intro = `<h6>Name Registration Submitted!</h6> `;
   }
   return `
     <form id="backup-template">
       <div class="saito-overlay-form">
         <div class="saito-overlay-form-header">
-          <div class="saito-overlay-form-header-title">Account Recovery Setup</div>
+          <div class="saito-overlay-form-header-title">On-chain Account Recovery</div>
         </div>
-        <div class="saito-overlay-form-text">${intro}Protect ${txt} by adding remote login. Your email / password will be used for wallet encryption and not shared publicly</div>
+        <div class="saito-overlay-form-text">${intro}</div>
         <div class="saito-overlay-subform">
+          <div class="saito-overlay-form-subtext">Protect ${txt} by adding remote login. Your email / password will be used for wallet encryption and not shared publicly.</div>
           <div></div><!-- Necessary for formatting -->
           <div class="saito-overlay-subform-inputs">
             <input type="text" id="saito-overlay-form-input" class="saito-overlay-form-input saito-overlay-form-email" placeholder="address@domain.com" value="" />
-            <input type="text" style="margin-top: 1.5rem; margin-bottom: 1rem;" id="saito-overlay-form-input" class="saito-overlay-form-input saito-overlay-form-password" placeholder="password" value="" />
+            <input type="text" id="saito-overlay-form-input" class="saito-overlay-form-input saito-overlay-form-password" placeholder="password" value="" />
           </div>
       		<input type="checkbox" class="saito-overlay-subform-checkbox" checked />
       		<div class="saito-overlay-subform-text">i understand my wallet will be only as secure as my password</div>
+          <button type="submit" class="saito-button-primary fat saito-overlay-form-submit" id="saito-overlay-submit">Encrypt & Upload</button>
 	      </div>
-        <button type="submit" class="saito-button-primary fat saito-overlay-form-submit" id="saito-overlay-submit">Encrypt & Upload</button>
+        <button type="submit" class="saito-button-primary fat saito-overlay-form-cancel" id="saito-overlay-cancel">No Thanks</button>
+
       </div>
     </form>
   `;
