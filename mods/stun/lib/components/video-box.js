313c3742cbe1f1031627634b7d2405ff3aa4d650
Daniel Worlton <f8daniel@gmail.com>
2024-01-09 15:22:45 +0700

    clean up videobox

--- mods/stun/lib/components/video-box.js
+++ mods/stun/lib/components/video-box.js
@@ -52,39 +52,21 @@ class VideoBox {
 
   attachEvents() {}
 
-  async render(stream = null, placeholder_info = null) {
+  render(stream = null, placeholder_info = null) {
     if (stream) {
       this.stream = stream;
     }
 
-    // this.stream = stream;
     if (this.stream !== null) {
       this.removeConnectionMessage();
-      if (this.stream_id === "local") {
-        this.renderStream({ muted: true });
-      } else {
-        this.renderStream({ muted: false });
+
+      //Add Video Box
+      if (!document.querySelector(`#stream${this.stream_id}`)) {
+        this.app.browser.addElementToClass(videoBoxTemplate(this.stream_id, this.app, this.mod), this.containerClass);
+        const videoBox = document.querySelector(`#stream${this.stream_id}`);
+        videoBox.firstElementChild.srcObject = this.stream;
       }
 
-      let key;
-      if (this.stream_id === "local") {
-        key = await this.app.wallet.getPublicKey();
-      } else {
-        key = this.stream_id;
-      }
-      let name = this.app.keychain.returnIdentifierByPublicKey(key, true);
-
-      const video_box = document.querySelector(`#stream${this.stream_id}`);
-
-      if (video_box) {
-        if (video_box.querySelector(".video-call-info")) {
-          video_box.querySelector(
-            ".video-call-info"
-          ).innerHTML = `<p class="saito-address" data-id="${key}" >${name}</p>`;
-        }
-      }
-
-      if (this.stream_id === "local") return;
     } else {
       this.renderPlaceholder(placeholder_info);
     }
@@ -92,22 +74,11 @@ class VideoBox {
     this.attachEvents();
   }
 
-  async rerender() {
-    await this.remove();
-    await this.render(this.stream);
+  rerender() {
+    this.remove();
+    this.render(this.stream);
   }
 
-  renderStream({ muted }) {
-    if (!document.querySelector(`#stream${this.stream_id}`)) {
-      this.app.browser.addElementToClass(
-        videoBoxTemplate(this.stream_id, muted, this.isPresentation),
-        this.containerClass
-      );
-    }
-
-    const videoBox = document.querySelector(`#stream${this.stream_id}`);
-    videoBox.firstElementChild.srcObject = this.stream;
-  }
 
   renderPlaceholder(placeholder_info = "negotiating peer connection") {
     if (!document.querySelector(`#stream${this.stream_id}`)) {
@@ -145,23 +116,7 @@ class VideoBox {
     // if it's in the expanded div, replace
   }
 
-  updateVideoMuteStatus(message) {
-    const video_box = document.querySelector(`#stream${this.stream_id}`);
-    if (video_box.querySelector(".video-call-info")) {
-      video_box.querySelector(".video-call-info").innerHTML = `<p>${message}</p>`;
-    }
-  }
-
-  removeVideoMuteStatus() {
-    const video_box = document.querySelector(`#stream${this.stream_id}`);
-    if (video_box.querySelector(".video-call-info")) {
-      video_box.querySelectorAll(".video-call-info").forEach((item) => {
-        item.parentElement.removeChild(video_box.querySelector(".video-call-info"));
-      });
-    }
-  }
-
-  async remove(is_disconnection = false) {
+  remove(is_disconnection = false) {
     let videoBox = document.querySelector(`#stream${this.stream_id}`);
     if (videoBox) {
       if (is_disconnection) {
@@ -169,7 +124,7 @@ class VideoBox {
           videoBox.remove();
           try {
             this.mod.CallInterface.video_boxes["local"].video_box.containerClass = "expanded-video";
-            await this.mod.CallInterface.video_boxes["local"].video_box.rerender();
+            this.mod.CallInterface.video_boxes["local"].video_box.rerender();
           } catch (err) {}
           return;
         }
@@ -178,9 +133,6 @@ class VideoBox {
     }
   }
 
-  streamExists() {
-    return this.stream;
-  }
 }
 
 module.exports = VideoBox;
