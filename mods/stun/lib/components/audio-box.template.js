33d97eb9cc57b064b4002494ffbbea1a32f4e9f3
Daniel Worlton <f8daniel@gmail.com>
2024-01-17 13:01:42 +0700

    Stun: mute local audio stream

--- mods/stun/lib/components/audio-box.template.js
+++ mods/stun/lib/components/audio-box.template.js
@@ -1,13 +1,16 @@
 module.exports = (app, mod, stream_id) => {
 
+    let videoAttribute = "";
     let imgsrc = app.keychain.returnIdenticon(stream_id);
     if (stream_id === "local"){
         imgsrc = app.keychain.returnIdenticon(mod.publicKey);
+        videoAttribute = "muted";
     }
+
 
     return `
      <div class="audio-box" id="audiostream_${stream_id}">
-      <audio autoplay playsinline="true" id="${stream_id}"></audio>
+      <audio autoplay ${videoAttribute} playsinline id="${stream_id}"></audio>
       <img data-id="${stream_id}" class="saito-identicon" src="${imgsrc}"/>
      </div>
      `;
