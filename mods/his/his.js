ce1f94123c8779b18bb8e9a138984f91253eb6ac
David Lancashire <david.lancashire@gmail.com>
2024-03-28 21:22:19 +0700

    fix: modified setup

--- mods/his/his.js
+++ mods/his/his.js
@@ -2579,15 +2579,6 @@ console.log("\n\n\n\n");
 	  this.addDebater("protestant", "bucer-debater");
 	  this.addDebater("protestant", "carlstadt-debater");
 
-
-//
-// TESTING
-//TEMP
-this.controlSpace("france", "granada");
-this.controlSpace("france", "cartagena");
-this.controlSpace("france", "cordoba");
-this.controlSpace("france", "valencia");
-
 	}
 
       }
