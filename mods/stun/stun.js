f07159e62cfbaec5b63ec1d011c5a774e36471dd
Daniel Worlton <f8daniel@gmail.com>
2024-04-05 13:36:06 +0900

    improve intersection of stun and relay

--- mods/stun/stun.js
+++ mods/stun/stun.js
@@ -142,10 +142,7 @@ class Stun extends ModTemplate {
 
 	sendTransaction(peerId, tx) {
 		let peerConnection = this.peers.get(peerId);
-		if (peerConnection?.dc) {
-			console.warn('Stun: no data channel with peer');
-			return;
-		}
+
 		if (!this.hasConnectionWithPeer(peerId)){
 			console.warn("Stun: cannot send transaction over stun");
 			return;
