f3069532f21f41cb820a6c764ced11b10d53be5c
Umair Khan <umair@saito.tech>
2024-04-13 04:58:09 +0500

    Validate in-game stake amount

--- mods/crypto/lib/overlays/select-amount.template.js
+++ mods/crypto/lib/overlays/select-amount.template.js
@@ -6,14 +6,19 @@ module.exports = (app, mod) => {
       <h2 class="auth_title">Amount to Stake?</h2>
       
       <div class="stake-input-container">
-        <input autocomplete="off" id="amount_to_stake_input" class="stake" value="0.0" />
+        <input autocomplete="off" id="amount_to_stake_input" class="stake" 
+        type="number" min="0" max="9999999999.99999999" step="0.00000001" value="0.0" >
+
         <div class="crypto_msg select_max">Max: ${mod.max_balance}</div>
         <div class="crypto-ticker">${mod.ticker}</div>
+
+        <div class="stake-input-error" id="stake-amount-error"></div>
       </div>
 
       <div class="crypto-stake-confirm-container">
         <input type="checkbox" name="crypto-stake-confirm-input" id="crypto-stake-confirm-input">
         <label for="crypto-stake-confirm-input" class="commentary">peer-to-peer gaming is legal in my jurisdiction</label>
+        <div class="stake-input-error" id="stake-checkbox-error"></div>
       </div>
 
       <div class="button saito-button-primary crypto_amount_btn" id="enable_staking_no">confirm</div>
