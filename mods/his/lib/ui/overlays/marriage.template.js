780198d054713103631fc17a35500b2a904c277a
David Lancashire <david.lancashire@gmail.com>
2024-04-24 22:08:31 +0700

    fix: cardnum fix

---
+++ mods/his/lib/ui/overlays/marriage.template.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+	let help = `Henry Advances Marriage Status`;
+
+	let html = `
+          <div class="marriage-overlay" id="marriage-overlay">
+	    <div class="help">${help}</div>
+	    <div class="content">
+	    </div>
+          </div>
+  	`;
+	return html;
+};
+
