e7924f213887354675823126f529ac191926b9c3
davikstone <onyeji@saito.tech>
2024-04-26 10:35:25 +0100

    load txs

--- mods/profile/profile.js
+++ mods/profile/profile.js
@@ -25,34 +25,43 @@ class Profile extends ModTemplate {
     }
 
 
+    async onPeerServiceUp(peer) {
+        if (this.app.BROWSER === 1) {
+            this.sendProfileTransaction({ description: "a description", banner: "banner", image: "image", archive: JSON.stringify({ publicKey: "key" }) })
+
+
+        }
+
+    }
+
+
 
 
 
     /**
  * Asynchronously sends a transaction to update a user's profile.
  * 
- * @param {string} image - Base64 encoded string 
- * @param {string} banner - Base64 encoded string
- * @param {string} description - Text description for the user's profile.
- * @param {Object} archive 
+ * 
  */
 
-    async sendProfileTransaction(image, banner, description, archive) {
+    async sendProfileTransaction(data) {
         try {
-            if (!image || !banner || !description || typeof archive !== 'object' || archive === null) {
-                throw new Error("All parameters must be provided and valid.");
+            const obj = {};
+            for (const key in data) {
+                if (data.hasOwnProperty(key)) {
+                    if (key === "archive" && typeof data[key] === 'object' && !Array.isArray(data[key])) {
+                        obj[key] = JSON.stringify(data[key]);
+                    } else {
+                        obj[key] = data[key];
+                    }
+                }
             }
 
             let newtx = await this.app.wallet.createUnsignedTransactionWithDefaultFee(this.publicKey)
             newtx.msg = {
                 module: this.name,
                 request: "update profile",
-                data: {
-                    image,
-                    banner,
-                    description,
-                    archive: JSON.stringify(archive)
-                }
+                data: obj
             }
             newtx.serialize_to_web(this.app);
             await newtx.sign();
@@ -60,8 +69,6 @@ class Profile extends ModTemplate {
         } catch (error) {
             console.error("Profile: Error creating profile transaction ", error)
         }
-
-
     }
 
     /**
@@ -69,32 +76,81 @@ class Profile extends ModTemplate {
  *
  * @param {Object} tx - The transaction object received, containing data to be processed.
  */
-    receiveProfileTransaction(tx) {
+    async receiveProfileTransaction(tx) {
         try {
-            let from = tx.from[0].publicKey;
+
+            let from = tx.from && tx.from.length > 0 ? tx.from[0].publicKey : null;
             if (!from) {
-                throw Error("No `from` public key")
+                throw new Error("No `from` public key");
             }
             let txmsg = tx.returnMessage();
+
             if (this.app.keychain.returnKey(from)) {
-                let data = {}
-                for (let i in txmsg.data) {
-                    if (i === "banner" || i === "image") {
-                        data[i] = tx.signature
-                    }
-                    if (i === "archive") {
-                        data[i] = JSON.parse(txmsg.data[i]);
+                let data = {};
+                for (let key in txmsg.data) {
+
+                    if (!txmsg.data.hasOwnProperty(key)) continue;
+
+
+                    if (key === "banner" || key === "image" || key === "description") {
+                        data[key] = tx.signature;
+                    } else if (key === "archive" && typeof txmsg.data[key] === 'string') {
+
+                        try {
+                            data[key] = JSON.parse(txmsg.data[key]);
+                        } catch (parseError) {
+                            console.error("Error parsing archive data", parseError);
+                            data[key] = txmsg.data[key];
+                        }
+                    } else {
+                        // console.error("")
+                        // key not supported
                     }
                 }
-                this.app.keychain.addKey(from, data)
+                this.app.keychain.addKey(from, data);
+                await this.saveProfileTransaction(tx)
             } else {
-                console.log("key not found")
+                console.log("Key not found");
             }
         } catch (error) {
-            console.error("Profile: Error receiving profile transaction ", error)
+            console.error("Profile: Error receiving profile transaction", error);
         }
-
     }
+
+
+
+
+    // / LOAD PROFILE VALUES
+
+    async returnProfilePicture(sig) {
+        let tx = await this.app.storage.loadTransactions({ field1: "Profile" }, (txs) => {
+            console.log(txs, "loaded transactions: profile picture")
+        });
+        console.log(tx)
+    }
+
+    async returnProfileBanner(sig) {
+        let tx = await this.app.storage.loadTransactions({}, (txs) => {
+
+        });
+        console.log(tx)
+    }
+
+    async returnProfileDescription(sig) {
+        let tx = await this.app.storage.loadTransactions({}, (txs) => {
+
+        });
+        console.log(tx)
+    }
+
+    async saveProfileTransaction(tx) {
+        await this.app.storage.saveTransaction(tx, {}, "localhost")
+
+        await this.returnProfileBanner("55c9babb413976e8ab7ea68a2c295e4bd52fb595affe411d31366763d780315309f3946109adbde592e33f119aee8558c55cbaca286d2947c2a3114d3eccf3fd")
+        await this.returnProfileDescription("55c9babb413976e8ab7ea68a2c295e4bd52fb595affe411d31366763d780315309f3946109adbde592e33f119aee8558c55cbaca286d2947c2a3114d3eccf3fd")
+        await this.returnProfilePicture("55c9babb413976e8ab7ea68a2c295e4bd52fb595affe411d31366763d780315309f3946109adbde592e33f119aee8558c55cbaca286d2947c2a3114d3eccf3fd")
+    }
+
 
 }
 
