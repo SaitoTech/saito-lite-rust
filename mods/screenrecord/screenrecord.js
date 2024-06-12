e4f50c9f0848ad0d7e43d21c490f86c642b86b94
davikstone <onyeji@saito.tech>
2024-06-12 23:38:49 +0700

    update

--- mods/screenrecord/screenrecord.js
+++ mods/screenrecord/screenrecord.js
@@ -106,308 +106,6 @@ class Record extends ModTemplate {
 
 
 
-	// async startRecording(container, members = [], callbackAfterRecord = null, type = "game") {
-	// 	let startRecording = await sconfirm('Do you  want to start recording?');
-	// 	if (!startRecording) return;
-	// 	this.observer = new MutationObserver((mutations) => {
-	// 		mutations.forEach((mutation) => {
-	// 			if (mutation.type === 'childList') {
-	// 				mutation.addedNodes.forEach(node => {
-
-	// 					if (node.nodeType === Node.ELEMENT_NODE && node.tagName === 'DIV' && node.id.startsWith('stream_')) {
-
-	// 						const videos = node.querySelectorAll('video');
-	// 						videos.forEach(video => {
-	// 							const stream = 'captureStream' in video ? video.captureStream() : ('mozCaptureStream' in video ? video.mozCaptureStream() : null);
-	// 							const rect = video.getBoundingClientRect();
-	// 							const parentID = video.parentElement.id;
-	// 							const videoElement = document.createElement('video');
-	// 							videoElement.srcObject = stream;
-	// 							videoElement.play();
-	// 							this.streamData.push({ stream, rect, parentID, videoElement });
-	// 						});
-	// 					}
-	// 				});
-	// 			}
-	// 			if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-	// 				this.streamData.forEach(data => {
-	// 					if (data.parentID === mutation.target.id) {
-	// 						data.rect = mutation.target.getBoundingClientRect();
-	// 					}
-	// 				});
-	// 			}
-
-	// 			if (mutation.removedNodes.length > 0) {
-	// 				mutation.removedNodes.forEach(node => {
-	// 					// If a video or its parent is removed, find it in streamData and remove it
-	// 					let index = this.streamData.findIndex(data => data.videoElement === node || data.videoElement.parentElement === node);
-	// 					if (index !== -1) {
-	// 						this.streamData.splice(index, 1);
-	// 					}
-	// 				});
-	// 			}
-	// 		});
-	// 	});
-
-	// 	this.observer.observe(document.body, {
-	// 		attributes: true,
-	// 		childList: true,
-	// 		subtree: true,
-	// 		attributeFilter: ['style']
-	// 	});
-
-
-	// 	let combinedStream = new MediaStream();
-
-
-	// 	if (type === "videocall") {
-
-
-	// 		if (!startRecording) return;
-
-	// 		const canvas = document.createElement('canvas');
-	// 		const scaleFactor = window.devicePixelRatio; // Handle high DPI devices
-	// 		canvas.width = window.innerWidth * scaleFactor;
-	// 		canvas.height = window.innerHeight * scaleFactor;
-	// 		const ctx = canvas.getContext('2d');
-	// 		ctx.scale(scaleFactor, scaleFactor); // Scale the context to match the device pixel ratio
-	// 		document.body.appendChild(canvas);
-
-	// 		this.observer = new MutationObserver(mutations => {
-	// 			mutations.forEach(mutation => {
-	// 				if (mutation.type === 'childList') {
-	// 					mutation.addedNodes.forEach(node => {
-	// 						if (node.nodeType === Node.ELEMENT_NODE && node.tagName === 'DIV' && node.id.startsWith('stream_')) {
-	// 							const videos = node.querySelectorAll('video');
-	// 							videos.forEach(video => {
-	// 								const stream = 'captureStream' in video ? video.captureStream() : ('mozCaptureStream' in video ? video.mozCaptureStream() : null);
-	// 								const rect = video.getBoundingClientRect();
-	// 								const parentID = video.parentElement.id;
-	// 								const videoElement = document.createElement('video');
-	// 								videoElement.srcObject = stream;
-	// 								videoElement.play();
-	// 								videoElement.style.display = "none";
-	// 								this.streamData.push({ stream, rect, parentID, videoElement });
-	// 							});
-	// 						}
-	// 					});
-	// 				}
-	// 				if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-	// 					this.streamData.forEach(data => {
-	// 						if (data.parentID === mutation.target.id) {
-	// 							data.rect = mutation.target.getBoundingClientRect();
-	// 						}
-	// 					});
-	// 				}
-	// 			});
-	// 		});
-	// 		this.observer.observe(document.body, {
-	// 			attributes: true,
-	// 			childList: true,
-	// 			subtree: true,
-	// 			attributeFilter: ['style']
-	// 		});
-
-	// 		let lastUpdateTime = 0;
-	// 		const updateInterval = 1000 / 30; // Update at 30 FPS
-
-	// 		const drawStreamsToCanvas = () => {
-	// 			if (Date.now() - lastUpdateTime > updateInterval) {
-	// 				ctx.clearRect(0, 0, canvas.width, canvas.height);
-	// 				html2canvas(document.querySelector(container), {
-	// 					scale: scaleFactor,
-	// 					useCORS: true
-	// 				}).then(contentCanvas => {
-	// 					ctx.drawImage(contentCanvas, 0, 0, canvas.width, canvas.height);
-	// 					this.streamData.forEach(data => {
-	// 						const parentElement = document.getElementById(data.parentID);
-	// 						if (!parentElement) return;
-
-	// 						const currentRect = parentElement.getBoundingClientRect();
-	// 						if (data.videoElement.readyState >= 2) {
-	// 							ctx.drawImage(data.videoElement, currentRect.left + window.pageXOffset, currentRect.top + window.pageYOffset, currentRect.width, currentRect.height);
-	// 						}
-	// 						data.rect = currentRect;
-	// 					});
-	// 					lastUpdateTime = Date.now();
-	// 				}).catch(error => {
-	// 					console.error('Error in html2canvas:', error);
-	// 				});
-	// 			}
-	// 			requestAnimationFrame(drawStreamsToCanvas);
-	// 		};
-	// 		drawStreamsToCanvas();
-
-	// 		let chunks = [];
-	// 		const mimeType = 'video/webm';
-	// 		this.mediaRecorder = new MediaRecorder(canvas.captureStream(60), {
-	// 			mimeType: mimeType,
-	// 			videoBitsPerSecond: 10 * 1024 * 1024,
-	// 			audioBitsPerSecond: 320 * 1024
-	// 		});
-	// 		this.mediaRecorder.ondataavailable = event => {
-	// 			if (event.data.size > 0) {
-	// 				chunks.push(event.data);
-	// 			}
-	// 		};
-	// 		this.mediaRecorder.onstop = async () => {
-	// 			const blob = new Blob(chunks, { type: 'video/webm' });
-	// 			const url = URL.createObjectURL(blob);
-	// 			const a = document.createElement('a');
-	// 			a.style.display = 'none';
-	// 			a.href = url;
-	// 			a.download = 'recorded.webm';  // Optionally prompt for file name
-	// 			document.body.appendChild(a);
-	// 			a.click();
-	// 			setTimeout(() => {
-	// 				document.body.removeChild(a);
-	// 				URL.revokeObjectURL(url);
-	// 			}, 100);
-	// 			if (callbackAfterRecord) {
-	// 				callbackAfterRecord(blob);
-	// 			}
-	// 		};
-	// 		this.mediaRecorder.start();
-
-	// 	} else {
-	// 		const canvas = document.createElement('canvas');
-	// 		canvas.width = window.innerWidth;
-	// 		canvas.height = window.innerHeight;
-	// 		const ctx = canvas.getContext('2d');
-	// 		document.body.appendChild(canvas);
-
-	// 		let result = document.querySelector(container);
-
-
-	// 		const drawStreamsToCanvas = () => {
-	// 			// Clear the entire canvas
-	// 			ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-	// 			html2canvas(result).then(contentCanvas => {
-	// 				ctx.drawImage(contentCanvas, 0, 0, canvas.width, canvas.height);
-	// 				this.streamData.forEach(data => {
-	// 					const parentElement = document.getElementById(data.parentID);
-	// 					if (!parentElement) return;
-
-	// 					const currentRect = parentElement.getBoundingClientRect();
-	// 					if (data.videoElement.readyState >= 2) {
-	// 						ctx.drawImage(data.videoElement, currentRect.left, currentRect.top, currentRect.width, currentRect.height);
-	// 					}
-	// 					data.rect = currentRect;
-	// 				});
-
-	// 				requestAnimationFrame(drawStreamsToCanvas);
-	// 			});
-	// 		};
-
-
-	// 		const otherParties = this.app.modules.getRespondTos('media-request');
-	// 		if (otherParties.length > 0) {
-	// 			const videoElements = document.querySelectorAll('div[id^="stream_"] video');
-	// 			this.streamData = Array.from(videoElements).map(video => {
-	// 				const stream = 'captureStream' in video ? video.captureStream() : ('mozCaptureStream' in video ? video.mozCaptureStream() : null);
-	// 				const rect = video.getBoundingClientRect();
-	// 				const parentID = video.parentElement.id;
-	// 				const videoElement = document.createElement('video');
-	// 				videoElement.srcObject = stream;
-	// 				videoElement.play();
-	// 				videoElement.style.display = "none";
-
-	// 				return { stream, rect, parentID, videoElement };
-	// 			}).filter(data => data.stream !== null)
-
-	// 		} else {
-	// 			let includeCamera = await sconfirm('Add webcam to stream?');
-	// 			try {
-	// 				if (includeCamera) {
-	// 					try {
-	// 						this.localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-	// 					} catch (error) {
-	// 						console.error("Failed to get user media:", error);
-	// 						alert("Failed to access camera and microphone.");
-	// 						return;
-	// 					}
-
-	// 					this.videoBox = new VideoBox(this.app, this, 'local');
-	// 					this.videoBox.render(this.localStream);
-	// 					let videoElement = document.querySelector('.video-box-container-large');
-	// 					console.log('video element', videoElement)
-	// 					videoElement.style.position = 'absolute';
-	// 					videoElement.style.top = '100px';
-	// 					videoElement.style.width = '350px';
-	// 					videoElement.style.height = '350px';
-	// 					this.app.browser.makeDraggable('stream_local');
-	// 					this.app.browser.makeDraggable('stream_local');
-
-
-
-	// 					const videoElements = document.querySelectorAll('div[id^="stream_"] video');
-	// 					this.streamData = Array.from(videoElements).map(video => {
-	// 						const stream = 'captureStream' in video ? video.captureStream() : ('mozCaptureStream' in video ? video.mozCaptureStream() : null);
-	// 						const rect = video.getBoundingClientRect();
-	// 						const parentID = video.parentElement.id;
-	// 						console.log('stream parent id', parentID)
-	// 						const videoElement = document.createElement('video');
-	// 						videoElement.srcObject = stream;
-	// 						videoElement.play();
-	// 						videoElement.style.display = "none";
-
-	// 						return { stream, rect, parentID, videoElement };
-	// 					}).filter(data => data.stream !== null);
-	// 				} else {
-	// 					this.localStream = await navigator.mediaDevices.getUserMedia({ audio: true });
-	// 				}
-	// 			} catch (error) {
-	// 				console.error('Access to user media denied: ', error);
-	// 				salert('Recording will continue without camera and/or microphone input');
-	// 			}
-	// 		}
-
-
-
-	// 		let chunks = [];
-	// 		const mimeType = 'video/webm';
-	// 		this.mediaRecorder = new MediaRecorder(canvas.captureStream(60), {
-	// 			mimeType: mimeType,
-	// 			videoBitsPerSecond: 25 * 1024 * 1024,
-	// 			audioBitsPerSecond: 320 * 1024
-	// 		});
-	// 		this.mediaRecorder.ondataavailable = event => {
-	// 			if (event.data.size > 0) {
-	// 				chunks.push(event.data);
-	// 			}
-	// 		};
-
-	// 		this.mediaRecorder.onstop = async () => {
-	// 			const blob = new Blob(chunks, { type: 'video/webm' });
-	// 			const url = URL.createObjectURL(blob);
-	// 			const a = document.createElement('a');
-	// 			a.style.display = 'none';
-	// 			a.href = url;
-	// 			const defaultFileName = 'saito_video.webm';
-	// 			const fileName = (await sprompt('Please enter a recording name', 'saito_video')) || defaultFileName;
-	// 			a.download = fileName;
-	// 			document.body.appendChild(a);
-	// 			a.click();
-	// 			setTimeout(() => {
-	// 				document.body.removeChild(a);
-	// 				URL.revokeObjectURL(url);
-	// 			}, 100);
-	// 			if (callbackAfterRecord) {
-	// 				callbackAfterRecord(blob);
-	// 			}
-	// 		};
-
-	// 		this.mediaRecorder.start();
-	// 		drawStreamsToCanvas();
-
-	// 	}
-
-	// 	this.localStream = null;
-	// 	this.externalMediaControl = false;
-	// 	this.updateUIForRecordingStart()
-	// }
-
 
 	async startRecording(container, members = [], callbackAfterRecord = null, type = "game") {
 		let startRecording = await sconfirm('Do you  want to start recording?');
@@ -443,7 +141,7 @@ class Record extends ModTemplate {
 
 				if (mutation.removedNodes.length > 0) {
 					mutation.removedNodes.forEach(node => {
-						// If a video or its parent is removed, find it in streamData and remove it
+
 						let index = this.streamData.findIndex(data => data.videoElement === node || data.videoElement.parentElement === node);
 						if (index !== -1) {
 							this.streamData.splice(index, 1);
@@ -478,31 +176,30 @@ class Record extends ModTemplate {
 					const parentElement = document.getElementById(data.parentID);
 					if (!parentElement) return;
 			
-					// Get the actual video element dimensions
+
 					const videoWidth = data.videoElement.videoWidth;
 					const videoHeight = data.videoElement.videoHeight;
 			
-					// Calculate the aspect ratio of the video
+
 					const videoAspectRatio = videoWidth / videoHeight;
 			
-					// Get the bounding rect of the parent element
+
 					const rect = parentElement.getBoundingClientRect();
 			
-					// Calculate scaling factors based on canvas size
+
 					const scaleX = canvas.width / window.innerWidth;
 					const scaleY = canvas.height / window.innerHeight;
 			
-					// Calculate the new dimensions maintaining the video aspect ratio
+
 					let drawWidth = rect.width * scaleX;
 					let drawHeight = drawWidth / videoAspectRatio;
 			
-					// Adjust dimensions if height exceeds the space
+
 					if (drawHeight > rect.height * scaleY) {
 						drawHeight = rect.height * scaleY;
 						drawWidth = drawHeight * videoAspectRatio;
 					}
 			
-					// Calculate position to center the video in its slot
 					const drawX = rect.left * scaleX + (rect.width * scaleX - drawWidth) / 2;
 					const drawY = rect.top * scaleY + (rect.height * scaleY - drawHeight) / 2;
 			
