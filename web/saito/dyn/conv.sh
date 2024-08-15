c1c6bfc47966bbb716a7d0a84ebe16cdd585aa42
sankadarshana <sankadarshana@gmail.com>
2024-08-15 09:34:38 +0530

    script update for dynamic modules loading

--- web/saito/dyn/conv.sh
+++ web/saito/dyn/conv.sh
@@ -1,5 +1,7 @@
  #!/bin/bash
 
+printf "export const DYN_MOD = \"\";" > ../../../lib/dyn_mod.js
+node ../../../webpack/webpack.config.debugmod.cjs
 printf "export const DYN_MOD = \"" > ../../../lib/dyn_mod.js
 base64 -i ./dyn.module.js -o base.txt
 #echo "content" > ./base.txt
