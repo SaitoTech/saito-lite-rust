a09e66ede8e5099df2684c8bbd22ccc69c2f4141
Umair Khan <umair@saito.tech>
2024-08-16 15:56:54 +0500

    Remove backup stream option and placement of yt icon

--- mods/youtube-client/lib/yt-client-init-stream.template.js
+++ mods/youtube-client/lib/yt-client-init-stream.template.js
@@ -11,12 +11,14 @@ module.exports = YoutubeInitStreamTemplate = (app,mod) => {
 			value="${(app.options?.youtube?.stream_key != null ? app.options.youtube.stream_key : ``)}" 
 			placeholder="Enter youtube stream key">
 
+			<!--
 			<div class="yt-stream-type">
 				<input type="radio" name="stream_type" value="main" checked>
 				<div class="yt-stream-type-name">Main stream</div>
 				<input type="radio" name="stream_type" value="backup">
 				<div class="yt-stream-type-name">Backup stream</div>
 			</div>
+			-->
 
 			<div id="yt-stream-btn" class="button saito-button-primary">Go Live</div>
 		</div>
