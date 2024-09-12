3af0ef33592d9de224602dff2aaa2d3480b55196
davikstone <onyeji@saito.tech>
2024-09-12 01:41:08 +0100

    update react

--- mods/react/react-components/App.js
+++ mods/react/react-components/App.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Button = require('./Button').default;
 
-const App = (app) => {
-    console.log(app, "saito app")
+const App = ({ app, mod }) => {
+
+    const createMessageTransaction = async () => {
+        let newtx =
+            await app.wallet.createUnsignedTransactionWithDefaultFee();
+        newtx.addTo(mod.publicKey)
+        newtx.msg = {
+            module: 'React',
+            request: "message-request",
+        };
+        await newtx.sign();
+        app.connection.emit('relay-transaction', newtx);
+    }
+
+
+    useEffect(() => {
+        mod.handlePeerTransaction = function (blk, tx) {
+            console.log("handle peer transacton", blk, tx)
+        }
+    }, [])
+
+
+
     return (
         <div>
-         <h1>Hello from React !</h1>
-         <Button text="click me"/>
+            <h1>Hello from React !</h1>
+            <Button app={app} onClick={() => createMessageTransaction()} text="click me" />
         </div>
-  
+
     );
-   
+
 }
 
 export default App
