de4a34c3d0eae8d162f1f0d9d95829f79d66fcb5
davikstone <onyeji@saito.tech>
2024-09-26 14:35:34 +0100

    refactor stream capturer

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	testRegex: 'lib/saito/.*\\.spec.(ts|tsx)$',
-	testPathIgnorePatterns: ['/node_modules/', '/mods/', '/bundler/']
-};
