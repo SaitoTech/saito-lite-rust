0bcc626d46e67d0a6a36f6a351c9ecba9aa46b08
David Lancashire <david.lancashire@gmail.com>
2024-10-23 13:22:40 +0700

    fix: his patches

--- mods/his/his.js
+++ mods/his/his.js
@@ -35636,12 +35636,12 @@ If this is your first game, it is usually fine to skip the diplomacy phase until
 		//
 		if (cardnum < 0) { cardnum = 0; }
 
-cardnum = 0;
+//cardnum = 0;
 //if (f == "papacy") { cardnum = 0; }
-if (f == "hapsburg") { cardnum = 3; }
+//if (f == "hapsburg") { cardnum = 3; }
 //if (f == "protestant") { cardnum = 0; }
 //if (f == "england") { cardnum = 0; }
-if (f == "ottoman") { cardnum = 3; }
+//if (f == "ottoman") { cardnum = 3; }
 
 
     	        this.game.queue.push("hand_to_fhand\t1\t"+(i+1)+"\t"+this.game.state.players_info[i].factions[z]);
