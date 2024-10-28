7044c23047e9e3cf0197b41c3ae6fd54917d0ad1
David Lancashire <david.lancashire@gmail.com>
2024-10-28 11:33:44 +0700

    fix: patch

--- mods/popup/web/js/review.js
+++ mods/popup/web/js/review.js
@@ -8,12 +8,6 @@ var enableNumberShortcuts = 0;
 // used in dict submission
 var tempLastQuestionData = '';
 
-function load_review(qntype) {
-	setup_reinforcement_lightbox();
-	loadQuestion();
-}
-
-
 var qid = ''; //question id
 var wid = ''; //word id
 var retry_load_delay = 5000;
@@ -23,6 +17,7 @@ try {
 	var trx = parseInt(document.getElementById('total_remaining').innerHTML);
 	total_remaining = trx;
 } catch (err) {}
+
 var total_answered = 0;
 var total_correct = 0;
 var total_incorrect = 0;
@@ -208,200 +203,47 @@ async function loadQuestion() {
 	document.getElementById('option3').classList.remove("red");
 	document.getElementById('option4').classList.remove("red");
 
-	resetCss();
+	// hovering effect
+	$('.option').removeClass('option_hover');
 
 	$('#answer_space').fadeIn(5);
 	$('#question_space').fadeIn(5);
 	$('#test_container').fadeIn(400);
 	try { $('#user_answer').focus(); } catch (err) {}
 
-}
+	$('#option1').unbind('click');
+	$('#option2').unbind('click');
+	$('#option3').unbind('click');
+	$('#option4').unbind('click');
 
+	$('#option1').bind('click', function () {
+		check_answer(1);
+	});
+	$('#option2').bind('click', function () {
+		check_answer(2);
+	});
+	$('#option3').bind('click', function () {
+		check_answer(3);
+	});
+	$('#option4').bind('click', function () {
+		check_answer(4);
+	});
 
-function resetCss() {
-	// hovering effect
-	$('.option').removeClass('option_hover');
-
-	correct_delay = 800;
-	incorrect_delay = 1500;
-
-	// restore sensible defaults
-	$('.option').removeClass('option_correct option_incorrect');
-	$('#question_text').css('padding-left', '10px');
-	$('#question_text').css('padding-right', '10px');
-	$('#question_text').css('padding-top', '40px');
-	$('#question_text').css('font-size', '10rem');
-	$('#question_text').css('line-height', '1em');
-	$('#instructions').css('background-color', '#F7F7F7');
-
-	try { $('#option1').classList.remove("green"); } catch (err) {}
-	try { $('#option2').classList.remove("green"); } catch (err) {}
-	try { $('#option3').classList.remove("green"); } catch (err) {}
-	try { $('#option4').classList.remove("green"); } catch (err) {}
-	try { $('#option1').classList.remove("red"); } catch (err) {}
-	try { $('#option2').classList.remove("red"); } catch (err) {}
-	try { $('#option3').classList.remove("red"); } catch (err) {}
-	try { $('#option4').classList.remove("red"); } catch (err) {}
-
-	$('#question_text').css('line-height', '1em');
-	$('#question_text').css('line-height', '1em');
-	$('#question_text').css('line-height', '1em');
-	if (question.length == 1) {
-		$('#question_text').css('font-size', '14em');
-		$('#question_text').css('line-height', '1em');
-	}
-	if (question.length == 2) {
-		$('#question_text').css('font-size', '12em');
-		$('#question_text').css('padding-top', '60px');
-		$('#question_text').css('line-height', '1em');
-	}
-	if (question.length == 3) {
-		$('#question_text').css('padding-top', '75px');
-		$('#question_text').css('line-height', '1em');
-	}
-	if (question.length > 3) {
-		$('#question_text').css('font-size', '6em');
-		$('#question_text').css('padding-top', '85px');
-	}
-	if (question.length > 5) {
-		$('#question_text').css('font-size', '4em');
-		$('#question_text').css('padding-top', '115px');
-	}
-	if (question.length > 10) {
-		$('#question_text').css('font-size', '2.5em');
-		$('#question_text').css('padding-top', '90px');
-		$('#question_text').css('line-height', '1.7em');
-	}
-	if (question_type != 'test') {
-		if (question.length > 20) {
-			$('#question_text').css('font-size', '2em');
-			$('#question_text').css('padding-top', '80px');
-			$('#question_text').css('line-height', '1.5em');
-		}
-	}
-
-
-	if (question_type == 'test') {
-
-		//
-		// format vertical question
-		//
-		$('#question_text').css('padding-top', '15px');
-		$('#question_space').css('border', 'none');
-		$('#question_space').css('width', '100%');
-		$('#question_space').css('min-height', '80px');
-		$('.option').css('line-height', '30px');
-		$('.option').css('width', '700px');
-		$('.option').css('margin-left', '75px');
-		$('#answer_space').css('width', '700px');
-		$('#lightbox_header').css('display', 'none');
-
-		if (option1 == '') {
-			$('#option1').hide();
-		}
-		if (option2 == '') {
-			$('#option2').hide();
-		}
-		if (option3 == '') {
-			$('#option3').hide();
-		}
-		if (option4 == '') {
-			$('#option4').hide();
-		}
-	} else {
-		//
-		// format horizontal question
-		//
-		$('#question_text').css('padding-top', '120px');
-		$('#question_space').css('border-left', '1px dashed #D7D7D7');
-		$('#question_space').css('width', '435px');
-		$('#question_space').css('min-height', '300px');
-		$('.option').css('line-height', '36px');
-		$('.option').css('width', '340px');
-		$('.option').css('margin-left', '9px');
-		$('#answer_space').css('width', '400px');
-		$('#lightbox_header').css('display', 'all');
-	}
-
-	if (
-		question_type == 'multiple_choice_english' ||
-		question_type == 'multiple_choice_pinyin' ||
-		question_type == 'fill_in_the_blank' ||
-		question_type == 'test' ||
-		question_type == 'vocab'
-	) {
-		if (option1 != '') {
-			$('#option1').show();
-		}
-		if (option2 != '') {
-			$('#option2').show();
-		}
-		if (option3 != '') {
-			$('#option3').show();
-		}
-		if (option4 != '') {
-			$('#option4').show();
-		}
-		$('#instructions').hide();
-		$('#instructions').html('');
-
-		$('#option1').unbind('click');
-		$('#option2').unbind('click');
-		$('#option3').unbind('click');
-		$('#option4').unbind('click');
-
-		$('#option1').bind('click', function () {
-			check_answer(1);
-		});
-		$('#option2').bind('click', function () {
-			check_answer(2);
-		});
-		$('#option3').bind('click', function () {
-			check_answer(3);
-		});
-		$('#option4').bind('click', function () {
-			check_answer(4);
-		});
-
-		$('#question_text').unbind('click');
-		$('#question_text').bind('click', function () {
+	$('#question_text').unbind('click');
+	$('#question_text').bind('click', function () {
 			try {
 				playWordAudio(source_audio_url, this);
 			} catch (err) {}
-		});
-		if (review_include_pinyin == 1) {
-			$('#question_hint').show();
-		} else {
-			$('#question_hint').hide();
-		}
+	});
+	if (review_include_pinyin == 1) {
+		$('#question_hint').show();
+	} else {
+		$('#question_hint').hide();
 	}
 
-	if (
-		question_type == 'generative_english' ||
-		question_type == 'generative_pinyin'
-	) {
-		$('#option1').hide();
-		$('#option2').hide();
-		$('#option3').hide();
-		$('#question_hint').hide();
-		$('#instructions').css('margin-top', '70px');
-		$('#instructions').show();
-		if (question_type == 'generative_english') {
-			$('#instructions').html(
-				'type the <span class="bold red">english</span> definition:'
-			);
-		}
-		if (question_type == 'generative_pinyin') {
-			$('#instructions').html(
-				'type the <span class="bold red">pinyin</span> definition:'
-			);
-		}
-		var inserthtml =
-			'<form id="user_answer_form" action="" method="POST" onsubmit="check_answer();return false;"><input id="user_answer" type="text" onsubmit="check_answer();return false;" style="background-color:#F7F7F7;border:0px solid black;border-bottom:1px dashed #333;font-size:1.4em;width:330px;" /></form>';
-		document.getElementById('option4').innerHTML = inserthtml;
-		$('#option4').unbind('click');
-	}
 }
+
+
 
 async function advance_to_next_question() {
 
@@ -422,13 +264,13 @@ async function advance_to_next_question() {
 
 		if (total_correct == total_answered) {
 			setTimeout(function () {
-				document.getElementById('lightbox_content').innerHTML =
+				document.getElementById('review-overlay').innerHTML =
 					return_test_finished_html();
 			}, correct_delay);
 			return;
 		} else {
 			setTimeout(function () {
-				document.getElementById('lightbox_content').innerHTML =
+				document.getElementById('review-overlay').innerHTML =
 					return_test_finished_html();
 			}, incorrect_delay);
 			return;
@@ -455,6 +297,7 @@ async function advance_to_next_question() {
 		setTimeout(loadQuestion, incorrect_delay);
 	}
 }
+
 
 function show_points(points) {
 	tmpobj = $('#adso_status');
@@ -486,35 +329,6 @@ function show_points(points) {
 	);
 }
 
-
-function setup_reinforcement_lightbox() {
-
-	var html2insert = `<div id="lightbox_header" class="lightbox_header">`;
-	if (endless_mode == 0) { html2insert += `Before we continue, <span class="red total_remaining" id="total_remaining">five</span> <span class="red" id="questions">questions</span> to review....`; }
-	html2insert += `
-        	</div>
-		<div id="test_container" class="test_container">
-        	  <div id="question_space" class="question_space"><div id="question_text" class="question_text"></div><div id="question_hint" class="question_hint"></div></div> \
-        	  <div id="answer_space" class="answer_space"> \
-        	    <div id="instructions" class="instructions"></div> \
-      		    <div id="option1" class="option"></div> \
-      		    <div id="option2" class="option"></div> \
-      		    <div id="option3" class="option"></div> \
-      		    <div id="option4" class="option"></div> \
-      		    <div id="feedback" class="feedback"></div> \
-      		  </div> 
-	        </div>
-	`;
-
-	document.getElementById('lightbox_content').innerHTML = html2insert;
-
-	$('.option').bind('mouseover', function () {
-		$(this).addClass('option_hover');
-	});
-	$('.option').bind('mouseout', function () {
-		$(this).removeClass('option_hover');
-	});
-}
 
 function hide_lightbox2() {
 	if (total_remaining == 0 && review_until_correct == 1) {
@@ -597,7 +411,7 @@ function return_test_finished_html() {
 	   <div id="lightbox_header" class="lightbox_header"> \
   	     Congratulations on a Perfect Score.... \
            </div> \
-   	   <div style="margin-top:50px"><div style="padding-top:20px;width:500px;float:left;text-align:center;font-size:2.3em;color:#888"> \
+   	   <div style="margin-top:50px"><div style="padding-top:20px;width:500px;float:left;text-align:center;font-size:2.3em;color:#888;display:flex;flex-direction:horizontal"> \
 	     <div style="font-size:0.65em;padding-left:110px;padding-right:100px;text-align:center;color:#333;margin-top:10px;"><b>Tip:</b> you can change the length and frequency of these popup tests by <a href="/account/review" class="red nonlink">customizing your account</a>. </div> \
 	     <div style="margin-top:45px;font-size:0.6em;color:#888;"><span class="red" style="cursor:pointer" onclick="hide_lightbox2();">Good to know, let me continue....</span></div> \
 	   </div> \
