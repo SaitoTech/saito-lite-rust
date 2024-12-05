c57ae3a1fbf94ca2af48ffe723509d97083132e3
Umair Khan <umair@saito.tech>
2024-12-05 23:31:36 +0500

    Code changes

--- mods/egld/egld.js
+++ mods/egld/egld.js
@@ -14,6 +14,7 @@ const {account:Account, transaction: Transaction} = require('@elrondnetwork/elro
 const CryptoModule = require("../../lib/templates/cryptomodule");
 
 
+
 class EGLDModule extends CryptoModule {
 
   constructor(app, mod) {
@@ -25,7 +26,7 @@ class EGLDModule extends CryptoModule {
     this.categories = "Cryptocurrency";
     this.balance = 0
     this.egld = {}
-    this.base_url = ""
+    this.base_url = "https://elrond-api-devnet.public.blastapi.io"
     // this.base_url = "https://elrond.saito.io/0"
     this.api_network_provider = "https://devnet-api.elrond.com"
     this.has_loaded = false
@@ -33,50 +34,175 @@ class EGLDModule extends CryptoModule {
 
 
   async initialize(){
-       if(!this.has_loaded){
-        try {
-          const res = await axios({
-            method: "get",
-            url: `https://elrond.saito.io/0/hello`
-           })
-           this.base_url = "https://elrond.saito.io/0"
-        } catch (error) {
-          this.base_url = "https://elrond-api-devnet.public.blastapi.io"
-        }
-        finally {
-          this.createEGLDAccount();
-          this.networkProvider = new ApiNetworkProvider(this.api_network_provider)
-          let address = this.egld.keyfile.bech32;
-          await this.updateBalance(address);
-          await this.updateAddressNonce(address);
-          console.log('base url', this.base_url);
-          this.has_loaded = true;
-        }
-       }
+       //if(!this.has_loaded){
+        // try {
+        //   const res = await axios({
+        //     method: "get",
+        //     url: `https://elrond.saito.io/0/hello`
+        //    })
+        //    this.base_url = "https://elrond.saito.io/0"
+        // } catch (error) {
+        //   this.base_url = "https://elrond-api-devnet.public.blastapi.io"
+        // }
+       //  this.base_url = "https://elrond-api-devnet.public.blastapi.io";
+       // // finally {
+       //    this.createEGLDAccount();
+       //    this.networkProvider = new ApiNetworkProvider(this.api_network_provider)
+       //    let address = this.egld.keyfile.bech32;
+       //    await this.updateBalance(address);
+       //    await this.updateAddressNonce(address);
+       //    console.log('base url', this.base_url);
+       //    this.has_loaded = true;
+       //  //}
+       // }
 
 
        // check saito api status periodically, use fallback if it fails
-       setInterval(async ()=> {
-        try {
-          const res = await axios({
-            method: "get",
-            url: `https://elrond.saito.io/0/hello`
-           })
-           this.base_url = "https://elrond.saito.io/0"
-        } catch (error) {
-          this.base_url = "https://elrond-api-devnet.public.blastapi.io"
-        }
-       }, 60000)
+       // setInterval(async ()=> {
+       //  try {
+       //    const res = await axios({
+       //      method: "get",
+       //      url: `https://elrond.saito.io/0/hello`
+       //     })
+       //     this.base_url = "https://elrond.saito.io/0"
+       //  } catch (error) {
+       //    this.base_url = "https://elrond-api-devnet.public.blastapi.io"
+       //  }
+       // }, 60000)
 
    
 
   }
 
 
+  async activate() {
+    console.log("activating egld ///");
+    if(!this.has_loaded){
+        this.createEGLDAccount();
+        this.networkProvider = new ApiNetworkProvider(this.api_network_provider)
+        let address = this.egld.keyfile.bech32;
+        await this.updateBalance(address);
+        await this.updateAddressNonce(address);
+        console.log('base url', this.base_url);
+        this.has_loaded = true;
+        await this.showBackupWallet();
+        this.save();
+    }
+
+    await super.activate();
+  }
+
+
+  async showBackupWallet(){
+        this.app.options.wallet.backup_required = 1;
+        await this.app.wallet.saveWallet();
+        
+        let msg = `Your wallet has added new crypto keys. 
+        Unless you backup your wallet, you may lose these keys. 
+        Do you want help backing up your wallet?`;
+        this.app.connection.emit(
+            'saito-backup-render-request',
+            {msg: msg, title: 'BACKUP YOUR WALLET'}
+        );
+    }
 
 
 
-  async sendPayment(amount, recipient,  unique_hash=""){
+
+
+async returnBalance(address){
+    return this.balance;
+    
+    let  res  = await  axios({
+        method: 'get',
+        url: `${this.base_url}/address/${address}/balance`,
+        data: ""
+      })
+      this.balance = res.data.data.balance;
+      return res.data.data.balance;
+}
+
+returnAddress (){
+    return this.egld.keyfile.bech32;
+}
+
+
+
+async getAddressNonce(address){
+   const res = await axios({
+    method: "get",
+    url: `${this.base_url}/address/${address}/nonce`
+   })
+   let nonce = res.data.data.nonce;
+//    console.log(res, 'data')
+   return  nonce
+}
+
+
+async updateAddressNonce(address){
+    console.log("this.nonce before: ", this.nonce);
+    const res = await axios({
+        method: "get",
+        url: `${this.base_url}/address/${address}/nonce`
+       })
+
+       this.nonce = res.data.data.nonce;
+       console.log("this.nonce after: ", this.nonce);
+}
+
+
+incrementNonce(nonce){
+    return  nonce + 1
+}
+
+
+
+returnPrivateKey(){
+    return this.egld.account.privateKey;
+}
+
+async updateBalance(address){
+    let balance = await this.returnBalance(address)
+    balance = TokenPayment.egldFromBigInteger(balance)
+    console.log('egld balance: ', balance);
+    this.balance  = 0; // balance; //balance.toPrettyString()
+    console.log('egld balance', this.balance)
+
+}
+
+
+
+async createEGLDAccount(){
+    console.log("createEGLDAccount egld ///");
+    if(this.app.options.egld){
+      let keyfile =  this.app.options.egld.keyfile
+      let account = new Account(this.app.options.egld.keyfile, this.app.options.egld.password)
+      this.egld.keyfile = keyfile;
+      this.egld.account = account;
+    }
+    else {
+      let password = this.app.crypto.generateRandomNumber().substring(0, 8);
+    //   let password = "password"
+      let keyfile =  new Account().initNewAccountFromPassword(password)
+      let account = new Account().loadFromKeyFile(keyfile, password)
+      this.egld.keyfile = keyfile;
+      this.egld.account = account;
+      //this.app.options.egld = {keyfile, password}
+      //this.app.storage.saveOptions(); 
+      console.log("keyfile: ", keyfile);
+      console.log("account: ", account);
+      this.address = keyfile.bech32;
+      this.destination = keyfile.bech32;
+
+      console.log("this.address: ", this.address);
+      console.log("this.destination: ", this.destination);
+    }
+
+  }
+
+
+
+    async sendPayment(amount, recipient,  unique_hash=""){
     let from = this.egld.keyfile.bech32
     let config = await this.networkProvider.getNetworkConfig();
     let nonce = await this.nonce
@@ -151,116 +277,6 @@ async receivePayment(amount="", sender="", recipient="", timestamp=0, unique_has
        }
        return toReturn
 }
-
-
-async returnBalance(address){
-    let  res  = await  axios({
-        method: 'get',
-        url: `${this.base_url}/address/${address}/balance`,
-        data: ""
-      })
-      this.balance = res.data.data.balance;
-      return res.data.data.balance;
-}
-
-returnAddress (){
-    return this.egld.keyfile.bech32;
-}
-
-
-
-async getAddressNonce(address){
-   const res = await axios({
-    method: "get",
-    url: `${this.base_url}/address/${address}/nonce`
-   })
-   let nonce = res.data.data.nonce;
-//    console.log(res, 'data')
-   return  nonce
-}
-
-
-async updateAddressNonce(address){
-    const res = await axios({
-        method: "get",
-        url: `${this.base_url}/address/${address}/nonce`
-       })
-
-       this.nonce = res.data.data.nonce;
-}
-
-
-incrementNonce(nonce){
-    return  nonce + 1
-}
-
-
-
-returnPrivateKey(){
-    return this.egld.account.privateKey;
-}
-
-async updateBalance(address){
-    let balance = await this.returnBalance(address)
-    balance = TokenPayment.egldFromBigInteger(balance)
-    this.balance  = balance.toPrettyString()
-    console.log('egld balance', this.balance)
-}
-
-
-
-async createEGLDAccount(){
-    if(this.app.options.egld){
-      let keyfile =  this.app.options.egld.keyfile
-      let account = new Account(this.app.options.egld.keyfile, this.app.options.egld.password)
-      this.egld.keyfile = keyfile;
-      this.egld.account = account;
-    }
-    else {
-      let password = this.app.crypto.generateRandomNumber().substring(0, 8);
-    //   let password = "password"
-      let keyfile =  new Account().initNewAccountFromPassword(password)
-      let account = new Account().loadFromKeyFile(keyfile, password)
-      this.egld.keyfile = keyfile;
-      this.egld.account = account;
-      this.app.options.egld = {keyfile, password}
-      this.app.storage.saveOptions(); 
-
-    // let keyfile =  {
-    //     "version": 4,
-    //     "id": "7ec57842-6808-4b24-acad-4ea97b004be2",
-    //     "address": "573f7f00a6c99fa399eb969ae08a73891bd46484a10f44c6207c3a8ef5e16840",
-    //     "bech32": "erd12ulh7q9xex068x0tj6dwpznn3ydageyy5y85f33q0sagaa0pdpqqal879j",
-    //     "crypto": {
-    //       "ciphertext": "e4ffc0b7aa7a269d347c25c440c279692379bd3cf525781711b6ad0830bfeed53ac705a91141c3e126c5b97178e1113019c4fd7975a23637733498cb5bb0f240",
-    //       "cipherparams": {
-    //         "iv": "604826587f31bdf765ca28115cd2d588"
-    //       },
-    //       "cipher": "aes-128-ctr",
-    //       "kdf": "scrypt",
-    //       "kdfparams": {
-    //         "dklen": 32,
-    //         "salt": "e0a132cd73464e414e94c21d453f52407918895b9d586316950c74e9fd4c8848",
-    //         "n": 4096,
-    //         "r": 8,
-    //         "p": 1
-    //       },
-    //       "mac": "c8770053d62c66ddb0e2d5105dd6dc7c72b05b2476867db7a3667f245b6aa260"
-    //     }
-    //   }
-    
-    //   let account = new Account(keyfile, password);
-    //   this.egld.keyfile = keyfile;
-    //   this.egld.account = account;
-    //   this.app.options.egld = {keyfile, password}
-    //   this.app.storage.saveOptions(); 
-    // }
-  
-
-}
-
-  }
-
 
 
 }
