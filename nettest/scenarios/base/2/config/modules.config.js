3ac2961eed097687920d80ab32e395e381388120
arpee <richard@saito.tech>
2024-12-12 02:18:03 +1100

    first cut wip - working

---
+++ nettest/scenarios/base/2/config/modules.config.js
@@ -0,0 +1,11 @@
+export default {
+  core: [
+    "disburse/disburse.js",
+    'encrypt/encrypt.js',
+    'explorer/explorer.js',
+    'registry/registry.js'
+    //'spam/spam.js',
+  ],
+  lite: [
+  ]
+};
