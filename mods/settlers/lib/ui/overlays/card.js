5e42e9c5e9981ab84d9958b797d0c54535a73bc1
Daniel Worlton <f8daniel@gmail.com>
2025-01-24 10:28:09 +0100

    snazz up Settlers slide in hud-notice

--- mods/settlers/lib/ui/overlays/card.js
+++ mods/settlers/lib/ui/overlays/card.js
@@ -1,41 +1,14 @@
 const CardTemplate = require('./card.template');
-const NoticeTemplate = require('./notice.template');
-const SaitoOverlay = require('./../../../../../lib/saito/ui/saito-overlay/saito-overlay');
 
 class CardOverlay {
 	constructor(app, mod) {
 		this.app = app;
 		this.mod = mod;
-		this.visible = false;
-		this.overlay = new SaitoOverlay(app, mod, false, true, true);
 	}
 
-	hide() {
-		this.overlay.close();
-	}
-
-	pullHudOverOverlay() {
-		//
-		// pull GAME HUD over overlay
-		//
-		let overlay_zindex = parseInt(this.overlay.zIndex);
-		if (document.querySelector('.hud')) {
-			document.querySelector('.hud').style.zIndex = overlay_zindex + 1;
-			this.mod.hud.zIndex = overlay_zindex + 1;
-		}
-	}
-	pushHudUnderOverlay() {
-		//
-		// push GAME HUD under overlay
-		//
-		let overlay_zindex = parseInt(this.overlay.zIndex);
-		if (document.querySelector('.hud')) {
-			document.querySelector('.hud').style.zIndex = overlay_zindex - 2;
-			this.mod.hud.zIndex = overlay_zindex - 2;
-		}
-	}
 
 	//obj = {player, card: cardname}
+
 	render(obj={}) {
 
 		let settlers_self = this.mod;
@@ -63,24 +36,14 @@ class CardOverlay {
 			}else{
 				cardtext = this.mod.game.playerNames[player - 1] + cardtext;
 			}
-
 			card.cardtext = cardtext;
 		}else{
 			card.cardtext = obj.cardtext;
 		}
 
-		//this.overlay.show(CardTemplate(card));
-		let hudnotice = document.querySelector('.hud-notice');
-		hudnotice.innerHTML = NoticeTemplate(card);
-		hudnotice.classList.add('show');
+
+		settlers_self.hud.showPopup(CardTemplate(card), 3500);
 			
-		setTimeout(() => {
-				hudnotice.classList.remove('show');
-				hudnotice.innerHTML = "";				
-			}, 3500); 
-
-		hudnotice.style.setProperty('--settlers-cardimg', `url(${card.img})`);
-
 		// this will clear any ACKNOWLEDGE
 		this.attachEvents();
 
@@ -88,13 +51,6 @@ class CardOverlay {
 
 	attachEvents() {
 
-		/*
-		let settlers_self = this.mod;
-	    $(".cardover-optout").on('click', function () {
-	    	console.log("!!!!!");
-          	settlers_self.saveGamePreference("settlers_overlays", 0);
-	    });
-		*/
 	}
 
 }
