911a43583393bb765dc378da00ef172ef22b47d3
davikstone <onyeji@saito.tech>
2025-02-03 06:39:45 +0100

    reset zk-poll

--- mods/zk-poll/zk-poll.js
+++ mods/zk-poll/zk-poll.js
@@ -1,16 +1,16 @@
 const ModTemplate = require("../../lib/templates/modtemplate");
-
+const snarkjs = require('snarkjs')
 const pageHome = require('./index');
 const SaitoHeader = require("../../lib/saito/ui/saito-header/saito-header");
 const { default: VoteLayout } = require("./lib/zk-poll-layout");
-
+const { resolve } = require("url");
 
 
 class Vote extends ModTemplate {
     constructor(app) {
         super(app);
         this.app = app;
-        this.name = 'zk-poll';
+        this.name = 'Zk-poll';
         this.slug = 'zk-poll';
         this.elections = new Map(); // Store elections
         this.votes = new Map();     // Store vote commitments
@@ -32,13 +32,6 @@ class Vote extends ModTemplate {
 
         }
 
-    }
-
-    initialize(app) {
-        if(app.BROWSER){
-        //   let snarkjs  = require('../../web/saito/lib/snark')
-        //     window.snarkjs = snarkjs
-        }
     }
 
     async handlePeerTransaction(app, tx = null, peer, mycallback) {
