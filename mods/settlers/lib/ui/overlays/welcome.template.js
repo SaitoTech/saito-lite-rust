f0d8c067856ae5db5fbf26a63eb5a65b44b31ef9
Daniel Worlton <f8daniel@gmail.com>
2025-05-05 12:40:51 +0200

    add optout for settlers help

--- mods/settlers/lib/ui/overlays/welcome.template.js
+++ mods/settlers/lib/ui/overlays/welcome.template.js
@@ -2,7 +2,8 @@ module.exports = () => {
 	return `
         <div class="welcome welcome-overlay">
           <div class="welcome-title"></div>
-            <div class="welcome-text"></div>
+          <div class="welcome-text"></div>
+          <div class="welcome-optout"><input type="checkbox" name="dontshowme" value="true"/> don't show again </div>
         </div>
   `;
 };
