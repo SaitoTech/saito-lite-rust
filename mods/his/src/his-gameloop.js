bbafb02ef2649c9df55e1a9fdf2f954eff130c75
David Lancashire <david.lancashire@gmail.com>
2025-05-09 22:14:32 +0700

    fix: his patch

--- mods/his/src/his-gameloop.js
+++ mods/his/src/his-gameloop.js
@@ -13343,17 +13343,17 @@ If this is your first game, it is usually fine to skip the diplomacy phase until
 
     	        this.game.queue.push("hand_to_fhand\t1\t"+(i+1)+"\t"+this.game.state.players_info[i].factions[z]);
 
-cardnum = 1;
-if (this.game.options.scenario == "is_testing") {
+//cardnum = 1;
+//if (this.game.options.scenario == "is_testing") {
 // if (f == "france") { cardnum = 0; }
 // if (f == "papacy") { cardnum = 0; }
  //if (f == "hapsburg") { cardnum = 1; }
 // if (f == "protestant") { cardnum = 0; }
 // if (f == "england") { cardnum = 0; }
  //if (f == "ottoman") { cardnum = 0; }
-} else {
+//} else {
     		this.game.queue.push("add_home_card\t"+(i+1)+"\t"+this.game.state.players_info[i].factions[z]);
-}
+//}
 
     	        this.game.queue.push("DEAL\t1\t"+(i+1)+"\t"+(cardnum));
 
